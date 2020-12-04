6fc86a9846e88606fce0b9de9e6316bbcd341f59
pareronia <49491686+pareronia@users.noreply.github.com>
2020-12-04 20:48:24 +0100

    AoC 2020 Day 1 - prettify + test

--- 2020/AoC2020_1.py
+++ 2020/AoC2020_1.py
@@ -16,7 +16,7 @@ def _print_elapsed(prefix: str, start: float, stop: float) -> None:
     print(f"{prefix} Elapsed time: {stop-start:0.5f} seconds")
 
 
-def _primeagen(inputs: tuple) -> None:
+def _primeagen(inputs: tuple) -> int:
     others = set()
     start = time.perf_counter()
     for _input in inputs:
@@ -24,13 +24,14 @@ def _primeagen(inputs: tuple) -> None:
         others.add(i)
         check = 2020 - i
         if check in others:
-            print(f"[primeagen] Twosome: {i} * {check} = {i*check}")
+            result = i*check
+            print(f"[primeagen] Twosome: {i} * {check} = {result}")
             stop = time.perf_counter()
             _print_elapsed("[primeagen]", start, stop)
-            return
+            return result
 
 
-def _squared(inputs: tuple) -> None:
+def _squared(inputs: tuple) -> int:
     start = time.perf_counter()
     twosome = None
     end = len(inputs) - 1
@@ -41,13 +42,15 @@ def _squared(inputs: tuple) -> None:
             if i1 + i2 == 2020:
                 twosome = (i1, i2)
                 break
+    result = twosome[0]*twosome[1]
     stop = time.perf_counter()
     print(f"[squared] Twosome: {twosome[0]} * {twosome[1]}"
-          f" = {twosome[0]*twosome[1]}")
+          f" = {result}")
     _print_elapsed("[squared]", start, stop)
+    return result
 
 
-def _cubed(inputs: tuple) -> None:
+def _cubed(inputs: tuple) -> int:
     start = time.perf_counter()
     end = len(inputs) - 1
     for i in range(0, end):
@@ -59,14 +62,34 @@ def _cubed(inputs: tuple) -> None:
                 if i1 + i2 + i3 == 2020:
                     threesome = (i1, i2, i3)
                     break
+    result = threesome[0]*threesome[1]*threesome[2]
     stop = time.perf_counter()
     print(f"[cubed] Threesome: {threesome[0]} * {threesome[1]}"
           f" * {threesome[2]}"
-          f" = {threesome[0]*threesome[1]*threesome[2]}")
+          f" = {result}")
     _print_elapsed("[cubed]", start, stop)
+    return result
+
+
+test = ("1721",
+        "979",
+        "366",
+        "299",
+        "675",
+        "1456",
+        )
 
 
 def main() -> None:
+    print("====================================================")
+    print("AoC 2020 Day 1 - https://adventofcode.com/2020/day/1")
+    print("====================================================")
+    print("")
+
+    assert _squared(test) == 514579
+    assert _cubed(test) == 241861950
+    assert _primeagen(test) == 514579
+
     inputs = _get_input()
     print(inputs)
     _squared(inputs)
