a005157927020d66ed4286218bb3bea73c4b1e8f
pareronia <49491686+pareronia@users.noreply.github.com>
2020-12-08 08:06:45 +0100

    AoC 2020 Day 8 Part 2

--- 2020/AoC2020_8.py
+++ 2020/AoC2020_8.py
@@ -9,20 +9,27 @@ def _parse(inputs: tuple[str]) -> list[tuple[str, int]]:
     inss = list[tuple[str, int]]()
     for input_ in inputs:
         sp = input_.split()
-        inss.append((sp[0], sp[1]))
+        inss.append((sp[0], int(sp[1])))
     return inss
 
 
-def part_1(inputs: tuple[str]) -> int:
-    inss = _parse(inputs)
-    print(inss)
+def _log(msg: str) -> None:
+    if __debug__:
+        print(msg)
+
+
+def _run_program(inss: list[tuple[str, int]], error_on_inf_loop: bool) -> int:
+    _log(inss)
     acc = 0
     seen = set[int]()
     i = 0
     while i not in seen:
+        if i == len(inss):
+            _log("Normal exit")
+            return acc
         seen.add(i)
         ins = inss[i]
-        print(f"{i}: {ins}")
+        _log(f"{i}: {ins}")
         if ins[0] == "nop":
             i += 1
         elif ins[0] == "acc":
@@ -34,8 +41,45 @@ def part_1(inputs: tuple[str]) -> int:
                 raise ValueError("Invalid input")
         else:
             raise ValueError("Invalid input")
-        print(f" -> {i} - {acc}")
-    return acc
+        _log(f" -> {i} - {acc}")
+    if error_on_inf_loop:
+        raise RuntimeError("Infinite loop!")
+    else:
+        return acc
+
+
+def part_1(inputs: tuple[str]) -> int:
+    _log(inputs)
+    inss = _parse(inputs)
+    return _run_program(inss, False)
+
+
+def part_2(inputs: tuple[str]) -> int:
+    _log(inputs)
+    inss = _parse(inputs)
+    for i in range(len(inss)):
+        ins = inss[i]
+        _log(ins)
+        if ins[0] == "nop":
+            if ins[1] == 0:
+                _log("nop 0 : skip")
+                continue
+            inss[i] = ("jmp", ins[1])
+            _log("nop -> jmp")
+            try:
+                return _run_program(inss, True)
+            except RuntimeError:
+                inss[i] = ("nop", ins[1])
+        if ins[0] == "jmp":
+            inss[i] = ("nop", ins[1])
+            _log("jmp -> nop")
+            try:
+                return _run_program(inss, True)
+            except RuntimeError:
+                inss[i] = ("jmp", ins[1])
+        if ins[0] == "acc":
+            _log("acc: skip")
+            continue
 
 
 test = ("nop +0",
@@ -54,13 +98,13 @@ def main() -> None:
     my_aocd.print_header(2020, 8)
 
     assert part_1(test) == 5
-    # assert part_2(test) == 6
+    assert part_2(test) == 8
 
     inputs = my_aocd.get_input_as_tuple(2020, 8, 608)
     result1 = part_1(inputs)
+    result2 = part_2(inputs)
     print(f"Part 1: {result1}")
-    # result2 = part_2(inputs)
-    # print(f"Part 2: {result2}")
+    print(f"Part 2: {result2}")
 
 
 if __name__ == '__main__':
