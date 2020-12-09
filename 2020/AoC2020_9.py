fd46e5c13b103c35b0e52e3950e9bf8984c6ef3a
pareronia <49491686+pareronia@users.noreply.github.com>
2020-12-09 09:12:28 +0100

    AoC 2020 Day 9 Part 2

--- 2020/AoC2020_9.py
+++ 2020/AoC2020_9.py
@@ -24,8 +24,26 @@ def part_1(inputs: tuple[int], window_size: int = 25) -> int:
     raise ValueError("Unsolvable")
 
 
-def part_2(inputs: tuple[int]) -> int:
-    return 0
+def _sublists(inputs: tuple[int]) -> [[]]:
+    sublist = [[]]
+    for i in range(len(inputs) + 1):
+        for j in range(i + 1, len(inputs) + 1):
+            sli = inputs[i:j]
+            sublist.append(sli)
+    return sublist
+
+
+def part_2(inputs: tuple[int], target: int,
+           window_size: int = 25) -> int:
+    print(inputs)
+    target_pos = inputs.index(target)
+    print(target_pos)
+    for s in _sublists(inputs[:target_pos]):
+        if len(s) >= 2:
+            # print(s)
+            if sum(s) == target:
+                print(f"Found: {s}")
+                return min(s)+max(s)
 
 
 test = (35,
@@ -55,13 +73,13 @@ def main() -> None:
     my_aocd.print_header(2020, 9)
 
     assert part_1(test, 5) == 127
-    # assert part_2(test) == 0
+    assert part_2(test, 127, 5) == 62
 
     inputs = my_aocd.get_input_as_ints_tuple(2020, 9, 1000)
     result1 = part_1(inputs)
     print(f"Part 1: {result1}")
-    # result2 = part_2(inputs)
-    # print(f"Part 2: {result2}")
+    result2 = part_2(inputs, result1)
+    print(f"Part 2: {result2}")
 
 
 if __name__ == '__main__':
