7118b6fd1844f4d401767c0b620d64acf056ca24
pareronia <49491686+pareronia@users.noreply.github.com>
2020-12-12 10:09:52 +0100

    AoC 2020 Day 12 - cleanup

--- 2020/AoC2020_12.py
+++ 2020/AoC2020_12.py
@@ -7,6 +7,15 @@ import my_aocd
 from common import log
 
 
+NORTH = "N"
+EAST = "E"
+SOUTH = "S"
+WEST = "W"
+RIGHT = "R"
+LEFT = "L"
+FORWARD = "F"
+
+
 @dataclass
 class NavigationInstruction:
     action: str
@@ -31,45 +40,45 @@ def _parse(inputs: tuple[str]) -> list[NavigationInstruction]:
 
 def _new_orientation(start: str, action: str, value: int) -> str:
     if value == 180:
-        if start == "N":
-            return "S"
-        elif start == "E":
-            return "W"
-        elif start == "S":
-            return "N"
-        elif start == "W":
-            return "E"
+        if start == NORTH:
+            return SOUTH
+        elif start == EAST:
+            return WEST
+        elif start == SOUTH:
+            return NORTH
+        elif start == WEST:
+            return EAST
         else:
             raise ValueError("invalid input")
     if value == 270:
-        if action == "L":
-            action = "R"
+        if action == LEFT:
+            action = RIGHT
             value = 90
-        elif action == "R":
-            action = "L"
+        elif action == RIGHT:
+            action = LEFT
             value = 90
         else:
             raise ValueError("invalid input")
     results = dict()
-    results["NR90"] = "E"
-    results["NL90"] = "W"
-    results["ER90"] = "S"
-    results["EL90"] = "N"
-    results["SR90"] = "W"
-    results["SL90"] = "E"
-    results["WR90"] = "N"
-    results["WL90"] = "S"
-    return results[start+action+str(value)]
+    results[NORTH + "/" + RIGHT] = EAST
+    results[NORTH + "/" + LEFT] = WEST
+    results[EAST + "/" + RIGHT] = SOUTH
+    results[EAST + "/" + LEFT] = NORTH
+    results[SOUTH + "/" + RIGHT] = WEST
+    results[SOUTH + "/" + LEFT] = EAST
+    results[WEST + "/" + RIGHT] = NORTH
+    results[WEST + "/" + LEFT] = SOUTH
+    return results[start + "/" + action]
 
 
 def _move(position: Position, direction: str, value: int) -> Position:
-    if direction == "N":
+    if direction == NORTH:
         vector = Vector(0, value)
-    elif direction == "E":
+    elif direction == EAST:
         vector = Vector(value, 0)
-    elif direction == "S":
+    elif direction == SOUTH:
         vector = Vector(0, -value)
-    elif direction == "W":
+    elif direction == WEST:
         vector = Vector(-value, 0)
     else:
         raise ValueError("invalid input")
@@ -93,45 +102,53 @@ def _rotate(position: Position, degrees: int) -> Position:
         raise ValueError("invalid input")
 
 
+def _navigate_1(position: Position, orientation: str,
+                nav: NavigationInstruction) -> (Position, str):
+    if nav.action in {RIGHT, LEFT}:
+        orientation = _new_orientation(orientation, nav.action, nav. value)
+    elif nav.action == FORWARD:
+        position = _move(position, orientation, nav.value)
+    else:
+        position = _move(position, nav.action, nav.value)
+    return position, orientation
+
+
 def part_1(inputs: tuple[str], start: str) -> int:
     navs = _parse(inputs)
-    log(navs)
-    orientation = start
     position = Position(0, 0)
+    orientation = start
+    log({"position": position, "orientation": orientation})
     for nav in navs:
-        if nav.action in ("R", "L"):
-            orientation = _new_orientation(orientation, nav.action, nav. value)
-            log(orientation)
-        elif nav.action == "F":
-            position = _move(position, orientation, nav.value)
-            log(position)
-        else:
-            position = _move(position, nav.action, nav.value)
-            log(position)
+        position, orientation = _navigate_1(position, orientation, nav)
+        log(f"{nav} ->"
+            f"{{'position': {position}, 'orientation': {orientation}}}")
     return abs(position.x) + abs(position.y)
+
+
+def _navigate_2(position: Position, waypoint: Position,
+                nav: NavigationInstruction) -> (Position, Position):
+    if nav.action == FORWARD:
+        vector = Vector(waypoint.x*nav.value, waypoint.y*nav.value)
+        position = _translate(position, vector)
+    elif nav.action in {NORTH, EAST, SOUTH, WEST}:
+        waypoint = _move(waypoint, nav.action, nav.value)
+    elif nav.action == RIGHT:
+        waypoint = _rotate(waypoint, nav.value)
+    elif nav.action == LEFT:
+        waypoint = _rotate(waypoint, -nav.value)
+    else:
+        raise ValueError("invalid input")
+    return position, waypoint
 
 
 def part_2(inputs: tuple[str], waypoint: Position) -> int:
     navs = _parse(inputs)
-    log(navs)
     position = Position(0, 0)
     waypoint = Position(waypoint.x, waypoint.y)
+    log({"position": position, "waypoint": waypoint})
     for nav in navs:
-        if nav.action == "F":
-            vector = Vector(waypoint.x*nav.value, waypoint.y*nav.value)
-            position = _translate(position, vector)
-            log((waypoint, position))
-        elif nav.action in ("N", "S", "E", "W"):
-            waypoint = _move(waypoint, nav.action, nav.value)
-            log((waypoint, position))
-        elif nav.action == "R":
-            waypoint = _rotate(waypoint, nav.value)
-            log((waypoint, position))
-        elif nav.action == "L":
-            waypoint = _rotate(waypoint, -nav.value)
-            log((waypoint, position))
-        else:
-            raise ValueError("invalid input")
+        position, waypoint = _navigate_2(position, waypoint, nav)
+        log(f"{nav} -> {{'position': {position}, 'waypoint': {waypoint}}}")
     return abs(position.x) + abs(position.y)
 
 
@@ -147,11 +164,11 @@ F11
 def main() -> None:
     my_aocd.print_header(2020, 12)
 
-    assert part_1(test.split(), "E") == 25
+    assert part_1(test.split(), EAST) == 25
     assert part_2(test.split(), Position(10, 1)) == 286
 
     inputs = my_aocd.get_input_as_tuple(2020, 12, 785)
-    result1 = part_1(inputs, "E")
+    result1 = part_1(inputs, EAST)
     print(f"Part 1: {result1}")
     result2 = part_2(inputs, Position(10, 1))
     print(f"Part 2: {result2}")
