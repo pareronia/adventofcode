d9db88a415d98ad77143eb020168bfe013fd9d7b
pareronia <49491686+pareronia@users.noreply.github.com>
2020-12-13 21:03:54 +0100

    [done,FIXME] AoC 2020 Day 13 Part 2
    with a little hint from Reddit...

--- 2020/AoC2020_13.py
+++ 2020/AoC2020_13.py
@@ -2,6 +2,7 @@
 #
 # Advent of Code 2020 Day 13
 #
+# FIXME: Make part2 bis into algorithm
 
 import my_aocd
 from common import log
@@ -48,27 +49,33 @@ def _find_largest_and_recalc_offsets(i: list[tuple[int, int]]):
     for j in i:
         if j != m:
             new_i.append((j[0], j[1]-m[1]))
+            new_i.sort(key=lambda t: t[0], reverse=True)
     return m, new_i
 
 
 def _init_multiples(buses: list[tuple[int, int]], start: int) -> dict:
     multiples = {}
     for b in buses:
-        multiples[b[0]] = start * b[0]
+        multiples[b[0]] = start // b[0] * b[0]
+    log(f"init multiples: {multiples}")
     return multiples
 
 
 def _lookup_multiple(multiples: dict, bus: int, wanted: int):
+    # log(f"{wanted} for bus {bus}")
     # return wanted % bus == 0
     last = multiples[bus]
     while True:
         next_ = last + bus
+        # if (not next_ % (10000*bus)):
+        #     print((last, next_, wanted))
         if next_ > wanted:
             return False
         else:
             multiples[bus] = next_
             last = next_
             if next_ == wanted:
+                # log(f"found; {next_}={wanted} for bus {bus}")
                 return True
 
 
@@ -80,23 +87,75 @@ def part_2(inputs: tuple[int], start: int) -> int:
     log(m)
     log(i)
     a = start // m[0]
-    multiples = _init_multiples(i, start=a)
     a_m = a * m[0]
+    log(f"start: {start}")
+    multiples = _init_multiples(i, start=a_m)
     cnt = 0
     while True:
         cnt += 1
         ok = True
         for j in range(len(i)):
-            found = _lookup_multiple(multiples, bus=i[j][0],
-                                     wanted=a_m + i[j][1])
+            bus = i[j][0]
+            wanted = a_m + i[j][1]
+            # if not cnt % 10000:
+            #     log((cnt, bus, wanted))
+            found = _lookup_multiple(multiples, bus, wanted)
             if not found:
                 ok = False
                 break
         if ok:
-            log(cnt)
-            return a_m if m[1] == 0 else a_m + i[0][1]
+            i.sort(key=lambda t: t[1])
+            result = a_m if m[1] == 0 else a_m + i[0][1]
+            log(f"Result: {(cnt, result)}")
+            return result
         a_m += m[0]
     return 0
+
+
+def part_2_bis(inputs: tuple[int], start: int) -> int:
+    cnt = 1
+    r = 0
+    while True:
+        r = cnt * 23
+        if (r + 13) % 41 == 0:
+            break
+        cnt += 1
+    lcm1 = 23 * 41
+    while True:
+        r = r + lcm1
+        if (r + 23) % 509 == 0:
+            break
+    lcm2 = 23 * 41 * 509
+    while True:
+        r = r + lcm2
+        if (r + 36) % 13 == 0:
+            break
+    lcm3 = 23 * 41 * 509 * 13
+    while True:
+        r = r + lcm3
+        if (r + 37) % 17 == 0:
+            break
+    lcm4 = 23 * 41 * 509 * 13 * 17
+    while True:
+        r = r + lcm4
+        if (r + 52) % 29 == 0:
+            break
+    lcm5 = 23 * 41 * 509 * 13 * 17 * 29
+    while True:
+        r = r + lcm5
+        if (r + 54) % 401 == 0:
+            break
+    lcm6 = 23 * 41 * 509 * 13 * 17 * 29 * 401
+    while True:
+        r = r + lcm6
+        if (r + 60) % 37 == 0:
+            break
+    lcm7 = 23 * 41 * 509 * 13 * 17 * 29 * 401 * 37
+    while True:
+        r = r + lcm7
+        if (r + 73) % 19 == 0:
+            break
+    return r
 
 
 test_1 = ("939",
@@ -123,7 +182,6 @@ def main() -> None:
     my_aocd.print_header(2020, 13)
 
     assert part_1(test_1) == 295
-    assert part_2(("0", "3,5,7"), 7) == 54
     assert part_2(test_1, 1000000) == 1068781
     assert part_2(test_2, 3000) == 3417
     assert part_2(test_3, 75000) == 754018
@@ -134,8 +192,10 @@ def main() -> None:
     inputs = my_aocd.get_input_as_tuple(2020, 13, 2)
     result1 = part_1(inputs)
     print(f"Part 1: {result1}")
-    result2 = part_2(inputs, 100000000000000)
-    print(f"Part 2: {result2}")
+    # result2 = part_2(inputs, 100000000000000)
+    # print(f"Part 2: {result2}")
+    result2 = part_2_bis(inputs, 0)
+    print(f"Part 2 bis: {result2}")
 
 
 if __name__ == '__main__':
