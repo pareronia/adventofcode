470d5e7bc7b349dc5d7e4301dce49c8a36238d15
pareronia <49491686+pareronia@users.noreply.github.com>
2020-12-15 13:16:36 +0100

    debug log not just as string / in color

--- 2020/common.py
+++ 2020/common.py
@@ -1,6 +1,6 @@
-from prettyprinter import pprint
+from prettyprinter import cpprint
 
 
-def log(msg: str) -> None:
+def log(msg) -> None:
     if __debug__:
-        pprint(msg)
+        cpprint(msg)
