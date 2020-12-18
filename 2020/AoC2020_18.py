bfd56b75e1a61dbb64bd6806b022aaa183d6cace
pareronia <49491686+pareronia@users.noreply.github.com>
2020-12-18 11:39:41 +0100

    AoC 2020 Day 18 Part 2

--- 2020/AoC2020_18.py
+++ 2020/AoC2020_18.py
@@ -3,6 +3,7 @@
 # Advent of Code 2020 Day 18
 #
 
+# import re
 from dataclasses import dataclass
 import my_aocd
 from common import log
@@ -49,12 +50,20 @@ def evaluate(expression: list[str], pos: int = 0) -> tuple[int, int]:
 
 def part_1(inputs: tuple[str]) -> int:
     expressions = _parse(inputs)
-    log(expressions)
+    [log(str(expression)) for expression in expressions]
     return sum([evaluate(exp)[0] for exp in expressions])
 
 
 def part_2(inputs: tuple[str]) -> int:
-    return 0
+    new_inputs = []
+    for input_ in inputs:
+        x = input_.replace("(", "((")
+        x = x.replace(")", "))")
+        x = "(" + x.replace("*", ") * (") + ")"
+        log(f"{input_} -> {x}")
+        new_inputs.append(x)
+    expressions = _parse(new_inputs)
+    return sum([evaluate(exp)[0] for exp in expressions])
 
 
 test = """\
@@ -71,13 +80,13 @@ def main() -> None:
     my_aocd.print_header(2020, 18)
 
     assert part_1(test) == 71 + 51 + 26 + 437 + 12240 + 13632
-    assert part_2(test) == 0
+    assert part_2(test) == 231 + 51 + 46 + 1445 + 669060 + 23340
 
     inputs = my_aocd.get_input_as_tuple(2020, 18, 383)
     result1 = part_1(inputs)
     print(f"Part 1: {result1}")
-    # result2 = part_2(inputs)
-    # print(f"Part 2: {result2}")
+    result2 = part_2(inputs)
+    print(f"Part 2: {result2}")
 
 
 if __name__ == '__main__':
