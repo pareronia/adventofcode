bbc7aca4b9c12460dc72dfe5c2d52d9ad7890d30
pareronia <49491686+pareronia@users.noreply.github.com>
2020-12-25 01:55:58 +0100

    [fixed] AoC 2020 Day 23 Part 2

--- 2020/AoC2020_23.py
+++ 2020/AoC2020_23.py
@@ -3,8 +3,25 @@
 # Advent of Code 2020 Day 23
 #
 
+from __future__ import annotations
+from dataclasses import dataclass
 import my_aocd
 from common import log
+
+
+@dataclass
+class Cup:
+    label: int
+    prev: Cup
+    next_: Cup
+
+    def move_after(self, cup: Cup):
+        self.next_.prev = self.prev
+        self.prev.next_ = self.next_
+        cup.next_.prev = self
+        self.next_ = cup.next_
+        self.prev = cup
+        cup.next_ = self
 
 
 def _parse(inputs: tuple[str]) -> list[int]:
@@ -12,100 +29,101 @@ def _parse(inputs: tuple[str]) -> list[int]:
     return [int(c) for c in inputs[0]]
 
 
-def _log(cups: dict, msg):
-    if len(cups) <= 10:
+def _log(size: int, msg):
+    if size <= 10:
         log(msg)
 
 
-def _print_cups(val: dict[int], current: int) -> str:
-    if len(val) > 10:
+def _print_cups(move: int, cups: dict[int, Cup], current: int) -> str:
+    if len(cups) > 10:
         return
-    cups = [str(val[i]) for i in sorted(val.keys(), key=lambda k: k)]
-    result = " ".join(cups)
-    result = result.replace(str(val[current]), "(" + str(val[current]) + ")")
+    c = cups[current]
+    start = c
+    for i in range(move):
+        start = start.prev
+    p = start
+    result = ""
+    while True:
+        result += f"{p.label} "
+        p = p.next_
+        if p == start:
+            break
+    result = result.replace(str(c.label), "(" + str(c.label) + ")")
     return result
 
 
-def _do_move(move: int, val: dict[int], idx: dict[int],
-             current: int) -> (dict[int], dict[int]):
-    min_val = min(val.values())
-    max_val = max(val.values())
-    size = len(val)
-    _log(val, f"-- move {move+1} --")
-    _log(val, f"cups: {_print_cups(val, current)}")
-    _log(val, idx)
-    _log(val, val)
-    cv = val[current]
-    p1v = val[(current+1) % size]
-    p2v = val[(current+2) % size]
-    p3v = val[(current+3) % size]
-    pick_up = [p1v, p2v, p3v]
-    _log(val, f"pick up: {pick_up}")
-    dv = cv-1
-    if dv < min_val:
-        dv = max_val
-    while dv in pick_up:
-        dv -= 1
-        if dv < min_val:
-            dv = max_val
-    _log(val, f"destination: {dv}")
-    for pv in reversed(pick_up):
-        di = idx[dv]
-        pvi = idx[pv]
-        if di > pvi:
-            for i in range(pvi+1, di+1):
-                i1 = i % size
-                v1 = val[i1]
-                i2 = (i-1) % size
-                val[i2], idx[v1] = v1, i2
-            val[di], idx[pv] = pv, di
-        else:
-            for i in range(pvi-1, di, -1):
-                i1 = i % size
-                v1 = val[i1]
-                i2 = (i+1) % size
-                val[i2], idx[v1] = v1, i2
-            val[(di+1) % size], idx[pv] = pv, (di+1) % size
-    current = (idx[cv]+1) % size
-    _log(val, "")
-    return val, idx, current
+def _prepare_cups(labels: list[int]) -> (dict[int, Cup], int, int, int):
+    size = len(labels)
+    min_val = min(labels)
+    max_val = max(labels)
+    cups = dict[int, Cup]()
+    for label in labels:
+        cups[label] = Cup(label, None, None)
+    for i, label in enumerate(labels):
+        prev = labels[(i-1) % len(labels)]
+        next_ = labels[(i+1) % len(labels)]
+        cups[label].prev = cups[prev] if prev in cups else None
+        cups[label].next_ = cups[next_] if next_ in cups else None
+    cups[labels[0]].prev = cups[labels[-1]]
+    cups[labels[-1]].next_ = cups[labels[0]]
+    return cups, size, min_val, max_val
 
 
-def _prepare_bidi(cups: list[int]) -> (dict[int], dict[int]):
-    val = dict[int]()
-    idx = dict[int]()
-    for i, cup in enumerate(cups):
-        val[i] = cup
-        idx[cup] = i
-    return val, idx
+def _do_move(move: int, cups: dict[int, Cup],
+             current: int, size: int, min_val: int,
+             max_val: int) -> (dict[int, Cup], int):
+    _log(size, f"-- move {move+1} --")
+    _log(size, f"cups: {_print_cups(move, cups, current)}")
+    c = cups[current]
+    p1 = c.next_
+    p2 = p1.next_
+    p3 = p2.next_
+    pickup = (p1.label, p2.label, p3.label)
+    _log(size, f"pick up: {p1.label}, {p2.label}, {p3.label}")
+    d = c.label-1
+    if d < min_val:
+        d = max_val
+    while d in pickup:
+        d -= 1
+        if d < min_val:
+            d = max_val
+    _log(size, f"destination: {d}")
+    p1.move_after(cups[d])
+    p2.move_after(p1)
+    p3.move_after(p2)
+    current = c.next_.label
+    _log(size, "")
+    return cups, current
 
 
 def part_1(inputs: tuple[str]) -> int:
     cups = _parse(inputs)
-    val, idx = _prepare_bidi(cups)
-    current = 0
+    cd, size, min_val, max_val = _prepare_cups(cups)
+    current = cups[0]
     for move in range(100):
-        val, idx, current = _do_move(move, val, idx, current)
-    log("-- final --")
-    _log(val, f"cups: {_print_cups(val, current)}")
-    one = idx[1]
-    result = sum([val[(one+1+i) % len(val)] * 10**(len(val)-2-i)
-                  for i in range(len(val)-1)])
+        cd, current = _do_move(move, cd, current, size, min_val, max_val)
+    cup = cd[1]
+    result = ""
+    while cup.next_ != cd[1]:
+        result += str(cup.next_.label)
+        cup = cup.next_
     log(result)
-    return result
+    return int(result)
 
 
 def part_2(inputs: tuple[str]) -> int:
     cups = _parse(inputs)
     cups.extend([i for i in range(max(cups)+1, 1_000_001)])
-    val, idx = _prepare_bidi(cups)
-    current = 0
+    cd, size, min_val, max_val = _prepare_cups(cups)
+    current = cups[0]
     for move in range(10_000_000):
-        val, idx, current = _do_move(move, val, idx, current)
-        print('.', end='', flush=True)
-    one = idx[1]
-    star1 = val[one+1]
-    star2 = val[one+2]
+        cd, current = _do_move(move, cd, current, size, min_val, max_val)
+        if move % 100_000 == 0:
+            print('.', end='', flush=True)
+    print("")
+    one = cd[1]
+    star1 = one.next_.label
+    star2 = one.next_.next_.label
     return star1 * star2
 
 
@@ -122,8 +140,8 @@ def main() -> None:
 
     result1 = part_1(["974618352"])
     print(f"Part 1: {result1}")
-    # result2 = part_2(inputs)
-    # print(f"Part 2: {result2}")
+    result2 = part_2(["974618352"])
+    print(f"Part 2: {result2}")
 
 
 if __name__ == '__main__':
