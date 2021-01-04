1ae1f5083d02157f754d7b60dafbc7f45543257c
pareronia <49491686+pareronia@users.noreply.github.com>
2021-01-04 19:04:34 +0100

    AoC 2015 Day 3 Part 2

--- src/main/python/AoC2015_03.py
+++ src/main/python/AoC2015_03.py
@@ -14,32 +14,50 @@ SOUTH = Headings["S"].value
 WEST = Headings["W"].value
 
 
+# TODO: make geometry.Position hashable ?
+def _count_unique_positions(positions: list[Position]) -> int:
+    return len({(p.x, p.y) for p in positions})
+
+
+def _add_navigation_instruction(navigation: NavigationWithHeading, c: str):
+    if c == ">":
+        navigation.drift(EAST, 1)
+    elif c == "v":
+        navigation.drift(SOUTH, 1)
+    elif c == "<":
+        navigation.drift(WEST, 1)
+    elif c == "^":
+        navigation.drift(NORTH, 1)
+    else:
+        raise ValueError("Invalid input")
+
+
 def part_1(inputs: tuple[str]) -> int:
     assert len(inputs) == 1
     navigation = NavigationWithHeading(Position(0, 0), NORTH)
     for c in inputs[0]:
-        if c == ">":
-            navigation.drift(EAST, 1)
-        elif c == "v":
-            navigation.drift(SOUTH, 1)
-        elif c == "<":
-            navigation.drift(WEST, 1)
-        elif c == "^":
-            navigation.drift(NORTH, 1)
-        else:
-            raise ValueError("Invalid input")
-    visited = navigation.get_visited_positions(True)
-    visited = {(p.x, p.y) for p in visited}
-    return len(visited)
+        _add_navigation_instruction(navigation, c)
+    return _count_unique_positions(navigation.get_visited_positions(True))
 
 
 def part_2(inputs: tuple[str]) -> int:
-    return 0
+    assert len(inputs) == 1
+    santa_navigation = NavigationWithHeading(Position(0, 0), NORTH)
+    robot_navigation = NavigationWithHeading(Position(0, 0), NORTH)
+    for i, c in enumerate(inputs[0]):
+        if i % 2 != 0:
+            _add_navigation_instruction(robot_navigation, c)
+        else:
+            _add_navigation_instruction(santa_navigation, c)
+    visited = santa_navigation.get_visited_positions(True)
+    visited.extend(robot_navigation.get_visited_positions(True))
+    return _count_unique_positions(visited)
 
 
 TEST1 = ">".splitlines()
 TEST2 = "^>v<".splitlines()
 TEST3 = "^v^v^v^v^v".splitlines()
+TEST4 = "^v".splitlines()
 
 
 def main() -> None:
@@ -48,6 +66,9 @@ def main() -> None:
     assert part_1(TEST1) == 2
     assert part_1(TEST2) == 4
     assert part_1(TEST3) == 2
+    assert part_2(TEST4) == 3
+    assert part_2(TEST2) == 3
+    assert part_2(TEST3) == 11
 
     inputs = my_aocd.get_input_as_tuple(2015, 3, 1)
     result1 = part_1(inputs)
