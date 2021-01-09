f10c4c5961c1832d2955bc76ed2524942f5dcecc
pareronia <49491686+pareronia@users.noreply.github.com>
2021-01-09 11:40:22 +0100

    AoC 2015 Day 16 Part 2

--- src/main/python/AoC2015_16.py
+++ src/main/python/AoC2015_16.py
@@ -101,7 +101,65 @@ def part_1(inputs: tuple[str]) -> int:
 
 
 def part_2(inputs: tuple[str]) -> int:
-    return 0
+    aunt_sues = _parse(inputs)
+    log(aunt_sues)
+    scores = defaultdict(int)
+    for aunt_sue in aunt_sues:
+        if aunt_sue.has_thing('children'):
+            if aunt_sue.get_thing('children') == 3:
+                scores[aunt_sue.nbr] += 1
+            else:
+                continue
+        if aunt_sue.has_thing('cats'):
+            if aunt_sue.get_thing('cats') > 7:
+                scores[aunt_sue.nbr] += 1
+            else:
+                continue
+        if aunt_sue.has_thing('samoyeds'):
+            if aunt_sue.get_thing('samoyeds') == 2:
+                scores[aunt_sue.nbr] += 1
+            else:
+                continue
+        if aunt_sue.has_thing('pomeranians'):
+            if aunt_sue.get_thing('pomeranians') < 3:
+                scores[aunt_sue.nbr] += 1
+            else:
+                continue
+        if aunt_sue.has_thing('akitas'):
+            if aunt_sue.get_thing('akitas') == 0:
+                scores[aunt_sue.nbr] += 1
+            else:
+                continue
+        if aunt_sue.has_thing('vizslas'):
+            if aunt_sue.get_thing('vizslas') == 0:
+                scores[aunt_sue.nbr] += 1
+            else:
+                continue
+        if aunt_sue.has_thing('goldfish'):
+            if aunt_sue.get_thing('goldfish') < 5:
+                scores[aunt_sue.nbr] += 1
+            else:
+                continue
+        if aunt_sue.has_thing('trees'):
+            if aunt_sue.get_thing('trees') > 3:
+                scores[aunt_sue.nbr] += 1
+            else:
+                continue
+        if aunt_sue.has_thing('cars'):
+            if aunt_sue.get_thing('cars') == 2:
+                scores[aunt_sue.nbr] += 1
+            else:
+                continue
+        if aunt_sue.has_thing('perfumes'):
+            if aunt_sue.get_thing('perfumes') == 1:
+                scores[aunt_sue.nbr] += 1
+            else:
+                continue
+    best_score = None
+    for score in scores.items():
+        if best_score is None or score[1] > best_score[1]:
+            best_score = score
+    return best_score[0]
 
 
 def main() -> None:
