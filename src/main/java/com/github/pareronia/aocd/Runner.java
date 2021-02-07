0fd0f227949adf04aaa2a3363192ce0cbbf67b7a
pareronia <49491686+pareronia@users.noreply.github.com>
2021-02-07 11:24:13 +0100

    Java entrypoint for aocd runner - test + refactor

--- src/main/java/com/github/pareronia/aocd/Runner.java
+++ src/main/java/com/github/pareronia/aocd/Runner.java
@@ -1,57 +1,108 @@
 package com.github.pareronia.aocd;
 
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class Runner {
 
 	public static void main(String[] args) throws Exception {
-		new Runner(args).run();
+		final Response result = Runner.create().run(args);
+		System.out.println(result.toString());
 	}
 
-	private final Integer year;
-	private final Integer day;
-	private final List<String> inputs;
-	
-	private Runner(String args[]) {
-		if (args.length < 3) {
-			throw new RuntimeException("Missing args: year, day, input");
-		}
-		final LocalDate now = LocalDate.now(Aocd.AOC_TZ);
-		final Integer year = Integer.valueOf(args[0]);
-		if (year < 2015 || year > now.getYear()) {
-			throw new RuntimeException("Invalid year");
-		}
-		this.year = year;
-		final Integer day = Integer.valueOf(args[1]);
-		if ((now.getMonth() == Month.DECEMBER && day > now.getDayOfMonth())
-				|| day < 1 || day > 25) {
-			throw new RuntimeException("Invalid day");
-		}
-		this.day = day;
-		this.inputs = Stream.iterate(2, i -> i + 1).limit(args.length - 2)
-						.map(i -> args[i])
-						.flatMap(string -> Stream.of(string.split("\\r?\\n")))
-						.collect(toList());
+	private static Runner create() {
+		return new Runner(() -> LocalDate.now(Aocd.AOC_TZ),
+						  className -> Class.forName(className));
 	}
 	
-	private void run() throws Exception {
+	static Runner create(Supplier<LocalDate> dateSupplier,
+								ClassFactory classFactory) {
+		return new Runner(dateSupplier, classFactory);
+	}
+	
+	private final Supplier<LocalDate> dateSupplier;
+	private final ClassFactory classFactory;
+	
+	private Runner(Supplier<LocalDate> dateSupplier,
+				   ClassFactory classFactory) {
+		this.dateSupplier = dateSupplier;
+		this.classFactory = classFactory;
+	}
+
+	Response run(String args[]) throws Exception {
+		final Request request = new Request(this.dateSupplier, args);
 		final Class<?> klass;
 		try {
-			final String className = "AoC" + this.year.toString()
-									+ "_" + String.format("%02d", this.day);
-			klass = Class.forName(className);
+			final String className = "AoC" + request.year.toString()
+									+ "_" + String.format("%02d", request.day);
+			klass = this.classFactory.getClass(className);
 		} catch (final ClassNotFoundException e) {
-			return;
+			return Response.EMPTY;
 		}
 		final Object puzzle = klass
 				.getDeclaredMethod("create", List.class)
-				.invoke(null, this.inputs);
-		System.out.println(klass.getDeclaredMethod("solvePart1").invoke(puzzle));
-		System.out.println(klass.getDeclaredMethod("solvePart2").invoke(puzzle));
+				.invoke(null, request.inputs);
+		final Object part1 = klass.getDeclaredMethod("solvePart1").invoke(puzzle);
+		final Object part2 = klass.getDeclaredMethod("solvePart2").invoke(puzzle);
+		return new Response(part1, part2);
+	}
+	
+	private static final class Request {
+		private final Integer year;
+		private final Integer day;
+		private final List<String> inputs;
+		
+		public Request(Supplier<LocalDate> dateSupplier, String args[]) {
+			if (args == null || args.length < 3) {
+				throw new IllegalArgumentException("Missing args: year, day, input");
+			}
+			final LocalDate now = dateSupplier.get();
+			final Integer year = Integer.valueOf(args[0]);
+			if (year < 2015 || year > now.getYear()) {
+				throw new IllegalArgumentException("Invalid year");
+			}
+			this.year = year;
+			final Integer day = Integer.valueOf(args[1]);
+			if ((now.getMonth() == Month.DECEMBER && day > now.getDayOfMonth())
+					|| day < 1 || day > 25) {
+				throw new IllegalArgumentException("Invalid day");
+			}
+			this.day = day;
+			this.inputs = Stream.iterate(2, i -> i + 1).limit(args.length - 2)
+							.map(i -> args[i])
+							.flatMap(string -> Stream.of(string.split("\\r?\\n")))
+							.collect(toList());
+		}
+	}
+	
+	static final class Response {
+		public static final Response EMPTY = new Response(null, null);
+		
+		private final Object part1;
+		private final Object part2;
+		
+		public Response(Object part1, Object part2) {
+			this.part1 = part1;
+			this.part2 = part2;
+		}
+
+		@Override
+		public String toString() {
+			return Stream.of(part1, part2)
+				.filter(Objects::nonNull)
+				.map(Object::toString)
+				.collect(joining(System.lineSeparator()));
+		}
+	}
+	
+	interface ClassFactory {
+		Class<?> getClass(String className) throws ClassNotFoundException;
 	}
 }
---
+++ src/test/java/com/github/pareronia/aocd/RunnerTestCase.java
@@ -0,0 +1,110 @@
+package com.github.pareronia.aocd;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.function.Supplier;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.github.pareronia.aocd.Runner.ClassFactory;
+import com.github.pareronia.aocd.Runner.Response;
+
+public class RunnerTestCase {
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void noArgs() throws Exception {
+		Runner.create(null, null).run(null);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void missingArgs() throws Exception {
+		Runner.create(null, null).run(new String[] {"1", "2"});
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void requestedYearBefore2015() throws Exception {
+		final Supplier<LocalDate> dateSupplier = () -> LocalDate.of(2021, Month.FEBRUARY, 7);
+		Runner.create(dateSupplier, null).run(new String[] {"2014", "1", ""});
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void requestedYearAfterCurrent() throws Exception {
+		final Supplier<LocalDate> dateSupplier = () -> LocalDate.of(2021, Month.FEBRUARY, 7);
+		Runner.create(dateSupplier, null).run(new String[] {"2022", "1", ""});
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void requestedDayAfterCurrentInDecember() throws Exception {
+		final Supplier<LocalDate> dateSupplier = () -> LocalDate.of(2021, Month.DECEMBER, 7);
+		Runner.create(dateSupplier, null).run(new String[] {"2021", "8", ""});
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void requestedDayLowerThan1() throws Exception {
+		final Supplier<LocalDate> dateSupplier = () -> LocalDate.of(2021, Month.FEBRUARY, 7);
+		Runner.create(dateSupplier, null).run(new String[] {"2021", "0", ""});
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void requestedDayHigherThan25() throws Exception {
+		final Supplier<LocalDate> dateSupplier = () -> LocalDate.of(2021, Month.FEBRUARY, 7);
+		Runner.create(dateSupplier, null).run(new String[] {"2021", "31", ""});
+	}
+	
+	@Test
+	public void emptyResponseWhenClassNotFound() throws Exception {
+		final Supplier<LocalDate> dateSupplier = () -> LocalDate.of(2021, Month.FEBRUARY, 7);
+		final ClassFactory classFactory = mock(ClassFactory.class);
+		when(classFactory.getClass(Mockito.anyString()))
+				.thenThrow(new ClassNotFoundException());
+		
+		final Response response = Runner.create(dateSupplier, classFactory)
+				.run(new String[] {"2020", "1", ""});
+		
+		assertThat(response.toString(), is(""));
+	}
+
+	@Test
+	public void happy() throws Exception {
+		final Supplier<LocalDate> dateSupplier = () -> LocalDate.of(2021, Month.FEBRUARY, 7);
+		final ClassFactory classFactory = className -> Stub.class;
+		
+		final Response response = Runner.create(dateSupplier, classFactory)
+				.run(new String[] {
+						"2020",
+						"1",
+						"line1" + System.lineSeparator() + "line2", "line3"});
+		
+		assertThat(response.toString(), is("a" + System.lineSeparator() + "b"));
+	}
+	
+	private static final class Stub {
+		
+		public Stub(List<String> input) {
+			assertThat(input, is(asList("line1", "line2", "line3")));
+		}
+
+		@SuppressWarnings("unused")
+		public static Stub create(List<String> input) {
+			return new Stub(input);
+		}
+		
+		@SuppressWarnings("unused")
+		public String solvePart1() {
+			return "a";
+		}
+
+		@SuppressWarnings("unused")
+		public String solvePart2() {
+			return "b";
+		}
+	}
+}
