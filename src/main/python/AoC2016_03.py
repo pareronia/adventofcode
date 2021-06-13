0c7b4b0f7dd47527139321afecf3a3b45e923424
pareronia <49491686+pareronia@users.noreply.github.com>
2021-06-13 20:47:00 +0200

    AoC 2016 Day 3 Part 2

--- src/main/python/AoC2016_03.py
+++ src/main/python/AoC2016_03.py
@@ -23,11 +23,21 @@ def part_1(inputs: tuple[str]) -> int:
 
 
 def part_2(inputs: tuple[str]) -> int:
-    return 0
+    ts = list(_parse(inputs))
+    return sum(int(_valid((ts[i][0], ts[i+1][0], ts[i+2][0])))
+               + int(_valid((ts[i][1], ts[i+1][1], ts[i+2][1])))
+               + int(_valid((ts[i][2], ts[i+1][2], ts[i+2][2])))
+               for i in range(0, len(ts), 3)
+               )
 
 
 TEST1 = "5 10 25".splitlines()
 TEST2 = "3 4 5".splitlines()
+TEST3 = '''\
+5 3 6
+10 4 8
+25 5 10
+'''.splitlines()
 
 
 def main() -> None:
@@ -35,6 +45,7 @@ def main() -> None:
 
     assert part_1(TEST1) == 0
     assert part_1(TEST2) == 1
+    assert part_2(TEST3) == 2
 
     inputs = my_aocd.get_input(2016, 3, 1911)
     result1 = part_1(inputs)
