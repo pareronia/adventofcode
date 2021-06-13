ad07d20e2dc8842372c189e973dd5cc46c850a70
pareronia <49491686+pareronia@users.noreply.github.com>
2021-06-13 12:42:21 +0200

    AoC 2016 Day 1 Part 2

--- src/main/python/AoC2016_01.py
+++ src/main/python/AoC2016_01.py
@@ -4,7 +4,6 @@
 #
 
 from aoc import my_aocd
-from aoc.common import log
 from aoc.navigation import NavigationWithHeading, Headings
 from aoc.geometry import Position
 
@@ -14,9 +13,7 @@ def _parse(inputs: tuple[str]) -> list[str]:
     return inputs[0].split(", ")
 
 
-def part_1(inputs: tuple[str]) -> int:
-    steps = _parse(inputs)
-    log(steps)
+def _navigate(steps: list[str]) -> NavigationWithHeading:
     navigation = NavigationWithHeading(Position(0, 0), Headings["N"].value)
     for step in steps:
         if step[0] == 'R':
@@ -25,17 +22,34 @@ def part_1(inputs: tuple[str]) -> int:
             navigation.left(90)
         else:
             raise ValueError("Invalid input")
-        navigation.forward(int(step[1:]))
+        for i in range(int(step[1:])):
+            navigation.forward(1)
+    return navigation
+
+
+def part_1(inputs: tuple[str]) -> int:
+    steps = _parse(inputs)
+    navigation = _navigate(steps)
     return abs(navigation.position.x) + abs(navigation.position.y)
 
 
 def part_2(inputs: tuple[str]) -> int:
-    return 0
+    steps = _parse(inputs)
+    navigation = _navigate(steps)
+    seen = set()
+    for pos in navigation.get_visited_positions(True):
+        _pos = (pos.x, pos.y)
+        if _pos in seen:
+            return abs(pos.x) + abs(pos.y)
+        else:
+            seen.add(_pos)
+    raise ValueError("Unsolvable")
 
 
 TEST1 = "R2, L3".splitlines()
 TEST2 = "R2, R2, R2".splitlines()
 TEST3 = "R5, L5, R5, R3".splitlines()
+TEST4 = "R8, R4, R4, R8".splitlines()
 
 
 def main() -> None:
@@ -44,9 +58,7 @@ def main() -> None:
     assert part_1(TEST1) == 5
     assert part_1(TEST2) == 2
     assert part_1(TEST3) == 12
-    assert part_2(TEST1) == 0
-    assert part_2(TEST2) == 0
-    assert part_2(TEST3) == 0
+    assert part_2(TEST4) == 4
 
     inputs = my_aocd.get_input(2016, 1, 1)
     result1 = part_1(inputs)
