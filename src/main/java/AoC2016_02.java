460ce622f638b4dcb1364673efd68b5f3a2f7a99
pareronia <49491686+pareronia@users.noreply.github.com>
2021-06-18 14:26:48 +0200

    AoC 2016 Day 2 Part 2 - java

--- src/main/java/AoC2016_02.java
+++ src/main/java/AoC2016_02.java
@@ -1,8 +1,11 @@
 import static com.github.pareronia.aoc.Utils.toAString;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 import com.github.pareronia.aoc.Utils;
@@ -14,16 +17,33 @@ import com.github.pareronia.aocd.Aocd;
 public class AoC2016_02 extends AoCBase {
     
     private static final Map<Position, Character> KEYPAD1 = Map.of(
-            Position.of(0, 0), '7',
-            Position.of(1, 0), '8',
-            Position.of(2, 0), '9',
-            Position.of(0, 1), '4',
-            Position.of(1, 1), '5',
-            Position.of(2, 1), '6',
-            Position.of(0, 2), '1',
-            Position.of(1, 2), '2',
-            Position.of(2, 2), '3'
+            Position.of(-1, 1), '1',
+            Position.of(0, 1), '2',
+            Position.of(1, 1), '3',
+            Position.of(-1, 0), '4',
+            Position.of(0, 0), '5',
+            Position.of(1, 0), '6',
+            Position.of(-1, -1), '7',
+            Position.of(0, -1), '8',
+            Position.of(1, -1), '9'
     );
+    @SuppressWarnings("serial")
+    private static final Map<Position, Character> KEYPAD2
+        = Collections.unmodifiableMap(new HashMap<>() {{
+            put(Position.of(2, 2), '1');
+            put(Position.of(1, 1), '2');
+            put(Position.of(2, 1), '3');
+            put(Position.of(3, 1), '4');
+            put(Position.of(0, 0), '5');
+            put(Position.of(1, 0), '6');
+            put(Position.of(2, 0), '7');
+            put(Position.of(3, 0), '8');
+            put(Position.of(4, 0), '9');
+            put(Position.of(1, -1), 'A');
+            put(Position.of(2, -1), 'B');
+            put(Position.of(3, -1), 'C');
+            put(Position.of(2, -2), 'D');
+    }});
 
     private final List<String> inputs;
 
@@ -60,27 +80,39 @@ public class AoC2016_02 extends AoCBase {
         return nav;
     }
 
-    @Override
-    public Integer solvePart1() {
+    private String solve(
+            Predicate<Position> inBounds,
+            Function<Position, Character> get
+        ) {
         final List<Character> code = new ArrayList<>();
-        Position start = Position.of(1,  1);
+        Position start = Position.of(0,  0);
         for (final String ins : this.inputs) {
-            final Position last
-                    = navigate(ins, start, pos -> KEYPAD1.keySet().contains(pos))
-                        .getPosition();
-            code.add(KEYPAD1.get(last));
+            final Position last = navigate(ins, start, inBounds).getPosition();
+            code.add(get.apply(last));
             start = last;
         }
-        return Integer.valueOf(code.stream().collect(toAString()));
+        return code.stream().collect(toAString());
     }
 
     @Override
-    public Integer solvePart2() {
-        return 0;
+    public String solvePart1() {
+        return solve(
+                pos -> KEYPAD1.keySet().contains(pos),
+                pos -> KEYPAD1.get(pos)
+        );
+    }
+    
+    @Override
+    public String solvePart2() {
+        return solve(
+                pos -> KEYPAD2.keySet().contains(pos),
+                pos -> KEYPAD2.get(pos)
+        );
     }
 
     public static void main(String[] args) throws Exception {
-        assert AoC2016_02.createDebug(TEST).solvePart1() == 1985;
+        assert AoC2016_02.createDebug(TEST).solvePart1().equals("1985");
+        assert AoC2016_02.createDebug(TEST).solvePart2().equals("5DB3");
 
         final List<String> input = Aocd.getData(2016, 2);
         lap("Part 1", () -> AoC2016_02.create(input).solvePart1());
