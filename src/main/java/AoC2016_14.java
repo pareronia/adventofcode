d9c237b96204675e0077b7c47a18e1d75e44dd50
pareronia <49491686+pareronia@users.noreply.github.com>
2021-06-19 22:47:23 +0200

    AoC 2016 Day 14 Part 2 - java

--- src/main/java/AoC2016_14.java
+++ src/main/java/AoC2016_14.java
@@ -32,33 +32,34 @@ public class AoC2016_14 extends AoCBase {
 		return new AoC2016_14(input, true);
 	}
 	
-	private String calculateMD5(String salt, Integer index) {
-	    final String toHash = salt + String.valueOf(index);
-	    return DigestUtils.md5Hex(toHash);
+	private String calculateMD5(String salt, Integer stretch, Integer index) {
+	    String toHash = salt + String.valueOf(index);
+	    for (int i = 0; i < 1 + stretch; i++) {
+	        toHash = DigestUtils.md5Hex(toHash);
+	    }
+	    return toHash;
 	}
 	
-	private String getMD5(String salt, Integer index) {
-	   return this.table.computeIfAbsent(index, i -> calculateMD5(salt, i));
+	private String getMD5(String salt, Integer stretch, Integer index) {
+	   return this.table.computeIfAbsent(index, i -> calculateMD5(salt, stretch, i));
 	}
 	
 	private Optional<String> findTriplet(String string) {
-	    final Matcher matcher = TRIPLET.matcher(string);
-	    if (matcher.find()) {
-	        return Optional.of(matcher.group(1));
-	    }
-	    return Optional.empty();
+	    return Optional.of(string)
+	            .map(TRIPLET::matcher)
+	            .filter(Matcher::find)
+	            .map(m -> m.group(1));
 	}
-	
-	@Override
-	public Integer solvePart1() {
-	    final MutableInt index = new MutableInt(0);
+
+    private Integer solve(final int stretch) {
+        final MutableInt index = new MutableInt(-1);
 	    final MutableInt cnt = new MutableInt(0);
 	    while (cnt.getValue() < 64) {
 	        final int idx = index.incrementAndGet();
-	        findTriplet(getMD5(this.salt, idx)).ifPresent(triplet -> {
+	        findTriplet(getMD5(this.salt, stretch, idx)).ifPresent(triplet -> {
 	            final String quintet = StringUtils.repeat(triplet.charAt(0), 5);
 	            for (int i = idx + 1; i < idx + 1001; i++) {
-	                if (getMD5(this.salt, i).contains(quintet)) {
+	                if (getMD5(this.salt, stretch, i).contains(quintet)) {
 	                    cnt.increment();
 	                    break;
 	                }
@@ -66,15 +67,21 @@ public class AoC2016_14 extends AoCBase {
 	        });
 	    }
 	    return index.getValue();
-	}
+    }
+    
+    @Override
+    public Integer solvePart1() {
+        return solve(0);
+    }
 	
 	@Override
 	public Integer solvePart2() {
-	    return 0;
+	    return solve(2016);
 	}
 
 	public static void main(String[] args) throws Exception {
 		assert AoC2016_14.createDebug(TEST).solvePart1() == 22728;
+		assert AoC2016_14.createDebug(TEST).solvePart2() == 22551;
 		
 		final List<String> input = Aocd.getData(2016, 14);
 		lap("Part 1", () -> AoC2016_14.create(input).solvePart1());
