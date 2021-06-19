613f52ee10e6ea72dbda8255dc2d5951e5e7e128
pareronia <49491686+pareronia@users.noreply.github.com>
2021-06-19 18:23:31 +0200

    AoC 2016 Day 9 Part 2 - java

--- src/main/java/AoC2016_09.java
+++ src/main/java/AoC2016_09.java
@@ -1,3 +1,6 @@
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
 import java.util.List;
 
 import com.github.pareronia.aocd.Aocd;
@@ -20,23 +23,29 @@ public class AoC2016_09 extends AoCBase {
 		return new AoC2016_09(input, true);
 	}
 	
-	private Integer decompressedLength(String input) {
-	    int cnt = 0;
-	    int skip = 0;
+	private Long decompressedLength(String input, boolean recursive) {
+	    if (!input.contains("(")) {
+	        return (long) input.length();
+	    }
+
+	    long cnt = 0;
 	    boolean inMarker = false;
 	    String marker = "";
         for (int i = 0; i < input.length(); i++) {
-            if (skip > 0 ) {
-                skip--;
-                continue;
-            }
             final char c = input.charAt(i);
             if (c == '(') {
                 inMarker = true;
             } else if (c == ')') {
                 final String[] splits = marker.split("x");
-                skip = Integer.valueOf(splits[0]);
-                cnt += skip * Integer.valueOf(splits[1]);
+                final Long skip = Long.valueOf(splits[0]);
+                final Long repeat = Long.valueOf(splits[1]);
+                if (recursive) {
+                    final String skipped = input.substring(i + 1, i + 1 + skip.intValue());
+                    cnt += decompressedLength(skipped, true) * repeat;
+                } else {
+                    cnt += skip * repeat;
+                }
+                i += skip;
                 marker = "";
                 inMarker = false;
             } else {
@@ -51,13 +60,13 @@ public class AoC2016_09 extends AoCBase {
 	}
 
 	@Override
-	public Integer solvePart1() {
-	    return decompressedLength(this.input);
+	public Long solvePart1() {
+	    return decompressedLength(this.input, FALSE);
 	}
 	
 	@Override
-	public Integer solvePart2() {
-		return 0;
+	public Long solvePart2() {
+	    return decompressedLength(this.input, TRUE);
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -67,6 +76,10 @@ public class AoC2016_09 extends AoCBase {
 		assert AoC2016_09.createDebug(TEST4).solvePart1() == 11;
 		assert AoC2016_09.createDebug(TEST5).solvePart1() == 6;
 		assert AoC2016_09.createDebug(TEST6).solvePart1() == 18;
+		assert AoC2016_09.createDebug(TEST3).solvePart2() == 9;
+		assert AoC2016_09.createDebug(TEST6).solvePart2() == 20;
+		assert AoC2016_09.createDebug(TEST7).solvePart2() == 241920;
+		assert AoC2016_09.createDebug(TEST8).solvePart2() == 445;
 		
 		final List<String> input = Aocd.getData(2016, 9);
 		lap("Part 1", () -> AoC2016_09.create(input).solvePart1());
@@ -79,4 +92,8 @@ public class AoC2016_09 extends AoCBase {
 	private static final List<String> TEST4 = splitLines("A(2x2)BCD(2x2)EFG");
 	private static final List<String> TEST5 = splitLines("(6x1)(1x3)A");
 	private static final List<String> TEST6 = splitLines("X(8x2)(3x3)ABCY");
+	private static final List<String> TEST7
+	                        = splitLines("(27x12)(20x12)(13x14)(7x10)(1x12)A");
+	private static final List<String> TEST8
+	    = splitLines("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN");
 }
