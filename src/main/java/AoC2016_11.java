692f7b9964cdd1ed0eda81ace02c1e928dd5dd48
pareronia <49491686+pareronia@users.noreply.github.com>
2021-07-01 10:09:16 +0200

    AoC 2016 Day 11 Part 2 - java

--- src/main/java/AoC2016_11.java
+++ src/main/java/AoC2016_11.java
@@ -1,16 +1,18 @@
 import static java.util.Collections.emptyList;
+import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
 import static org.apache.commons.collections4.ListUtils.intersection;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.PriorityQueue;
 import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
@@ -20,8 +22,8 @@ import org.eclipse.collections.impl.tuple.Tuples;
 
 import com.github.pareronia.aocd.Aocd;
 
-import lombok.EqualsAndHashCode;
-import lombok.RequiredArgsConstructor;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.ToString;
 import lombok.Value;
 import lombok.With;
@@ -69,62 +71,68 @@ public final class AoC2016_11 extends AoCBase {
         return new AoC2016_11(input, true);
     }
     
-    @Override
-    public Integer solvePart1() {
-        log(() -> "Initial state: " + this.initialState);
-        this.initialState.moves().forEach(this::log);
+    private Integer scoreStep(final Pair<Integer, State> p) {
+        final Integer numberOfSteps = p.getOne();
+        final State state = p.getTwo();
+        return -state.getDiff() * numberOfSteps;
+    }
+    
+    private Integer solve(final State initialState) {
         Integer numberOfSteps = 0;
-        State complete = null;
-        final Deque<Pair<Integer, State>> steps = new ArrayDeque<>();
-        steps.add(Tuples.pair(0, this.initialState));
-        final Set<State> seen = new HashSet<>();
-        while (!steps.isEmpty() && steps.size() < 5_000_000) {
+        final PriorityQueue<Pair<Integer, State>> steps
+                = new PriorityQueue<>(comparing(this::scoreStep));
+        steps.add(Tuples.pair(0, initialState));
+        final Set<Pair<Integer, Map<Integer, Map<String, Integer>>>> seen = new HashSet<>();
+        seen.add(initialState.equivalentState());
+        int cnt = 0;
+        while (!steps.isEmpty()) {
             final Pair<Integer, State> step = steps.poll();
+            cnt++;
             final State state = step.getTwo();
-            if (state.getElevator() == 4
-                    && state.getChips().values().stream().allMatch(c -> c == 4)
-                    && state.getGennys().values().stream().allMatch(g -> g == 4)) {
+            if (state.isDestination()) {
                 numberOfSteps = step.getOne();
-                complete = state;
                 break;
             }
-            if (seen.contains(state)) {
-                continue;
-            }
-            seen.add(state);
-            final List<State> moves = state.moves();
-            final List<State> valid = moves.stream()
-                    .filter(m -> Set.of(1, 2, 3, 4).contains(m.getElevator()))
-                    .filter(State::isSafe)
-                    .collect(toList());
-            valid.forEach(m -> steps.add(Tuples.pair(step.getOne() + 1, m)));
+            state.moves().stream()
+                    .filter(m -> !seen.contains(m.equivalentState()))
+                    .forEach(m -> {
+                        seen.add(m.equivalentState());
+                        steps.add(Tuples.pair(step.getOne() + 1, m));
+                    });
         }
-        if (complete == null) {
-            throw new IllegalStateException("Unsolvable");
-        }
+        log(cnt);
         log("#steps: " + numberOfSteps);
-        log("Path:");
-        log(complete);
-        State cameFrom = complete.getCameFrom();
-        while (cameFrom != null) {
-            log(cameFrom);
-            cameFrom = cameFrom.getCameFrom();
-        }
-        log("==================");
         return numberOfSteps;
+    }
+
+    @Override
+    public Integer solvePart1() {
+        log("==================");
+        log(() -> "Initial state: " + this.initialState);
+        this.initialState.moves().forEach(this::log);
+        return solve(this.initialState);
     }
     
     @Override
     public Integer solvePart2() {
-        return 0;
+        final Map<String, Integer> chips = new HashMap<>(this.initialState.getChips());
+        chips.put("elerium", 1);
+        chips.put("dilithium", 1);
+        final Map<String, Integer> gennys = new HashMap<>(this.initialState.getGennys());
+        gennys.put("elerium", 1);
+        gennys.put("dilithium", 1);
+        final State newState = this.initialState.withChips(chips).withGennys(gennys);
+        log("==================");
+        log(() -> "Initial state: " + newState);
+        return solve(newState);
     }
 
     public static void main(final String[] args) throws Exception {
         assert AoC2016_11.createDebug(TEST).solvePart1() == 11;
 
         final List<String> input = Aocd.getData(2016, 11);
-        lap("Part 1", () -> AoC2016_11.create(input).solvePart1());
-        lap("Part 2", () -> AoC2016_11.create(input).solvePart2());
+        lap("Part 1", () -> AoC2016_11.createDebug(input).solvePart1());
+        lap("Part 2", () -> AoC2016_11.createDebug(input).solvePart2());
     }
     
     private static final List<String> TEST = splitLines(
@@ -135,33 +143,90 @@ public final class AoC2016_11 extends AoCBase {
     );
     
     @Value
-    @RequiredArgsConstructor
-    private static final class State {
+    static final class State {
+        @Getter(value = AccessLevel.PRIVATE)
         @With
         private final Integer elevator;
+        @Getter(value = AccessLevel.PRIVATE)
         @With
         private final Map<String, Integer> chips;
+        @Getter(value = AccessLevel.PRIVATE)
         @With
         private final Map<String, Integer> gennys;
-        @EqualsAndHashCode.Exclude
-        @ToString.Exclude
         @With
-        private final State cameFrom;
+        private final Integer diff;
+        @Getter(value = AccessLevel.PRIVATE)
+        @ToString.Exclude
+        private final Map<Integer, List<String>> chipsPerFloor;
+        @Getter(value = AccessLevel.PRIVATE)
+        @ToString.Exclude
+        private final Map<Integer, List<String>> gennysPerFloor;
+
+        private State(
+                final Integer elevator,
+                final Map<String, Integer> chips,
+                final Map<String, Integer> gennys,
+                final Integer diff,
+                final Map<Integer, List<String>> chipsPerFloor,
+                final Map<Integer, List<String>> gennysPerFloor
+                ) {
+            super();
+            this.elevator = elevator;
+            this.chips = chips;
+            this.gennys = gennys;
+            this.diff = diff;
+            this.chipsPerFloor = chips.keySet().stream().collect(groupingBy(chips::get));
+            this.gennysPerFloor = gennys.keySet().stream().collect(groupingBy(gennys::get));
+        }
+        
+        public State(
+                final Integer elevator,
+                final Map<String, Integer> chips,
+                final Map<String, Integer> gennys,
+                final Integer diff
+                ) {
+            this(   elevator,
+                    Collections.unmodifiableMap(chips),
+                    Collections.unmodifiableMap(gennys),
+                    diff, null, null);
+        }
 
         public State(
                 final Integer elevator,
                 final Map<String, Integer> chips,
                 final Map<String, Integer> gennys
                 ) {
-            this(elevator, chips, gennys, null);
+            this(elevator, chips, gennys, 0);
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final State other = (State) obj;
+            final boolean same = Objects.equals(elevator, other.elevator)
+                                    && Objects.equals(chips, other.chips)
+                                    && Objects.equals(gennys, other.gennys);
+            return same;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(chips, elevator, gennys);
         }
         
         @ToString.Include
-        public boolean isSafe() {
-            final Map<Integer, List<String>> gennysPerFloor = this.gennysPerFloor();
+        boolean isSafe() {
             for (final Entry<String, Integer> chip : this.chips.entrySet()) {
                 final List<String> gennysOnSameFloor
-                        = gennysPerFloor.get(chip.getValue());
+                        = this.gennysPerFloor.get(chip.getValue());
                 if (gennysOnSameFloor != null
                         && !gennysOnSameFloor.contains(chip.getKey())) {
                     return false;
@@ -169,13 +234,18 @@ public final class AoC2016_11 extends AoCBase {
             }
             return true;
         }
-
+        
+        public boolean isDestination() {
+            return this.elevator == 4
+                    && this.chips.values().stream().allMatch(c -> c == 4)
+                    && this.gennys.values().stream().allMatch(g -> g == 4);
+        }
+        
         public List<State> moves() {
             final List<State> states = new ArrayList<>();
-            final Map<Integer, List<String>> chipsPerFloor = this.chipsPerFloor();
             final Integer floor = this.elevator;
             final List<String> chipsOnFloor
-                    = chipsPerFloor.getOrDefault(floor, emptyList());
+                    = this.chipsPerFloor.getOrDefault(floor, emptyList());
             if (chipsOnFloor.size() >= 2) {
                 CombinatoricsUtils.combinationsIterator(chipsOnFloor.size(), 2)
                     .forEachRemaining(c -> {
@@ -183,20 +253,23 @@ public final class AoC2016_11 extends AoCBase {
                             = List.of(chipsOnFloor.get(c[0]),
                                       chipsOnFloor.get(c[1]));
                         states.add(withChipsTo(chipsToMove, floor + 1)
-                                    .withCameFrom(this));
-                        states.add(withChipsTo(chipsToMove, floor - 1)
-                                    .withCameFrom(this));
+                                    .withDiff(2));
+                        if (!floorsBelowEmpty(floor)) {
+                            states.add(withChipsTo(chipsToMove, floor - 1)
+                                    .withDiff(-2));
+                        }
                     });
             }
             for (final String chip : chipsOnFloor) {
                 states.add(withChipsTo(List.of(chip), floor + 1)
-                            .withCameFrom(this));
-                states.add(withChipsTo(List.of(chip), floor - 1)
-                            .withCameFrom(this));
+                            .withDiff(1));
+                if (!floorsBelowEmpty(floor)) {
+                    states.add(withChipsTo(List.of(chip), floor - 1)
+                            .withDiff(-1));
+                }
             }
-            final Map<Integer, List<String>> gennysPerFloor = this.gennysPerFloor();
             final List<String> gennysOnFloor
-                    = gennysPerFloor.getOrDefault(floor, emptyList());
+                    = this.gennysPerFloor.getOrDefault(floor, emptyList());
             if (gennysOnFloor.size() >= 2) {
                 CombinatoricsUtils.combinationsIterator(gennysOnFloor.size(), 2)
                     .forEachRemaining(c -> {
@@ -204,36 +277,53 @@ public final class AoC2016_11 extends AoCBase {
                             = List.of(gennysOnFloor.get(c[0]),
                                       gennysOnFloor.get(c[1]));
                         states.add(withGeneratorsTo(gennysToMove, floor + 1)
-                                    .withCameFrom(this));
-                        states.add(withGeneratorsTo(gennysToMove, floor - 1)
-                                    .withCameFrom(this));
+                                    .withDiff(2));
+                        if (!floorsBelowEmpty(floor)) {
+                            states.add(withGeneratorsTo(gennysToMove, floor - 1)
+                                    .withDiff(-2));
+                        }
                     });
             }
             for (final String genny : gennysOnFloor) {
                 states.add(withGeneratorsTo(List.of(genny), floor + 1)
-                            .withCameFrom(this));
-                states.add(withGeneratorsTo(List.of(genny), floor - 1)
-                            .withCameFrom(this));
+                            .withDiff(1));
+                if (!floorsBelowEmpty(floor)) {
+                    states.add(withGeneratorsTo(List.of(genny), floor - 1)
+                            .withDiff(-1));
+                }
             }
             for (final String match : intersection(chipsOnFloor, gennysOnFloor)) {
                 states.add(withChipsTo(List.of(match), floor + 1)
                         .withGeneratorsTo(List.of(match), floor + 1)
-                        .withCameFrom(this));
-                states.add(withChipsTo(List.of(match), floor - 1)
-                        .withGeneratorsTo(List.of(match), floor - 1)
-                        .withCameFrom(this));
+                        .withDiff(2));
+                if (!floorsBelowEmpty(floor)) {
+                    states.add(withChipsTo(List.of(match), floor - 1)
+                            .withGeneratorsTo(List.of(match), floor - 1)
+                            .withDiff(-2));
+                }
             }
-            return states;
+            return states.stream()
+                    .filter(s -> Set.of(1, 2, 3, 4).contains(s.elevator))
+                    .filter(State::isSafe)
+                    .collect(toList());
         }
         
-        private Map<Integer, List<String>> chipsPerFloor() {
-            return this.chips.keySet().stream()
-                    .collect(groupingBy(this.chips::get));
+        public Pair<Integer, Map<Integer, Map<String, Integer>>> equivalentState() {
+            assert this.isSafe();
+            return Tuples.pair(this.elevator, countOfType());
         }
         
-        private Map<Integer, List<String>> gennysPerFloor() {
-            return this.gennys.keySet().stream()
-                    .collect(groupingBy(this.gennys::get));
+        private Map<Integer, Map<String, Integer>> countOfType() {
+            final Map<Integer, Map<String, Integer>> counts = new HashMap<>();
+            for (final String chip : this.chips.keySet()) {
+                counts.computeIfAbsent(this.chips.get(chip), HashMap::new)
+                        .merge("chip", 1, Integer::sum);
+            }
+            for (final String genny : this.gennys.keySet()) {
+                counts.computeIfAbsent(this.gennys.get(genny), HashMap::new)
+                        .merge("genny", 1, Integer::sum);
+            }
+            return counts;
         }
         
         private State withChipsTo(final List<String> chips, final Integer floor) {
@@ -246,6 +336,18 @@ public final class AoC2016_11 extends AoCBase {
             final Map<String, Integer> newGenerators = new HashMap<>(this.gennys);
             generators.forEach(g -> newGenerators.put(g, floor));
             return this.withGennys(newGenerators).withElevator(floor);
+        }
+        
+        private boolean floorsBelowEmpty(final Integer floor) {
+            for (int f = floor; f > 1; f--) {
+                if (this.chipsPerFloor.get(f - 1) != null) {
+                    return false;
+                }
+                if (this.gennysPerFloor.get(f - 1) != null) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
---
+++ src/test/java/AoC2016_11TestCase.java
@@ -0,0 +1,56 @@
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class AoC2016_11TestCase {
+
+    @Test
+    public void isSafe() {
+        final AoC2016_11.State state1 = new AoC2016_11.State(1, Map.of("L", 1, "H", 1), Map.of("L", 3, "H", 2));
+        final AoC2016_11.State state2 = new AoC2016_11.State(1, Map.of("L", 1, "H", 3), Map.of("L", 3, "H", 2));
+        
+        assertThat(state1.isSafe(), is(TRUE));
+        assertThat(state2.isSafe(), is(FALSE));
+    }
+    
+    @Test
+    public void moves() {
+        final AoC2016_11.State state = new AoC2016_11.State(1, Map.of("L", 1, "H", 1), Map.of("L", 3, "H", 2));
+        
+        final List<AoC2016_11.State> moves = state.moves();
+        
+        assertThat(moves, containsInAnyOrder(
+            new AoC2016_11.State(2, Map.of("L", 1, "H", 2), Map.of("L", 3, "H", 2))
+        ));
+    }
+    
+    @Test
+    public void dontMoveTowardsEmptiedFloors() {
+        final AoC2016_11.State state = new AoC2016_11.State(2, Map.of(), Map.of("A", 2));
+        
+        final List<AoC2016_11.State> moves = state.moves();
+        
+        assertThat(moves, contains(
+            new AoC2016_11.State(3, Map.of(), Map.of("A", 3))
+        ));
+    }
+    
+    @Test
+    public void countOfType() {
+        final AoC2016_11.State state1 = new AoC2016_11.State(2, Map.of("A", 1, "B", 2), Map.of("A", 1, "B", 2));
+        final AoC2016_11.State state2 = new AoC2016_11.State(2, Map.of("A", 1, "B", 3), Map.of("A", 1, "B", 2));
+        
+        assertThat(state1.equivalentState().getTwo(),
+                is(Map.of(1, Map.of("chip", 1, "genny", 1), 2, Map.of("chip", 1, "genny", 1))));
+        assertThat(state2.equivalentState().getTwo(),
+                is(Map.of(1, Map.of("chip", 1, "genny", 1), 2, Map.of("genny", 1), 3, Map.of("chip", 1))));
+    }
+}
