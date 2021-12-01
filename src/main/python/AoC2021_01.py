21b34e4ad8af4fed2c84a765ada9ed9e99fdaaab
pareronia <49491686+pareronia@users.noreply.github.com>
2021-12-01 06:18:07 +0100

    AoC 2021 Day 1 Part 1

---
+++ src/main/python/AoC2021_01.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+#
+# Advent of Code 2021 Day 1
+#
+
+from aoc import my_aocd
+
+
+def part_1(inputs: tuple[str]) -> int:
+    count = 0
+    prev = int(inputs[0])
+    for i in range(1, len(inputs)):
+        curr = int(inputs[i])
+        if curr > prev:
+            count += 1
+        prev = curr
+    return count
+
+
+def part_2(inputs: tuple[str]) -> int:
+    return 0
+
+
+TEST = """\
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+""".splitlines()
+
+
+def main() -> None:
+    my_aocd.print_header(2021, 1)
+
+    assert part_1(TEST) == 7
+    assert part_2(TEST) == 0
+
+    inputs = my_aocd.get_input(2021, 1, 2000)
+    result1 = part_1(inputs)
+    print(f"Part 1: {result1}")
+    result2 = part_2(inputs)
+    print(f"Part 2: {result2}")
+
+
+if __name__ == '__main__':
+    main()
