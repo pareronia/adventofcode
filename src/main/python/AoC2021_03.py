e56624cd6afb7df6e431c89136eef7b4bb375747
pareronia <49491686+pareronia@users.noreply.github.com>
2021-12-03 12:01:46 +0100

    AoC 2021 Day 3 - cleanup

--- src/main/python/AoC2021_03.py
+++ src/main/python/AoC2021_03.py
@@ -3,67 +3,53 @@
 # Advent of Code 2021 Day 3
 #
 
+from typing import NamedTuple
 from aoc import my_aocd
-from aoc.common import log
 
 
-def _to_dec(s: str) -> int:
-    return int(s, 2)
+class BitCount(NamedTuple):
+    ones: int
+    zeroes: int
+
+    def most_common(self) -> str:
+        return '1' if self.ones >= self.zeroes else '0'
+
+    def least_common(self) -> str:
+        return '1' if self.ones < self.zeroes else '0'
 
 
-def _mcb(inputs: list[str], pos: int) -> tuple[int, int]:
-    cnt0 = 0
-    cnt1 = 0
-    for b in inputs:
-        if b[pos] == '0':
-            cnt0 += 1
-        else:
-            cnt1 += 1
-    return cnt0, cnt1
+def _ans(value1: str, value2: str) -> int:
+    return int(value1, 2) * int(value2, 2)
+
+
+def _bit_counts(strings: list[str], pos: int) -> BitCount:
+    zeroes = sum(s[pos] == '0' for s in strings)
+    return BitCount(len(strings) - zeroes, zeroes)
 
 
 def part_1(inputs: tuple[str]) -> int:
     gamma = ""
     epsilon = ""
     for i in range(len(inputs[0])):
-        cnt0, cnt1 = _mcb(inputs, i)
-        if cnt0 > cnt1:
-            gamma += '0'
-            epsilon += '1'
-        else:
-            gamma += '1'
-            epsilon += '0'
-    return _to_dec(gamma) * _to_dec(epsilon)
+        bit_count = _bit_counts(inputs, i)
+        gamma += bit_count.most_common()
+        epsilon += bit_count.least_common()
+    return _ans(gamma, epsilon)
 
 
-def _filter(inputs: list[str], bit: str, pos: int) -> list[str]:
-    return [s for s in inputs if s[pos] == bit]
+def _reduce(strings: list[str], keep) -> str:
+    pos = 0
+    while len(strings) > 1:
+        to_keep = keep(_bit_counts(strings, pos))
+        strings = [s for s in strings if s[pos] == to_keep]
+        pos += 1
+    return strings[0]
 
 
 def part_2(inputs: tuple[str]) -> int:
-    sso2 = list(inputs)
-    io2 = 0
-    while len(sso2) > 1:
-        cnt0, cnt1 = _mcb(sso2, io2)
-        if cnt0 > cnt1:
-            sso2 = _filter(sso2, '0', io2)
-        else:
-            sso2 = _filter(sso2, '1', io2)
-        io2 += 1
-    oxygen = _to_dec(sso2[0])
-    log(oxygen)
-    ssco2 = list(inputs)
-    ico2 = 0
-    while len(ssco2) > 1:
-        cnt0, cnt1 = _mcb(ssco2, ico2)
-        if cnt0 > cnt1:
-            ssco2 = _filter(ssco2, '1', ico2)
-        else:
-            ssco2 = _filter(ssco2, '0', ico2)
-        ico2 += 1
-    co2 = _to_dec(ssco2[0])
-    log(co2)
-    return oxygen * co2
+    o2 = _reduce(list(inputs), lambda x: x.most_common())
+    co2 = _reduce(list(inputs), lambda x: x.least_common())
+    return _ans(o2, co2)
 
 
 TEST = """\
