f0be4714a358912781b163556632b3d3ef9bf44d
pareronia <49491686+pareronia@users.noreply.github.com>
2021-12-11 21:42:54 +0100

    AoC 2021 Day 9 - java - cleanup

--- src/main/java/AoC2021_09.java
+++ src/main/java/AoC2021_09.java
@@ -1,5 +1,4 @@
 import static java.util.stream.Collectors.summingInt;
-import static java.util.stream.Collectors.toList;
 
 import java.util.ArrayDeque;
 import java.util.Comparator;
@@ -8,36 +7,22 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
-import java.util.stream.Stream.Builder;
 
 import org.apache.commons.lang3.mutable.MutableInt;
 
-import com.github.pareronia.aoc.StringOps;
+import com.github.pareronia.aoc.IntGrid;
+import com.github.pareronia.aoc.IntGrid.Cell;
+import com.github.pareronia.aoc.navigation.Headings;
 import com.github.pareronia.aocd.Aocd;
-
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
+import com.github.pareronia.aocd.Puzzle;
 
 public class AoC2021_09 extends AoCBase {
     
-    private static final Cell N = Cell.at(-1, 0);
-    private static final Cell E = Cell.at(0, 1);
-    private static final Cell S = Cell.at(1, 0);
-    private static final Cell W = Cell.at(0, -1);
-    private static final Set<Cell> NESW = Set.of(N, E, S, W);
-
-    private final Grid grid;
+    private final IntGrid grid;
     
     private AoC2021_09(final List<String> input, final boolean debug) {
         super(debug);
-        final int[][] heights = new int[input.size()][input.get(0).length()];
-        input.stream()
-            .map(s -> StringOps.getDigitsPrimitive(s, s.length()))
-            .collect(toList())
-            .toArray(heights);
-        this.grid = new Grid(heights);
+        this.grid = IntGrid.from(input);
     }
     
     public static final AoC2021_09 create(final List<String> input) {
@@ -49,12 +34,12 @@ public class AoC2021_09 extends AoCBase {
     }
     
     private Stream<Cell> findNeighbours(final Cell c) {
-        return NESW.stream()
-            .filter(n -> c.getRow() + n.getRow() >= 0)
-            .filter(n -> c.getRow() + n.getRow() < this.grid.getHeight())
-            .filter(n -> c.getCol() + n.getCol() >= 0)
-            .filter(n -> c.getCol() + n.getCol() < this.grid.getWidth())
-            .map(n -> Cell.at(c.getRow() + n.getRow(), c.getCol() + n.getCol()));
+        return Headings.CAPITAL.stream()
+            .filter(n -> c.getRow() + n.getX() >= 0)
+            .filter(n -> c.getRow() + n.getX() < this.grid.getHeight())
+            .filter(n -> c.getCol() + n.getY() >= 0)
+            .filter(n -> c.getCol() + n.getY() < this.grid.getWidth())
+            .map(n -> Cell.at(c.getRow() + n.getX(), c.getCol() + n.getY()));
     }
 
     private Stream<Cell> findLows() {
@@ -103,9 +88,11 @@ public class AoC2021_09 extends AoCBase {
         assert AoC2021_09.create(TEST).solvePart1() == 15;
         assert AoC2021_09.create(TEST).solvePart2() == 1134;
 
-        final List<String> input = Aocd.getData(2021, 9);
-        lap("Part 1", () -> AoC2021_09.create(input).solvePart1());
-        lap("Part 2", () -> AoC2021_09.create(input).solvePart2());
+        final Puzzle puzzle = Aocd.puzzle(2021, 9);
+        puzzle.check(
+            () -> lap("Part 1", () -> AoC2021_09.create(puzzle.getInputData()).solvePart1()),
+            () -> lap("Part 2", () -> AoC2021_09.create(puzzle.getInputData()).solvePart2())
+        );
     }
 
     private static final List<String> TEST = splitLines(
@@ -115,41 +102,4 @@ public class AoC2021_09 extends AoCBase {
         "8767896789\r\n" +
         "9899965678"
     );
-    
-    @RequiredArgsConstructor(staticName = "at")
-    @Getter
-    @EqualsAndHashCode
-    @ToString
-    private static final class Cell {
-        private final int row;
-        private final int col;
-    }
-    
-    @RequiredArgsConstructor
-    private static final class Grid {
-        private final int[][] heights;
-        
-        public int getWidth() {
-            assert this.heights.length > 0;
-            return this.heights[0].length;
-        }
-        
-        public int getHeight() {
-            return this.heights.length;
-        }
-        
-        public int getValue(final Cell c) {
-            return this.heights[c.getRow()][c.getCol()];
-        }
-        
-        public Stream<Cell> getCells() {
-            final Builder<Cell> builder = Stream.builder();
-            for (int r = 0; r < this.getHeight(); r++) {
-                for (int c = 0; c < getWidth(); c++) {
-                    builder.add(Cell.at(r, c));
-                }
-            }
-            return builder.build();
-        }
-    }
 }
