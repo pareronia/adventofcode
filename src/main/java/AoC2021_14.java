49e4dde79673e78d9cb81f17c52cfb781f17227b
pareronia <49491686+pareronia@users.noreply.github.com>
2021-12-14 12:38:52 +0100

    AoC 2021 Day 14 - java - cleanup

--- src/main/java/AoC2021_14.java
+++ src/main/java/AoC2021_14.java
@@ -1,41 +1,32 @@
-import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.summarizingLong;
-import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.LongSummaryStatistics;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
+import java.util.Map.Entry;
+import java.util.Objects;
 
-import com.github.pareronia.aoc.Utils;
 import com.github.pareronia.aocd.Aocd;
 import com.github.pareronia.aocd.Puzzle;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 public class AoC2021_14 extends AoCBase {
     
-    private final String template;
-    private final Map<String, String> rules;
+    private final char[] template;
+    private final Map<CharacterPair, Character> rules;
 
     private AoC2021_14(final List<String> input, final boolean debug) {
         super(debug);
-        this.template = input.get(0);
+        this.template = input.get(0).toCharArray();
         this.rules = input.subList(2, input.size()).stream()
             .map(s -> s.split(" -> "))
-            .collect(toMap(sp -> sp[0], sp -> sp[1]));
-        log(template);
-        log(rules);
+            .collect(toMap(sp -> CharacterPair.from(sp[0]), sp -> sp[1].charAt(0)));
     }
     
     public static final AoC2021_14 create(final List<String> input) {
@@ -47,72 +38,49 @@ public class AoC2021_14 extends AoCBase {
     }
 
     private Long solve(final int cycles) {
-        final Polymer polymer = Polymer.create(this.template.toCharArray());
-//        polymer.elements().forEach(this::log);
-        for (int i = 0; i < cycles; i++) {
-            polymer.grow(this.rules);
-        }
-        final Map<Character, Long> counters = polymer.elements()
-            .collect(groupingBy(e -> e.getName(), HashMap::new, counting()));
-//        log(counters);
-        final LongSummaryStatistics summary = counters.values().stream()
-                .collect(summarizingLong(Long::valueOf));
-        final long ans = summary.getMax() - summary.getMin();
-        log(ans);
-        return ans;
-    }
-    
-    private Long solveBis(final int cycles) {
-        Map<String, Long> pairCounters = new HashMap<>();
-        for (int i = 1; i < this.template.length(); i++) {
-            final String pair = String.valueOf(new char[] {
-                    this.template.toCharArray()[i - 1],
-                    this.template.toCharArray()[i]});
+        Map<CharacterPair, Long> pairCounters = new HashMap<>();
+        final Map<Character, Long> elemCounters = new HashMap<>();
+        for (int i = 0; i < this.template.length; i++) {
+            elemCounters.merge(this.template[i], 1L, Long::sum);
+            if (i == 0) {
+                continue;
+            }
+            final CharacterPair pair
+                    = new CharacterPair(this.template[i - 1], this.template[i]);
             pairCounters.merge(pair, 1L, Long::sum);
         }
-        log(pairCounters);
-        final Map<Character, Long> elemCounters = new HashMap<>();
-        for (int i = 0; i < this.template.length(); i++) {
-            elemCounters.merge(this.template.charAt(i), 1L, Long::sum);
-        }
-        log(elemCounters);
+        
         for (int i = 0; i < cycles; i++) {
-            final Set<String> keys = new HashSet<>(pairCounters.keySet());
-            final Map<String, Long> pairCounters2 = new HashMap<>();
-            for (final String k : keys) {
-                final String elem = rules.get(k);
-                final Long count = pairCounters.get(k);
-                elemCounters.merge(elem.charAt(0), count, Long::sum);
-                pairCounters2.merge(k.substring(0, 1) + elem, count, Long::sum);
-                pairCounters2.merge(elem + k.substring(1), count, Long::sum);
+            final Map<CharacterPair, Long> pairCounters2 = new HashMap<>();
+            for (final Entry<CharacterPair, Long> e : pairCounters.entrySet()) {
+                final CharacterPair pair = e.getKey();
+                final Character elem = rules.get(pair);
+                final Long count = e.getValue();
+                elemCounters.merge(elem, count, Long::sum);
+                pairCounters2.merge(new CharacterPair(pair.getLeft(), elem), count, Long::sum);
+                pairCounters2.merge(new CharacterPair(elem, pair.getRight()), count, Long::sum);
             }
             pairCounters = pairCounters2;
-            log(pairCounters);
-            log(elemCounters);
         }
+        
         final LongSummaryStatistics summary = elemCounters.values().stream()
                 .collect(summarizingLong(Long::valueOf));
-        final long ans = summary.getMax() - summary.getMin();
-        log(ans);
-        return ans;
+        return summary.getMax() - summary.getMin();
     }
     
     @Override
     public Long solvePart1() {
-        final long ans = solve(10);
-        final long ansBis = solveBis(10);
-        assert ans == ansBis;
-        return ansBis;
+        return solve(10);
     }
     
     @Override
     public Long solvePart2() {
-        return solveBis(40);
+        return solve(40);
     }
 
     public static void main(final String[] args) throws Exception {
-        assert AoC2021_14.createDebug(TEST).solvePart1() == 1588;
-        assert AoC2021_14.createDebug(TEST).solvePart2() == 2_188_189_693_529L;
+        assert AoC2021_14.create(TEST).solvePart1() == 1588;
+        assert AoC2021_14.create(TEST).solvePart2() == 2_188_189_693_529L;
 
         final Puzzle puzzle = Aocd.puzzle(2021, 14);
         puzzle.check(
@@ -144,68 +112,17 @@ public class AoC2021_14 extends AoCBase {
     
     @RequiredArgsConstructor
     @Getter
+    @EqualsAndHashCode
     @ToString
-    private static final class Element {
-        private final char name;
-        @ToString.Exclude
-        private Element next;
-    }
-    
-    @RequiredArgsConstructor
-    @Setter
-    @Getter
-    private static final class Polymer implements Iterable<Element> {
-        private Element head;
+    private static final class CharacterPair {
+        private final Character left;
+        private final Character right;
         
-        public static Polymer create(final char[] elements) {
-            final List<Element> list = Utils.asCharacterStream(String.valueOf(elements))
-                .map(Element::new)
-                .collect(toList());
-            for (int i = 0; i < list.size() - 1; i++) {
-                list.get(i).next = list.get(i + 1);
+        public static CharacterPair from(final String string) {
+            if (Objects.requireNonNull(string).length() != 2) {
+                throw new IllegalArgumentException();
             }
-            final Polymer polymer = new Polymer();
-            polymer.setHead(list.get(0));
-            return polymer;
-        }
-        
-        public void grow(final Map<String, String> rules) {
-            Element curr = this.head;
-            while (curr != null && curr.next != null) {
-                final char[] key = new char[2];
-                key[0] = curr.name;
-                key[1] = curr.next.name;
-                final String keyString = String.valueOf(key);
-                final String string = rules.get(keyString);
-                final Element toAdd = new Element(string.charAt(0));
-                toAdd.next = curr.next;
-                curr.next = toAdd;
-                curr = curr.next.next;
-            }
-        }
-        
-        @Override
-        public Iterator<Element> iterator() {
-            return new Iterator<>() {
-                private Element curr = Polymer.this.getHead();
-                
-                @Override
-                public Element next() {
-                    final Element ans = curr;
-                    curr = curr.next;
-                    return ans;
-                }
-                
-                @Override
-                public boolean hasNext() {
-                    return curr != null;
-                }
-            };
-        }
-        
-        public Stream<Element> elements() {
-            final Iterable<Element> iterable = () -> this.iterator();
-            return StreamSupport.stream(iterable.spliterator(), false);
+            return new CharacterPair(string.charAt(0), string.charAt(1));
         }
     }
 }
