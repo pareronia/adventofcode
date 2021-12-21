f1f0ba88f3a1229e650dcc8ee6dde6b46a6e274a
pareronia <49491686+pareronia@users.noreply.github.com>
2021-12-21 22:07:05 +0100

    AoC 2021 Day 21 Part 2 - java

--- src/main/java/AoC2021_21.java
+++ src/main/java/AoC2021_21.java
@@ -1,10 +1,18 @@
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.apache.commons.lang3.StringUtils;
+import org.eclipse.collections.api.tuple.Pair;
+import org.eclipse.collections.impl.tuple.Tuples;
 
 import com.github.pareronia.aocd.Aocd;
 import com.github.pareronia.aocd.Puzzle;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+// TODO: add iterative verson
 public class AoC2021_21 extends AoCBase {
     
     private final int p1;
@@ -15,8 +23,6 @@ public class AoC2021_21 extends AoCBase {
         assert input.size() == 2;
         this.p1 = Character.digit(input.get(0).charAt(input.get(0).length() - 1), 10);
         this.p2 = Character.digit(input.get(1).charAt(input.get(1).length() - 1), 10);
-        log(p1);
-        log(p2);
     }
     
     public static final AoC2021_21 create(final List<String> input) {
@@ -27,51 +33,104 @@ public class AoC2021_21 extends AoCBase {
         return new AoC2021_21(input, true);
     }
     
-    @Override
-    public Integer solvePart1() {
-        int score1 = 0;
-        int cnt = 0;
-        int pos1 = this.p1 - 1;
-        int score2 = 0;
-        int pos2 = this.p2 - 1;
-        int die = 0;
-        while (true) {
-            final List<Integer> throw1 = List.of(1 + (die++ % 100), 1 + (die++ % 100), 1 + (die++ % 100));
-            String s1 = "Player 1 throws " + StringUtils.join(throw1, "+");
-            cnt += 3;
-            pos1 = (pos1 + throw1.get(0) + throw1.get(1) + throw1.get(2)) % 10;
-            score1 += pos1 + 1;
-            s1 += " and moves to space " + (pos1 + 1) + " for a total score of " + score1;
-            log(s1);
-            if (score1 >= 1000) {
-                break;
-            }
-            final List<Integer> throw2 = List.of(1 + (die++ % 100), 1 + (die++ % 100), 1 + (die++ % 100));
-            String s2 = "Player 2 throws " + StringUtils.join(throw2, "+");
-            cnt += 3;
-            pos2 = (pos2 + throw2.get(0) + throw2.get(1) + throw2.get(2)) % 10;
-            score2 += pos2 + 1;
-            s2 += " and moves to space " + (pos2 + 1) + " for a total score of " + score2;
-            log(s2);
-            if (score2 >= 1000) {
-                break;
-            }
+    @AllArgsConstructor
+    @EqualsAndHashCode
+    @ToString
+    private static final class Game {
+        private int pos1;
+        private int pos2;
+        private int score1;
+        private int score2;
+        private final int player;
+        
+        public static Game copyForPlayer1(final Game game) {
+            return new Game(game.pos1, game.pos2, game.score1, game.score2, 1);
         }
-        if (score1 < score2) {
-            return score1 * cnt;
-        } else {
-            return score2 * cnt;
+
+        public static Game copyForPlayer2(final Game game) {
+            return new Game(game.pos1, game.pos2, game.score1, game.score2, 2);
+        }
+        
+        public void turn1(final int[] rolls) {
+            for (final int roll : rolls) {
+                pos1 = (pos1 + roll) % 10;
+            }
+            score1 += pos1 + 1;
+        }
+
+        public void turn2(final int[] rolls) {
+            for (final int roll : rolls) {
+                pos2 = (pos2 + roll) % 10;
+            }
+            score2 += pos2 + 1;
         }
     }
     
     @Override
-    public Integer solvePart2() {
-        return null;
+    public Integer solvePart1() {
+        final Game game = new Game(this.p1 - 1, this.p2 - 1, 0, 0, -1);
+        int die = 0;
+        int cnt = 0;
+        while (true) {
+            game.turn1(new int[] { 1 + die++, 1 + die++, 1 + die++ });
+            cnt += 3;
+            if (game.score1 >= 1000) {
+                log("p1 wins");
+                return game.score2 * cnt;
+            }
+            game.turn2(new int[] { 1 + die++, 1 + die++, 1 + die++ });
+            cnt += 3;
+            if (game.score2 >= 1000) {
+                log("p2 wins");
+                return game.score1 * cnt;
+            }
+        }
+    }
+    
+    private final Map<Game, Pair<Long, Long>> winsCache = new HashMap<>();
+    
+    private Pair<Long, Long> solve2(final Game game) {
+        if (winsCache.containsKey(game)) {
+            return winsCache.get(game);
+        }
+        if (game.score1 >= 21) {
+            return Tuples.pair(1L,  0L);
+        } else if (game.score2 >= 21) {
+            return Tuples.pair(0L, 1L);
+        }
+        Pair<Long, Long> wins = Tuples.pair(0L,  0L);
+        for (final int die1 : List.of(1, 2, 3)) {
+            for (final int die2 : List.of(1, 2, 3)) {
+                for (final int die3 : List.of(1, 2, 3)) {
+                    final Game newGame;
+                    if (game.player == 1) {
+                        newGame = Game.copyForPlayer2(game);
+                        newGame.turn1(new int[] { die1, die2, die3 });
+                    } else {
+                        newGame = Game.copyForPlayer1(game);
+                        newGame.turn2(new int[] { die1, die2, die3 });
+                    }
+                    final Pair<Long, Long> nwins = solve2(newGame);
+                    wins = Tuples.pair(wins.getOne() + nwins.getOne(),
+                                       wins.getTwo() + nwins.getTwo());
+                }
+            }
+        }
+        winsCache.put(game, wins);
+        return wins;
+    }
+    
+    @Override
+    public Long solvePart2() {
+        final Pair<Long, Long> ans
+            = solve2(new Game(this.p1 - 1, this.p2 - 1, 0, 0, 1));
+        log(ans);
+        return Math.max(ans.getOne(), ans.getTwo());
     }
 
     public static void main(final String[] args) throws Exception {
-        assert AoC2021_21.createDebug(TEST).solvePart1() == 739785;
-        assert AoC2021_21.create(TEST).solvePart2() == null;
+        assert AoC2021_21.create(TEST).solvePart1() == 739_785;
+        assert AoC2021_21.create(TEST).solvePart2() == 444_356_092_776_315L;
 
         final Puzzle puzzle = Aocd.puzzle(2021, 21);
         puzzle.check(
