aa3bc4a3794e15f2175d5e9750caa20cacd53672
pareronia <49491686+pareronia@users.noreply.github.com>
2021-12-21 21:15:30 +0100

    AoC 2021 Day 21 Part 2

--- src/main/python/AoC2021_21.py
+++ src/main/python/AoC2021_21.py
@@ -3,9 +3,10 @@
 # Advent of Code 2021 Day 21
 #
 
+from functools import lru_cache
 from aoc import my_aocd
 import aocd
-from aoc.common import clog
+from aoc.common import clog, log
 
 
 def _parse(inputs: tuple[str]) -> int:
@@ -30,8 +31,35 @@ def part_1(inputs: tuple[str]) -> int:
                 return score[(p + 1) % 2] * die
 
 
+@lru_cache(maxsize=None)
+def _solve2(pos1: int, pos2: int, score1: int, score2: int, player: int) \
+        -> tuple[int, int]:
+    if score1 >= 21:
+        return 1, 0
+    elif score2 >= 21:
+        return 0, 1
+    wins1, wins2 = 0, 0
+    for t1 in [1, 2, 3]:
+        for t2 in [1, 2, 3]:
+            for t3 in [1, 2, 3]:
+                if player == 1:
+                    npos1 = (pos1 + t1 + t2 + t3) % 10
+                    nscore1 = score1 + npos1 + 1
+                    nwins1, nwins2 = _solve2(npos1, pos2, nscore1, score2, 2)
+                else:
+                    npos2 = (pos2 + t1 + t2 + t3) % 10
+                    nscore2 = score2 + npos2 + 1
+                    nwins1, nwins2 = _solve2(pos1, npos2, score1, nscore2, 1)
+                wins1 += nwins1
+                wins2 += nwins2
+    return wins1, wins2
+
+
 def part_2(inputs: tuple[str]) -> int:
-    return None
+    pos1, pos2 = map(lambda x: x - 1, _parse(inputs))
+    ans = _solve2(pos1, pos2, score1=0, score2=0, player=1)
+    log(ans)
+    return max(ans)
 
 
 TEST = """\
@@ -45,7 +73,7 @@ def main() -> None:
     my_aocd.print_header(puzzle.year, puzzle.day)
 
     assert part_1(TEST) == 739785
-    assert part_2(TEST) == 0
+    assert part_2(TEST) == 444356092776315
 
     inputs = my_aocd.get_input(puzzle.year, puzzle.day, 2)
     result1 = part_1(inputs)
