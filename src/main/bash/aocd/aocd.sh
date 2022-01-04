19392353f38941531282407ceaea1a22aa7adf1f
pareronia <49491686+pareronia@users.noreply.github.com>
2022-01-05 00:02:39 +0100

    aocd for bash

---
+++ src/main/bash/aocd/aocd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+
+if [ "$AOCD_DIR" ]; then
+    _aocd__memodir="$AOCD_DIR"
+else
+    _aocd__memodir="$HOME/.config/aocd"
+fi
+
+if [ "$AOCD_SESSION" ]; then
+    _aocd__token="$AOCD_SESSION"
+else
+    IFS='' read -r _aocd__token < "$_aocd__memodir/token"
+fi
+
+_aocd__userid=$(eval "jq -r '.[\"$_aocd__token\"]' $_aocd__memodir/token2id.json")
+
+_aocd__inputFile() {
+    basename=$(basename "$0")
+    echo "$_aocd__memodir/$_aocd__userid/${basename:3:7}_input.txt"
+}
+
+_aocd__inputfile=$(_aocd__inputFile)
