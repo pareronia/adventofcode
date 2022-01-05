62798de11c468dd5a8dbc85617329cf4dd9d922d
pareronia <49491686+pareronia@users.noreply.github.com>
2022-01-05 12:43:03 +0100

    AoC 2021 Day 1 - bash - refactor

--- src/main/bash/AoC2021_01.sh
+++ src/main/bash/AoC2021_01.sh
@@ -3,14 +3,11 @@
 # Advent of Code 2021 Day 1
 #
 
-# shellcheck source=SCRIPTDIR/aocd/aocd.sh
-. "$(dirname "$0")/aocd/aocd.sh"
-
-mapfile -t depths < "$_aocd__inputfile"
-size="${#depths[@]}"
-
 countIncreases() {
     window="$1"
+    shift
+    depths=("$@")
+    size="${#depths[@]}"
     count=0
     for ((i = "$window"; i < size; i++))
     do
@@ -21,5 +18,18 @@ countIncreases() {
     echo "$count"
 }
 
-echo "Part 1: $(countIncreases 1)"
-echo "Part 2: $(countIncreases 3)"
+part1() {
+    countIncreases 1 "$@"
+}
+
+part2() {
+    countIncreases 3 "$@"
+}
+
+# shellcheck source=SCRIPTDIR/aocd/aocd.sh
+. "$(dirname "$0")/aocd/aocd.sh"
+
+mapfile -t input < "$_aocd__inputfile"
+
+echo "Part 1: $(part1 "${input[@]}")"
+echo "Part 2: $(part2 "${input[@]}")"
