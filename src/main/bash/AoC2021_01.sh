11c750682486e3b080803e63ada3a51c81370a22
pareronia <49491686+pareronia@users.noreply.github.com>
2022-01-07 14:03:59 +0100

    AoC 2021 Day 1 - bash - refactor
    Work directly on input file

--- src/main/bash/AoC2021_01.sh
+++ src/main/bash/AoC2021_01.sh
@@ -5,25 +5,23 @@
 
 countIncreases() {
     window="$1"
-    shift
-    depths=("$@")
-    size="${#depths[@]}"
+    mapfile -t depths < "$2"
     count=0
-    for ((i = "$window"; i < size; i++))
+    for ((i = "$window"; i < "${#depths[@]}"; i++))
     do
         if (("${depths[i]}" > "${depths[i-window]}")); then
-            count=$((count + 1))
+            ((count++))
         fi
     done
     echo "$count"
 }
 
 part1() {
-    countIncreases 1 "$@"
+    countIncreases 1 "$1"
 }
 
 part2() {
-    countIncreases 3 "$@"
+    countIncreases 3 "$1"
 }
 
 sample=(
@@ -39,9 +37,9 @@ sample=(
 263
 )
 
-sample1="$(part1 "${sample[@]}")"
+sample1="$(part1 <(for line in "${sample[@]}"; do echo "$line"; done))"
 [ "$sample1" = 7 ] || { echo "Part 1 sample failed: $sample1" >&2; exit 1; }
-sample2="$(part2 "${sample[@]}")"
+sample2="$(part2 <(for line in "${sample[@]}"; do echo "$line"; done))"
 [ "$sample2" = 5 ] || { echo "Part 2 sample failed: $sample2" >&2; exit 1; }
 
 # shellcheck source=SCRIPTDIR/aocd/aocd.sh
@@ -51,11 +49,10 @@ year=2021
 day=01
 inputfile="$(_aocd__inputFile "$year" "$day")" \
     || { echo "Input file not found: $inputfile" >&2; exit 1; }
-mapfile -t input < "$inputfile"
 
-part1="$(part1 "${input[@]}")"
+part1="$(part1 "$inputfile")"
 echo "Part 1: $part1"
-part2="$(part2 "${input[@]}")"
+part2="$(part2 "$inputfile")"
 echo "Part 2: $part2"
 
 check="$(_aocd__check "$year" "$day" "$part1" "$part2")" \
