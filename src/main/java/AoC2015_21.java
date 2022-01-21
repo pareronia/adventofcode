5fc6c379cc287c3c0959636ca25b204a9327fff1
pareronia <49491686+pareronia@users.noreply.github.com>
2022-01-22 00:26:44 +0100

    AoC 2015 Day 21 - java - correction, cleanup

--- src/main/java/AoC2015_21.java
+++ src/main/java/AoC2015_21.java
@@ -16,33 +16,43 @@ import java.util.Set;
 import java.util.function.Predicate;
 
 import org.apache.commons.collections4.SetUtils;
-import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import com.github.pareronia.aocd.Aocd;
+import com.github.pareronia.aocd.Puzzle;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 public class AoC2015_21 extends AoCBase {
 	
 	private final Boss boss;
 	private final List<PlayerConfig> playerConfigs;
 
-	protected AoC2015_21(List<String> input, boolean debug) {
+	protected AoC2015_21(final List<String> input, final boolean debug) {
 		super(debug);
 		this.boss = parse(input);
 		this.playerConfigs = setUpPlayerConfigs(setUpShop());
 	}
 
-	public static AoC2015_21 create(List<String> input) {
+	public static AoC2015_21 create(final List<String> input) {
 		return new AoC2015_21(input, false);
 	}
 	
-	private final Boss parse(List<String> inputs) {
+	public static AoC2015_21 createDebug(final List<String> input) {
+	    return new AoC2015_21(input, true);
+	}
+	
+	private final Boss parse(final List<String> inputs) {
 		assert inputs.size() == 3;
+		final Integer hitPoints
+				= Integer.valueOf(inputs.get(0).substring("Hit Points: ".length()));
 		final Integer damage
 				= Integer.valueOf(inputs.get(1).substring("Damage: ".length()));
 		final Integer armor
 				= Integer.valueOf(inputs.get(2).substring("Armor: ".length()));
-		return new Boss(damage, armor);
+		return new Boss(hitPoints, damage, armor);
 	}
 	
 	private Shop setUpShop() {
@@ -67,8 +77,8 @@ public class AoC2015_21 extends AoCBase {
 		return new Shop(items);
 	}
 	
-	private List<PlayerConfig> setUpPlayerConfigs(Shop shop) {
-		final ArrayList<PlayerConfig> configs = new ArrayList<>();
+	private List<PlayerConfig> setUpPlayerConfigs(final Shop shop) {
+		final List<PlayerConfig> configs = new ArrayList<>();
 		for (final ShopItem weapon : shop.getWeapons()) {
 			for (final ShopItem armor : shop.getArmor()) {
 				for (final Set<ShopItem> rings : shop.getRings()) {
@@ -87,13 +97,24 @@ public class AoC2015_21 extends AoCBase {
 	}
 
 	private Predicate<PlayerConfig> winsFromBoss() {
-		return playerConfig ->
-				playerConfig.getTotalDamage() - boss.getArmor() >=
-				boss.getDamage() - playerConfig.getTotalArmor();
+		return playerConfig -> {
+		    int playerHP = playerConfig.getHitPoints();
+		    int bossHP = boss.getHitPoints();
+		    while (true) {
+		        bossHP -= Math.max(playerConfig.getTotalDamage() - boss.getArmor(), 1);
+		        if (bossHP <= 0) {
+		            return true;
+		        }
+		        playerHP -= Math.max(boss.getDamage() - playerConfig.getTotalArmor(), 1);
+		        if (playerHP <= 0) {
+		            return false;
+		        }
+		    }
+		};
 	}
 	
-	private int solve(	Comparator<PlayerConfig> comparator,
-						Predicate<PlayerConfig> filter) {
+	private int solve(	final Comparator<PlayerConfig> comparator,
+						final Predicate<PlayerConfig> filter) {
 		return this.playerConfigs.stream()
 			.sorted(comparator)
 			.filter(filter)
@@ -114,41 +135,39 @@ public class AoC2015_21 extends AoCBase {
 						winsFromBoss().negate());
 	}
 
-	public static void main(String[] args) throws Exception {
-		final List<String> input = Aocd.getData(2015, 21);
+	public static void main(final String[] args) throws Exception {
+        final Puzzle puzzle = Aocd.puzzle(2015, 21);
 		
-		lap("Part 1", () -> AoC2015_21.create(input).solvePart1());
-		lap("Part 2", () -> AoC2015_21.create(input).solvePart2());
+        puzzle.check(
+            () -> lap("Part 1", () -> AoC2015_21.create(puzzle.getInputData()).solvePart1()),
+            () -> lap("Part 2", () -> AoC2015_21.create(puzzle.getInputData()).solvePart2())
+        );
 	}
 	
+	@RequiredArgsConstructor
+	@Getter
+	@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 	private static final class ShopItem {
 		private enum Type { WEAPON, ARMOR, RING, NONE }
 		
+		@EqualsAndHashCode.Include
 		private final Type type;
+		@EqualsAndHashCode.Include
 		private final String name;
 		private final Integer cost;
 		private final Integer damage;
 		private final Integer armor;
 
-		private ShopItem(Type type, String name, Integer cost,
-						 Integer damage, Integer armor) {
-			this.type = type;
-			this.name = name;
-			this.cost = cost;
-			this.damage = damage;
-			this.armor = armor;
-		}
-		
-		public static ShopItem weapon(String name, Integer cost, Integer damage) {
+		public static ShopItem weapon(final String name, final Integer cost, final Integer damage) {
 			return new ShopItem(Type.WEAPON, name, cost, damage, 0);
 		}
 		
-		public static ShopItem armor(String name, Integer cost, Integer armor) {
+		public static ShopItem armor(final String name, final Integer cost, final Integer armor) {
 			return new ShopItem(Type.ARMOR, name, cost, 0, armor);
 		}
 		
-		public static ShopItem ring(String name, Integer cost,
-									Integer damage, Integer armor) {
+		public static ShopItem ring(final String name, final Integer cost,
+									final Integer damage, final Integer armor) {
 			return new ShopItem(Type.RING, name, cost, damage, armor);
 		}
 		
@@ -166,49 +185,6 @@ public class AoC2015_21 extends AoCBase {
 			return this.type == Type.RING;
 		}
 
-		public String getName() {
-			return name;
-		}
-
-		public Integer getCost() {
-			return cost;
-		}
-
-		public Integer getDamage() {
-			return damage;
-		}
-
-		public Integer getArmor() {
-			return armor;
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((name == null) ? 0 : name.hashCode());
-			result = prime * result + ((type == null) ? 0 : type.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj) {
-				return true;
-			}
-			if (obj == null) {
-				return false;
-			}
-			if (getClass() != obj.getClass()) {
-				return false;
-			}
-			final ShopItem other = (ShopItem) obj;
-			return new EqualsBuilder()
-					.append(this.name, other.name)
-					.append(this.type, other.type)
-				.isEquals();
-		}
-
 		@Override
 		public String toString() {
 			final StringBuilder builder = new StringBuilder();
@@ -222,13 +198,10 @@ public class AoC2015_21 extends AoCBase {
 		}
 	}
 	
+	@RequiredArgsConstructor
 	private static final class Shop {
 		private final Set<ShopItem> items;
 
-		public Shop(Set<ShopItem> items) {
-			this.items = items;
-		}
-		
 		public Set<ShopItem> getWeapons() {
 			return this.items.stream()
 					.filter(ShopItem::isWeapon)
@@ -269,13 +242,12 @@ public class AoC2015_21 extends AoCBase {
 		}
 	}
 	
+	@RequiredArgsConstructor
 	private static final class PlayerConfig {
+	    @Getter
+	    private final int hitPoints = 100;
 		private final Set<ShopItem> items;
 
-		public PlayerConfig(Set<ShopItem> items) {
-			this.items = items;
-		}
-		
 		public int getTotalCost() {
 			return this.items.stream().collect(summingInt(ShopItem::getCost));
 		}
@@ -299,21 +271,11 @@ public class AoC2015_21 extends AoCBase {
 		}
 	}
 	
+	@RequiredArgsConstructor
+	@Getter
 	private static final class Boss {
+	    private final Integer hitPoints;
 		private final Integer damage;
 		private final Integer armor;
-
-		public Boss(Integer damage, Integer armor) {
-			this.damage = damage;
-			this.armor = armor;
-		}
-
-		public Integer getDamage() {
-			return damage;
-		}
-
-		public Integer getArmor() {
-			return armor;
-		}
 	}
 }
