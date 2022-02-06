67b10bf09caecae06c039fa951aaf7be2cbe24eb
pareronia <49491686+pareronia@users.noreply.github.com>
2022-02-06 22:48:05 +0100

    AoC 2015 Day 2 - julia

---
+++ src/main/julia/AoC2015_02.jl
@@ -0,0 +1,43 @@
+#! /usr/bin/env julia
+#
+
+include("aoc.jl")
+include("aocd.jl")
+using .Aocd
+
+function solve(input, f)
+    ans = 0
+    for line ∈ input
+        l, w, h = parse.(Int, split(line, "x"))
+        ans += f(l, w, h)
+    end
+    return ans
+end
+
+function part1(input)
+    function area(l, w, h)
+        sides = (2 * l * w, 2 * w * h, 2 * h * l)
+        return sum(sides) + minimum(sides) ÷ 2
+    end
+    return solve(input, (l, w, h) -> area(l, w, h))
+end
+
+function part2(input)
+    function length(l, w, h)
+        circumferences = (2 * (l + w), 2 * (w + h), 2 * (h + l))
+        return minimum(circumferences) + l * w * h
+    end
+    return solve(input, (l, w, h) -> length(l, w, h))
+end
+
+TEST1 = @aoc_splitlines("2x3x4")
+TEST2 = @aoc_splitlines("1x1x10")
+
+function samples()
+    @assert part1(TEST1) == 58
+    @assert part1(TEST2) == 43
+    @assert part2(TEST1) == 34
+    @assert part2(TEST2) == 14
+end
+
+@aoc_main 2015 2
