f0581316ba8e54581e2953f7d3b6ef0faff3d5ef
pareronia <49491686+pareronia@users.noreply.github.com>
2022-02-06 19:51:38 +0100

    AoC 2015 Day 1 - julia

---
+++ src/main/julia/AoC2015_01.jl
@@ -0,0 +1,47 @@
+#! /usr/bin/env julia
+#
+
+include("aoc.jl")
+include("aocd.jl")
+using .Aocd
+
+function part1(input)
+    return length(input[1]) - 2 * count(==(')'), input[1])
+end
+
+function part2(input)
+    sum = 0
+    for (i, c) ∈ enumerate(input[1])
+        sum += (c == '(' ? 1 : -1)
+        sum == -1 && return i
+    end
+    error("Unsolvable")
+end
+
+TEST1 = @aoc_splitlines("(())")
+TEST2 = @aoc_splitlines("()()")
+TEST3 = @aoc_splitlines("(((")
+TEST4 = @aoc_splitlines("(()(()(")
+TEST5 = @aoc_splitlines("))(((((")
+TEST6 = @aoc_splitlines("())")
+TEST7 = @aoc_splitlines("))(")
+TEST8 = @aoc_splitlines(")))")
+TEST9 = @aoc_splitlines(")())())")
+TEST10 = @aoc_splitlines(")")
+TEST11 = @aoc_splitlines("()())")
+
+function samples()
+    @assert part1(TEST1) == 0
+    @assert part1(TEST2) == 0
+    @assert part1(TEST3) == 3
+    @assert part1(TEST4) == 3
+    @assert part1(TEST5) == 3
+    @assert part1(TEST6) == -1
+    @assert part1(TEST7) == -1
+    @assert part1(TEST8) == -3
+    @assert part1(TEST9) == -3
+    @assert part2(TEST10) == 1
+    @assert part2(TEST11) == 5
+end
+
+@aoc_main 2015 1
