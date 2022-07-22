17d5bc9eb0ccd93a0a942f2049bd3c31536862cc
pareronia <49491686+pareronia@users.noreply.github.com>
2022-07-22 21:37:30 +0200

    java - fix unit tests

--- src/test/java/AllUnitTests.java
+++ src/test/java/AllUnitTests.java
@@ -1,10 +1,10 @@
-import org.junit.extensions.dynamicsuite.Directory;
+import org.junit.extensions.dynamicsuite.ClassPath;
 import org.junit.extensions.dynamicsuite.Filter;
 import org.junit.extensions.dynamicsuite.suite.DynamicSuite;
 import org.junit.runner.RunWith;
 
 @RunWith(DynamicSuite.class)
 @Filter(UnitTestClassFilter.class)
-@Directory
+@ClassPath
 public class AllUnitTests {
 }
