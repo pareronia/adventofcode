e2ee2586eb8dda9236ce941ae1c51471c1b730bd
pareronia <49491686+pareronia@users.noreply.github.com>
2022-08-03 22:43:36 +0200

    AoC 2020 Day 16 - faster, smaller

--- src/main/python/AoC2020_16.py
+++ src/main/python/AoC2020_16.py
@@ -3,143 +3,95 @@
 # Advent of Code 2020 Day 16
 #
 
-from dataclasses import dataclass
+from __future__ import annotations
+from typing import NamedTuple
 from collections import defaultdict
-from functools import lru_cache
+from collections.abc import Generator
 from math import prod
 from aoc import my_aocd
-from aoc.common import log
+import aocd
 
 
-@dataclass(eq=True, frozen=True)
-class Rule:
+class Rule(NamedTuple):
     field: str
     valid_ranges: frozenset[tuple[int, int]]
 
     def validate(self, value: int) -> bool:
-        valid = False
-        for range_ in self.valid_ranges:
-            if range_[0] <= value <= range_[1]:
-                valid = True
-        return valid
+        return any(rng[0] <= value <= rng[1] for rng in self.valid_ranges)
 
 
-def parse_rules(lines: list[str]) -> list[Rule]:
-    rules = list()
-    for line in lines:
-        split1 = line.split(": ")
-        split2 = split1[1].split(" or ")
-        ranges = set()
-        for split in split2:
-            split3 = split.split("-")
-            ranges.add((int(split3[0]), int(split3[1])))
-        rule = Rule(split1[0], frozenset(ranges))
-        rules.append(rule)
-    return rules
+class Matches:
+    matches: dict[Rule, set[int]]
+
+    def __init__(self, matches: dict[Rule, set[int]]):
+        self.matches = matches
+
+    @classmethod
+    def create(cls, tickets: tuple[tuple[int]], rules: tuple[Rule]) -> Matches:
+        valid_tickets = tuple(t for t in tickets if not _is_invalid(t, rules))
+        matches = defaultdict(set)
+        for rule in rules:
+            for idx in range(len(tickets[0])):
+                if all(rule.validate(t[idx]) for t in valid_tickets):
+                    matches[rule] |= {idx}
+        return Matches(matches)
+
+    def _remove(self, match: tuple[Rule, int]) -> None:
+        rule, idx = match
+        del self.matches[rule]
+        for v in self.matches.values():
+            v -= {idx}
+
+    def get_matches(self) -> Generator[tuple[Rule, int]]:
+        while len(self.matches) > 0:
+            rule = [k for k, v in self.matches.items() if len(v) == 1][0]
+            idx = [i for i in self.matches[rule]][0]
+            self._remove((rule, idx))
+            yield (rule, idx)
 
 
-def parse_tickets(lines: list[str]) -> tuple[int]:
-    return tuple([tuple([int(_) for _ in line.split(",")]
-                        ) for line in lines[1:]]
-                 )
+def _parse_rule(line: str) -> Rule:
+    split1 = line.split(": ")
+    ranges = {tuple(int(_) for _ in split.split("-"))
+              for split in split1[1].split(" or ")}
+    return Rule(split1[0], frozenset(ranges))
 
 
-def parse(inputs: tuple[str]):
+def _parse_ticket(line: str) -> tuple[int]:
+    return tuple(int(_) for _ in line.split(","))
+
+
+def _parse(inputs: tuple[str]):
     blocks = my_aocd.to_blocks(inputs)
-    rules = parse_rules(blocks[0])
-    my_ticket = parse_tickets(blocks[1])[0]
-    tickets = parse_tickets(blocks[2])
+    rules = tuple(_parse_rule(_) for _ in blocks[0])
+    my_ticket = _parse_ticket(blocks[1][1])
+    tickets = tuple(_parse_ticket(_) for _ in blocks[2][1:])
     return rules, my_ticket, tickets
 
 
-def _get_invalid_values(t: list[int], rules: list[Rule]) -> list[int]:
-    all_valid_for_one_field = False
-    invalid_t = []
-    for _ in t:
-        valid_for_one_field = False
-        for r in rules:
-            valid_for_one_field = valid_for_one_field or r.validate(_)
-        if not valid_for_one_field:
-            invalid_t.append(_)
-        all_valid_for_one_field = all_valid_for_one_field \
-            and valid_for_one_field
-    if not all_valid_for_one_field:
-        return invalid_t
-    else:
-        return []
+def _does_not_match_any_rule(value: int, rules: tuple[Rule]) -> bool:
+    return not any(r.validate(value) for r in rules)
+
+
+def _get_invalid_values(ticket: tuple[int], rules: tuple[Rule]) -> tuple[int]:
+    return tuple(v for v in ticket if _does_not_match_any_rule(v, rules))
+
+
+def _is_invalid(ticket: tuple[int], rules: tuple[Rule]) -> bool:
+    return any(_does_not_match_any_rule(v, rules) for v in ticket)
 
 
 def part_1(inputs: tuple[str]) -> int:
-    rules, my_ticket, tickets = parse(inputs)
-    log(rules)
-    log(tickets)
-    return sum([
-        _ for _ in [
-            __ for t in tickets for __ in _get_invalid_values(t, rules)
-        ]
-    ])
-
-
-def _all_values_valid_for_rule(values: list[int], rule: Rule) -> bool:
-    all_is_matched = True
-    for ticket_i in values:
-        all_is_matched = all_is_matched and rule.validate(ticket_i)
-    return all_is_matched
-
-
-@lru_cache(maxsize=400)
-def _match_ticket_field_and_rule(tickets, i: int, rule: Rule) -> bool:
-    ticket_is = [t[i] for t in tickets]
-    return _all_values_valid_for_rule(ticket_is, rule)
-
-
-def _do_match(tickets, idxs: list[int], rules) -> dict[int, list[Rule]]:
-    matches = defaultdict(list)
-    [matches[i].append(rule) for i in idxs for rule in rules
-     if _match_ticket_field_and_rule(tickets, i, rule)]
-    return matches
-
-
-def _find_fields(fields, tickets, idxs: list[int], rules) -> None:
-    if len(idxs) == 0 and len(rules) == 0:
-        return
-    log("Fields:")
-    log(fields)
-    matches = _do_match(tickets, idxs, rules)
-    log("matches:")
-    [log(i) for i in matches.items()]
-    temp = [i for i in matches.items()]
-    temp.sort(key=lambda m: len(m[1]))
-    if not len(temp[0][1]) == 1:
-        raise RuntimeError("Expected single match")
-    idx_t = temp[0][0]
-    r_t = temp[0][1][0]
-    fields.append((idx_t, r_t))
-    new_idxs = [idx for idx in idxs if idx != idx_t]
-    new_rules = [r for r in rules if r != r_t]
-    _find_fields(fields, tickets, new_idxs, new_rules)
+    rules, _, tickets = _parse(inputs)
+    return sum(v for t in tickets for v in _get_invalid_values(t, rules))
 
 
 def part_2(inputs: tuple[str]) -> int:
-    rules, my_ticket, tickets = parse(inputs)
-    log(rules)
-    log(my_ticket)
-    log("tickets: " + str(len(tickets)))
-    assert sum([len(t) for t in tickets]) % len(rules) == 0
-    valid_tickets = tuple([t for t in tickets
-                           if len(_get_invalid_values(t, rules)) == 0])
-    log("valid tickets: " + str(len(valid_tickets)))
-    idxs = [i for i in range(len(valid_tickets[0]))]
-    fields = list[tuple[int, str]]()
-    _find_fields(fields, valid_tickets, idxs, rules)
-    log("Fields:")
-    log(fields)
-    log(_match_ticket_field_and_rule.cache_info())
-    departure_fields_values = []
-    for f in fields:
-        if f[1].field.startswith("departure"):
-            departure_fields_values.append(my_ticket[f[0]])
-    return prod(departure_fields_values)
+    rules, my_ticket, tickets = _parse(inputs)
+    matches = Matches.create(tickets, rules)
+    return prod(my_ticket[idx]
+                for rule, idx in matches.get_matches()
+                if rule.field.startswith("departure "))
 
 
 TEST1 = """\
@@ -172,16 +124,18 @@ nearby tickets:
 
 
 def main() -> None:
-    my_aocd.print_header(2020, 16)
+    puzzle = aocd.models.Puzzle(2020, 16)
+    my_aocd.print_header(puzzle.year, puzzle.day)
 
     assert part_1(TEST1) == 71
     assert part_2(TEST2) == 1716
 
-    inputs = my_aocd.get_input(2020, 16, 261)
+    inputs = my_aocd.get_input_data(puzzle, 261)
     result1 = part_1(inputs)
     print(f"Part 1: {result1}")
     result2 = part_2(inputs)
     print(f"Part 2: {result2}")
+    my_aocd.check_results(puzzle, result1, result2)
 
 
 if __name__ == '__main__':
