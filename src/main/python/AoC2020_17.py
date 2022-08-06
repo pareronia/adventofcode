49d03a34326e41f26cce4ce303cffcc8b5c8f09b
pareronia <49491686+pareronia@users.noreply.github.com>
2022-08-06 21:00:02 +0200

    AoC 2020 Day 17 - smaller

--- src/main/python/AoC2020_17.py
+++ src/main/python/AoC2020_17.py
@@ -2,145 +2,37 @@
 #
 # Advent of Code 2020 Day 17
 #
-# TODO: make one solver for GoL for multiple dimensions
 
-from __future__ import annotations
-from collections import defaultdict
-from functools import lru_cache
 from aoc import my_aocd
-from aoc.common import log
+from aoc.game_of_life import GameOfLife
+import aocd
 
 
 ON = "#"
-OFF = "."
+GENERATIONS = 6
 
 
-def _create_cell_1(r: int, c: int) -> tuple[int, ...]:
-    return (0, r, c)
+def _parse(inputs: tuple[str], dim: int) -> GameOfLife:
+    return GameOfLife({(r, c, 0, 0)
+                       for r, row in enumerate(inputs)
+                       for c, state in enumerate(row)
+                       if state == ON},
+                      dim)
 
 
-def _create_cell_2(r: int, c: int) -> tuple[int, ...]:
-    return (0, 0, r, c)
-
-
-def _parse(inputs: tuple[str], cell_factory) -> set[tuple[int, ...]]:
-    return set(cell_factory(r, c)
-               for r, row in enumerate(inputs)
-               for c, state in enumerate(row)
-               if state == ON
-               )
-
-
-@lru_cache(maxsize=1500)
-def _get_neighbours_1(z: int, y: int, x: int) -> tuple[int]:
-    return tuple((layer, row, col)
-                 for layer in range(z-1, z+2)
-                 for row in range(y-1, y+2)
-                 for col in range(x-1, x+2)
-                 if not (layer == z and row == y and col == x)
-                 )
-
-
-@lru_cache(maxsize=25000)
-def _get_neighbours_2(w: int, z: int, y: int, x: int) -> tuple[int]:
-    return tuple((wtf, layer, row, col)
-                 for wtf in range(w-1, w+2)
-                 for layer in range(z-1, z+2)
-                 for row in range(y-1, y+2)
-                 for col in range(x-1, x+2)
-                 if not (wtf == w and layer == z and row == y and col == x)
-                 )
-
-
-def _next_generation(space: set[tuple[int, ...]],
-                     neighbour_factory) -> set[tuple[int, ...]]:
-    to_on = set()
-    to_off = set()
-    for cell in space:
-        neighbours = neighbour_factory(*cell)
-        neighbours_on = sum(1 for _ in neighbours if _ in space)
-        if neighbours_on in {2, 3}:
-            to_on.add(cell)
-        else:
-            to_off.add(cell)
-        for n in set(neighbours) - space:
-            n_neighbours = neighbour_factory(*n)
-            n_neighbours_on = sum(1 for _ in n_neighbours if _ in space)
-            if n_neighbours_on == 3:
-                to_on.add(n)
-    space |= to_on
-    space ^= to_off
-    return space
-
-
-def _output_1(space: set):
-    if not __debug__:
-        return
-    zz = defaultdict(set)
-    [zz[cell[0]].add((cell[1], cell[2])) for cell in space]
-    result = list[str]()
-    for z in sorted(zz.keys()):
-        min_c = min(c for r, c in zz[z])
-        max_c = max(c for r, c in zz[z])
-        min_r = min(r for r, c in zz[z])
-        max_r = max(r for r, c in zz[z])
-        result.append(f"z={z}")
-        for r in range(min_r, max_r+1):
-            line = ""
-            for c in range(min_c, max_c+1):
-                line += (ON if (r, c) in zz[z] else OFF)
-            result.append(line)
-        result.append(" ")
-    log(result)
-
-
-def _output_2(space: set):
-    if not __debug__:
-        return
-    w = defaultdict(set)
-    [w[cell[0]].add((cell[1], cell[2], cell[3])) for cell in space]
-    wz = defaultdict(set)
-    for k, v in w.items():
-        z = defaultdict(set)
-        [z[cell[0]].add((cell[1], cell[2])) for cell in v]
-        wz[k] = z
-    result = list[str]()
-    for w in sorted(wz.keys()):
-        min_z = min(z for z in wz[w].keys() if z >= 0)
-        min_c = min(c for r, c in wz[w][min_z])
-        max_c = max(c for r, c in wz[w][min_z])
-        min_r = min(r for r, c in wz[w][min_z])
-        max_r = max(r for r, c in wz[w][min_z])
-        for z in sorted(wz[w].keys()):
-            result.append(f"z={z}, w={w}")
-            for r in range(min_r, max_r+1):
-                line = ""
-                for c in range(min_c, max_c+1):
-                    line += (ON if (r, c) in wz[w][z] else OFF)
-                result.append(line)
-            result.append(" ")
-    log(result)
-
-
-def _run_generations(space: set[tuple[int, ...]],
-                     generations: int,
-                     neighbour_factory,
-                     output) -> set[tuple[int, ...]]:
-    for i in range(generations):
-        log(f"GENERATION #{i+1}")
-        output(space := _next_generation(space, neighbour_factory))
-    log(neighbour_factory.cache_info())
-    return space
+def _solve(inputs: tuple[str], dim: int) -> int:
+    game_of_life = _parse(inputs, dim)
+    for i in range(GENERATIONS):
+        game_of_life.next_generation()
+    return len(game_of_life.alive)
 
 
 def part_1(inputs: tuple[str]) -> int:
-    _output_1(space := _parse(inputs, _create_cell_1))
-    return len(_run_generations(space, 6, _get_neighbours_1, _output_1))
+    return _solve(inputs, 3)
 
 
 def part_2(inputs: tuple[str]) -> int:
-    _output_2(space := _parse(inputs, _create_cell_2))
-    return len(_run_generations(space, 6, _get_neighbours_2, _output_2))
+    return _solve(inputs, 4)
 
 
 TEST = """\
@@ -151,16 +43,18 @@ TEST = """\
 
 
 def main() -> None:
-    my_aocd.print_header(2020, 17)
+    puzzle = aocd.models.Puzzle(2020, 17)
+    my_aocd.print_header(puzzle.year, puzzle.day)
 
     assert part_1(TEST) == 112
     assert part_2(TEST) == 848
 
-    inputs = my_aocd.get_input(2020, 17, 8)
+    inputs = my_aocd.get_input_data(puzzle, 8)
     result1 = part_1(inputs)
     print(f"Part 1: {result1}")
     result2 = part_2(inputs)
     print(f"Part 2: {result2}")
+    my_aocd.check_results(puzzle, result1, result2)
 
 
 if __name__ == '__main__':
