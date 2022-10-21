e5fcbb5f912494608ebfc67845c8d93e73822227
pareronia <49491686+pareronia@users.noreply.github.com>
2022-10-21 22:48:15 +0200

    AoC 2017 Day 11 - java - smaller

--- src/main/java/AoC2017_11.java
+++ src/main/java/AoC2017_11.java
@@ -3,28 +3,33 @@ import static java.util.stream.Collectors.toList;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
-import java.util.EnumSet;
 import java.util.List;
+import java.util.Map;
 
 import com.github.pareronia.aoc.geometry3d.Position3D;
 import com.github.pareronia.aoc.geometry3d.Vector3D;
 import com.github.pareronia.aocd.Aocd;
 import com.github.pareronia.aocd.Puzzle;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
 public final class AoC2017_11 extends AoCBase {
     
     private static final Position3D ORIGIN = Position3D.of(0, 0, 0);
+    private static final Map<String, Vector3D> HEADING = Map.of(
+        "n",  Vector3D.of( 0, -1,  1),
+        "ne", Vector3D.of( 1, -1,  0),
+        "se", Vector3D.of( 1,  0, -1),
+        "s",  Vector3D.of( 0,  1, -1),
+        "sw", Vector3D.of(-1,  1,  0),
+        "nw", Vector3D.of(-1,  0,  1)
+    );
 
-    final List<Heading> path;
+    final List<Vector3D> path;
 
     private AoC2017_11(final List<String> inputs, final boolean debug) {
         super(debug);
         assert inputs.size() == 1;
         this.path = Arrays.stream(inputs.get(0).split(","))
-            .map(Heading::fromValue)
+            .map(HEADING::get)
             .collect(toList());
     }
 
@@ -39,7 +44,6 @@ public final class AoC2017_11 extends AoCBase {
     private Deque<Position3D> positions() {
         final Deque<Position3D> positions = new ArrayDeque<>(List.of(ORIGIN));
         this.path.stream()
-            .map(Heading::asVector3D)
             .map(v -> positions.getLast().translate(v))
             .forEach(positions::add);
         return positions;
@@ -77,30 +81,4 @@ public final class AoC2017_11 extends AoCBase {
     private static final List<String> TEST2 = splitLines("ne,ne,sw,sw");
     private static final List<String> TEST3 = splitLines("ne,ne,s,s");
     private static final List<String> TEST4 = splitLines("se,sw,se,sw,sw");
-
-    @RequiredArgsConstructor
-    @Getter
-    private enum Heading {
-        NORTH       ("n",    0, -1,  1),
-        NORTHEAST   ("ne",   1, -1,  0),
-        SOUTHEAST   ("se",   1,  0, -1),
-        SOUTH       ("s",    0,  1, -1),
-        SOUTHWEST   ("sw",  -1,  1,  0),
-        NORTHWEST   ("nw",  -1,  0,  1);
-        
-        private final String value;
-        private final int q;
-        private final int r;
-        private final int s;
-        
-        public static Heading fromValue(final String value) {
-            return EnumSet.allOf(Heading.class).stream()
-                .filter(h -> h.value.equals(value))
-                .findFirst().orElseThrow();
-        }
-        
-        public Vector3D asVector3D() {
-            return Vector3D.of(q, r, s);
-        }
-    }
 }
