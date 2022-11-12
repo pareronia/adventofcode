3a857e54cebfb65ebb2b99e7cba15ae96b057b4c
pareronia <49491686+pareronia@users.noreply.github.com>
2022-11-12 23:11:49 +0100

    runner module - java - correctly handling missing implementation

--- src/main/python/aoc/runner/java.py
+++ src/main/python/aoc/runner/java.py
@@ -11,11 +11,15 @@ from .config import config
 class Java(Plugin):
     def _java(self, year: int, day: int, data: str):
         completed = subprocess.run(  # nosec
-            [config.java['command'],
-             "-cp",
-             ":".join(config.java['classpath']),
-             config.java['class'],
-             str(year), str(day), data],
+            [
+                config.java["command"],
+                "-cp",
+                ":".join(config.java["classpath"]),
+                config.java["class"],
+                str(year),
+                str(day),
+                data,
+            ],
             text=True,
             capture_output=True,
         )
@@ -37,17 +41,18 @@ class Java(Plugin):
 
     def java(self, year: int, day: int, data: str):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((config.java['server']['host'],
-                       config.java['server']['port']))
-            s.send(f'{year}\r\n'.encode('UTF-8'))
-            s.send(f'{day}\r\n'.encode('UTF-8'))
-            s.send(f'{data}\r\n'.encode('UTF-8'))
-            s.send(b'END\r\n')
+            s.connect(
+                (config.java["server"]["host"], config.java["server"]["port"])
+            )
+            s.send(f"{year}\r\n".encode("UTF-8"))
+            s.send(f"{day}\r\n".encode("UTF-8"))
+            s.send(f"{data}\r\n".encode("UTF-8"))
+            s.send(b"END\r\n")
             data = s.recv(1024)
-        result = data.decode('UTF-8').rstrip()
+        result = data.decode("UTF-8").rstrip()
         self.log.info(f"Result: {result}")
-        if result:
-            result = json.loads(result)
+        result = json.loads(result)
+        if "part1" in result and "part2" in result:
             return (
                 Result.ok(
                     result["part1"]["answer"],
@@ -63,29 +68,32 @@ class Java(Plugin):
 
     def start_java(self):
         completed = subprocess.Popen(  # nosec
-            [config.java['server']['command'],
-             "-cp",
-             ":".join(config.java['server']['classpath']),
-             config.java['server']['class'],
-             ],
+            [
+                config.java["server"]["command"],
+                "-cp",
+                ":".join(config.java["server"]["classpath"]),
+                config.java["server"]["class"],
+            ],
         )
         if not completed.pid:
             raise RuntimeError("Could not start Java run server")
         time.sleep(0.5)
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((config.java['server']['host'],
-                       config.java['server']['port']))
-            s.send(('HELLO' + os.linesep).encode('UTF-8'))
+            s.connect(
+                (config.java["server"]["host"], config.java["server"]["port"])
+            )
+            s.send(("HELLO" + os.linesep).encode("UTF-8"))
             data = s.recv(1024)
-        results = data.decode('UTF-8').rstrip().splitlines()
+        results = data.decode("UTF-8").rstrip().splitlines()
         if results != ["HELLO"]:
             raise RuntimeError("No response from Java run server")
 
     def stop_java(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((config.java['server']['host'],
-                       config.java['server']['port']))
-            s.send(b'STOP')
+            s.connect(
+                (config.java["server"]["host"], config.java["server"]["port"])
+            )
+            s.send(b"STOP")
 
     def start(self):
         self.log.debug("Starting")
