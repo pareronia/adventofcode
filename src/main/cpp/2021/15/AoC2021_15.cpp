05c0f0b57f8277fc9ba52c20ba57973164069d95
pareronia <49491686+pareronia@users.noreply.github.com>
2022-11-13 13:14:19 +0100

    cpp - refactor A*

--- src/main/cpp/2021/15/AoC2021_15.cpp
+++ src/main/cpp/2021/15/AoC2021_15.cpp
@@ -1,38 +1,31 @@
-#include <bits/stdc++.h>
-#include "../../aoc/aoc.hpp"
-#include "../../aoc/grid/grid.hpp"
 #include "../../aoc/graph/graph.hpp"
-#include "../../aocd/aocd.hpp"
+#include "../../aoc/grid/grid.hpp"
 
 using namespace std;
 
-const vector<int> DR = { -1, 0, 1, 0 };
-const vector<int> DC = { 0, 1, 0, -1 };
+const vector<int> DR = {-1, 0, 1, 0};
+const vector<int> DC = {0, 1, 0, -1};
 const Cell START = Cell::at(0, 0);
 
 uint getRisk(const Grid<int>& grid, const Cell& cell) {
-    const Cell actual = Cell(cell.row() % grid.height(),
-                             cell.col() % grid.width());
-    int value = grid.get(actual)
-                    + cell.row() / grid.height()
-                    + cell.col() / grid.width();
+    const Cell actual =
+        Cell(cell.row() % grid.height(), cell.col() % grid.width());
+    int value = grid.get(actual) + cell.row() / grid.height() +
+                cell.col() / grid.width();
     while (value > 9) {
         value -= 9;
     }
     return value;
 }
 
-unordered_set<Cell> findNeighbours(
-        const Grid<int>& grid,
-        const Cell& cell,
-        const uint tiles
-) {
+unordered_set<Cell> findNeighbours(const Grid<int>& grid, const Cell& cell,
+                                   const uint tiles) {
     unordered_set<Cell> neighbours;
     for (uint i = 0; i < 4; i++) {
         const uint rr = cell.row() + DR[i];
         const uint cc = cell.col() + DC[i];
-        if (rr >= 0 && rr < tiles * grid.height()
-                && cc >= 0 && cc < tiles * grid.width()) {
+        if (rr >= 0 && rr < tiles * grid.height() && cc >= 0 &&
+            cc < tiles * grid.width()) {
             neighbours.insert(Cell::at(rr, cc));
         }
     }
@@ -41,27 +34,21 @@ unordered_set<Cell> findNeighbours(
 
 int solve(const vector<string>& input, const uint tiles) {
     const Grid<int> grid = Grid<int>::from(input);
-    const Cell end = Cell(tiles * grid.height() - 1,
-                          tiles * grid.width() - 1);
+    const Cell end = Cell(tiles * grid.height() - 1, tiles * grid.width() - 1);
     const AStar::Result<Cell> result = AStar::execute<Cell>(
-            START,
-            [&end](const Cell& cell) { return cell == end; },
-            [&grid, &tiles](const Cell& cell) {
-                return findNeighbours(grid, cell, tiles);
-            },
-            [&grid](const Cell& cell) { return getRisk(grid, cell); }
-    );
-    return result.cost;
+        START, [&end](const Cell& cell) { return cell == end; },
+        [&grid, &tiles](const Cell& cell) {
+            return findNeighbours(grid, cell, tiles);
+        },
+        [&grid](const Cell& cell) { return getRisk(grid, cell); });
+    return result.getDistances()[end];
 }
 
-int part1(const vector<string>& input) {
-    return solve(input, 1);
-}
+int part1(const vector<string>& input) { return solve(input, 1); }
 
-int part2(const vector<string>& input) {
-    return solve(input, 5);
-}
+int part2(const vector<string>& input) { return solve(input, 5); }
 
+// clang-format off
 const vector<string> TEST = {
     "1163751742",
     "1381373672",
@@ -74,8 +61,9 @@ const vector<string> TEST = {
     "1293138521",
     "2311944581"
 };
+// clang-format on
 
- void samples() {
+void samples() {
     assert(part1(TEST) == 40);
     assert(part2(TEST) == 315);
 }
--- src/main/cpp/aoc/graph/graph.hpp
+++ src/main/cpp/aoc/graph/graph.hpp
@@ -1,111 +1,141 @@
 #ifndef _AOC_GRAPH_HPP_
 #define _AOC_GRAPH_HPP_
 
-#include <bits/stdc++.h>
+#include <assert.h>
+
+#include <deque>
+#include <functional>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "../../aoc/aoc.hpp"
 
 using namespace std;
+template <typename T>
+using Distances = unordered_map<T, ulong>;
+template <typename T>
+using Paths = unordered_map<T, T>;
 
-template<typename T>
+template <typename T>
 class State {
-    public:
-        State(const T _node, const ulong _cost)
-            : node(std::move(_node)), cost(_cost) {}
-        bool operator >(const State<T>& other) const {
-            return compare(other) > 0;
-        }
-        bool operator ==(const State<T>& other) const {
-            return this->compare(other) == 0;
-        }
-        ostream& operator <<(ostream& strm) {
-            strm << "State[node: " << node << ", cost: " << cost << "]";
-            return strm;
-        }
-        T node;
-        ulong cost;
-    private:
-        int compare(const State<T>& other) const {
-            if (cost == other.cost) {
-                return 0;
-            } else if (cost > other.cost) {
-                return 1;
-            } else {
-                return -1;
-            }
-        }
+    friend class AStar;
+
+   public:
+    State(const T _node, const ulong _cost)
+        : node(std::move(_node)), cost(_cost) {}
+    bool operator>(const State<T>& other) const { return compare(other) > 0; }
+    bool operator==(const State<T>& other) const {
+        return this->compare(other) == 0;
+    }
+    ostream& operator<<(ostream& strm);
+
+   private:
+    T node;
+    ulong cost;
+    int compare(const State<T>& other) const;
 };
 
-template<typename T>
+template <typename T>
 struct std::hash<State<T>> {
-    std::size_t operator ()(const State<T>& state) const noexcept {
+    std::size_t operator()(const State<T>& state) const noexcept {
         return std::hash<T>{}(state.node);
     }
 };
 
+template <typename T>
+int State<T>::compare(const State<T>& other) const {
+    if (cost == other.cost) {
+        return 0;
+    } else if (cost > other.cost) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+template <typename T>
+ostream& State<T>::operator<<(ostream& strm) {
+    strm << "State[node: " << node << ", cost: " << cost << "]";
+    return strm;
+}
+
 class AStar {
-private:
-    template<typename T>
-    static ulong getOrDefault(
-            const unordered_map<T, ulong>& map,
-            const T& node,
-            const ulong defValue
-    ) {
-        auto it = map.find(node);
-        return it == map.end() ? defValue : it->second;
-    }
+   public:
+    template <typename T>
+    class Result {
+       public:
+        Result(const T& source_, const Distances<T>& distances_,
+               const Paths<T>& paths_)
+            : source(source_), distances(distances_), paths(paths_) {}
+        Distances<T> getDistances() const { return distances; }
+        Paths<T> getPaths() const { return paths; }
+        vector<T> getPath(const T& t) const;
 
-    template<typename T>
-    static vector<T> createPath(
-            const unordered_map<T, T>& parent,
-            const T& end
-    ) {
-        T curr = end;
-        vector<T> path({end});
-        while (parent.find(curr) != parent.end()) {
-            curr = parent.at(curr);
-            path.push_back(curr);
-        }
-        return path;
-    }
-
-public:
-    template<typename T>
-    struct Result {
-        Result(vector<T> path_, ulong cost_) {
-            path = path_;
-            cost = cost_;
-        }
-        vector<T> path;
-        ulong cost;
+       private:
+        const T source;
+        const Distances<T> distances;
+        const Paths<T> paths;
     };
+    template <typename T>
+    static Result<T> execute(const T& start, const function<bool(T)> end,
+                             const function<unordered_set<T>(T)> adjacent,
+                             const function<ulong(T)> cost);
 
-    template<typename T>
-    static Result<T> execute(
-            const T& start,
-            const function<bool(T)> end,
-            const function<unordered_set<T>(T)> adjacent,
-            const function<ulong(T)> cost
-    ) {
-        priority_queue<State<T>, deque<State<T>>, greater<State<T>>> q;
-        q.push(State<T>(start, 0));
-        unordered_map<T, ulong> best({{start, 0}});
-        unordered_map<T, T> parent;
-        while (!q.empty()) {
-            const State<T> state = q.top();
-            q.pop();
-            if (end(state.node)) {
-                return Result<T>(createPath(parent, state.node), state.cost);
-            }
-            const ulong total = getOrDefault(best, state.node, LONG_MAX);
-            for (const T& n : adjacent(state.node)) {
-                const ulong newRisk = total + cost(n);
-                if (newRisk < getOrDefault(best, n, LONG_MAX)) {
-                    best[n] = newRisk;
-                    parent.insert({n, state.node});
-                    q.push(State<T>(n, newRisk));
-                }
+   private:
+    template <typename T>
+    static ulong getOrDefault(const Distances<T>& map, const T& node,
+                              const ulong defValue);
+};
+
+template <typename T>
+vector<T> AStar::Result<T>::getPath(const T& t) const {
+    vector<T> p;
+    T parent = t;
+    if (t != source) {
+        while (parent != source) {
+            p.insert(p.begin(), parent);
+            parent = paths[parent];
+        }
+        p.insert(p.begin(), source);
+    } else {
+        p.push_back(source);
+    }
+    return p;
+}
+
+template <typename T>
+AStar::Result<T> AStar::execute(const T& start, const function<bool(T)> end,
+                                const function<unordered_set<T>(T)> adjacent,
+                                const function<ulong(T)> cost) {
+    priority_queue<State<T>, deque<State<T>>, greater<State<T>>> q;
+    q.push(State<T>(start, 0));
+    Distances<T> best({{start, 0}});
+    Paths<T> parent;
+    while (!q.empty()) {
+        const State<T> state = q.top();
+        q.pop();
+        if (end(state.node)) {
+            break;
+        }
+        const ulong total = getOrDefault(best, state.node, LONG_MAX);
+        for (const T& n : adjacent(state.node)) {
+            const ulong newRisk = total + cost(n);
+            if (newRisk < getOrDefault(best, n, LONG_MAX)) {
+                best[n] = newRisk;
+                parent.insert({n, state.node});
+                q.push(State<T>(n, newRisk));
             }
         }
-        throw "Unsolvable";
     }
-};
-#endif //_AOC_GRAPH_HPP_
+    return Result<T>(start, best, parent);
+}
+
+template <typename T>
+ulong AStar::getOrDefault(const Distances<T>& map, const T& node,
+                          const ulong defValue) {
+    auto it = map.find(node);
+    return it == map.end() ? defValue : it->second;
+}
+
+#endif  //_AOC_GRAPH_HPP_
