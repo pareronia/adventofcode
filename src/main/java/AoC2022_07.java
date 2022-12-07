d2c533f67615a2a30382577e8aa069f68632eaad
pareronia <49491686+pareronia@users.noreply.github.com>
2022-12-07 21:25:53 +0100

    AoC 2022 Day 7 - java - cleanup

--- src/main/java/AoC2022_07.java
+++ src/main/java/AoC2022_07.java
@@ -1,5 +1,5 @@
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.summingInt;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -11,52 +11,50 @@ import java.util.Map;
 import com.github.pareronia.aocd.Aocd;
 import com.github.pareronia.aocd.Puzzle;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 public class AoC2022_07 extends AoCBase {
     
-    private final Map<List<String>, Node> nodes;
+    private static final String ROOT = "/";
+    
+    private final Map<String, Node> nodes;
     
     private AoC2022_07(final List<String> input, final boolean debug) {
         super(debug);
         this.nodes = new HashMap<>();
         final Deque<String> path = new ArrayDeque<>();
-        path.addLast("/");
-        Node curr = new Node(List.of("/"), null, 0);
-        nodes.put(curr.name, curr);
+        path.addLast(ROOT);
+        Node curr = new Node(ROOT, null, 0);
+        nodes.put(ROOT, curr);
         for (final String line : input.subList(1, input.size())) {
-            if (line.startsWith("$ cd ")) {
+            if (line.equals("$ ls")) {
+                continue;
+            } else if (line.startsWith("$ cd ")) {
                 final String name = line.substring("$ cd ".length());
                 if (name.equals("..")) {
                     curr = curr.parent;
                     path.removeLast();
                 } else {
                     path.addLast(name);
-                    curr = nodes.get(path.stream().collect(toList()));
+                    curr = nodes.get(path.stream().collect(joining("/")));
                 }
-            } else if (line.equals("$ ls")) {
-                continue;
             } else {
                 final String[] splits = line.split(" ");
+                final int size;
                 if (splits[0].equals("dir")) {
-                    final String name = splits[1];
-                    path.addLast(name);
-                    final Node node = new Node(path.stream().collect(toList()), curr, 0);
-                    curr.addChild(node);
-                    assert !nodes.containsKey(node.name);
-                    nodes.put(node.name, node);
-                    path.removeLast();
+                    size = 0;
                 } else {
-                    final String name = splits[1];
-                    path.addLast(name);
-                    final long size = Long.parseLong(splits[0]);
-                    final Node node = new Node(path.stream().collect(toList()), curr, size);
-                    curr.addChild(node);
-                    assert !nodes.containsKey(node.name);
-                    nodes.put(node.name, node);
-                    path.removeLast();
+                    size = Integer.parseInt(splits[0]);
                 }
+                path.addLast(splits[1]);
+                final String name = path.stream().collect(joining("/"));
+                final Node node = new Node(name, curr, size);
+                curr.addChild(node);
+                assert !nodes.containsKey(name);
+                nodes.put(name, node);
+                path.removeLast();
             }
         }
         log("nodes: " + nodes);
@@ -70,38 +68,40 @@ public class AoC2022_07 extends AoCBase {
         return new AoC2022_07(input, true);
     }
     
-    private long getSize(final Node node) {
+    private int getSize(final Map<String, Integer> map, final Node node) {
         if (node.children.isEmpty()) {
             return node.size;
+        } else if (map.containsKey(node.name)) {
+            return map.get(node.name);
         }
-        long size = 0;
-        for (final Node c : node.children) {
-            size += getSize(c);
-        }
+        final int size = node.children.stream()
+                .mapToInt(n -> getSize(map, n))
+                .sum();
+        map.put(node.name, size);
         return size;
+    }
+
+    private Map<String, Integer> directoriesWithSizes() {
+        final Map<String, Integer> map = new HashMap<>();
+        getSize(map, nodes.get(ROOT));
+        return map;
     }
     
     @Override
-    public Long solvePart1() {
-        return this.nodes.keySet().stream()
-            .map(this.nodes::get)
-            .filter(n -> !n.children.isEmpty())
-            .mapToLong(this::getSize)
-            .filter(s -> s <= 100_000)
-            .sum();
+    public Integer solvePart1() {
+        return directoriesWithSizes().values().stream()
+                .filter(s -> s <= 100_000)
+                .collect(summingInt(Integer::valueOf));
     }
 
     @Override
-    public Long solvePart2() {
-        final Map<List<String>, Long> map = this.nodes.keySet().stream()
-            .map(this.nodes::get)
-            .filter(n -> !n.children.isEmpty())
-            .collect(toMap(n -> n.name, this::getSize));
-        final long total = map.get(List.of("/"));
-        final long wanted = 30_000_000 - (70_000_000 - total);
+    public Integer solvePart2() {
+        final Map<String, Integer> map = directoriesWithSizes();
+        final int total = map.get(ROOT);
+        final int wanted = 30_000_000 - (70_000_000 - total);
         return map.values().stream()
-                .sorted()
                 .filter(v -> v >= wanted)
+                .sorted()
                 .findFirst().orElseThrow();
     }
 
@@ -118,39 +118,40 @@ public class AoC2022_07 extends AoCBase {
     }
 
     private static final List<String> TEST = splitLines(
-        "$ cd /\r\n"
-        + "$ ls\r\n"
-        + "dir a\r\n"
-        + "14848514 b.txt\r\n"
-        + "8504156 c.dat\r\n"
-        + "dir d\r\n"
-        + "$ cd a\r\n"
-        + "$ ls\r\n"
-        + "dir e\r\n"
-        + "29116 f\r\n"
-        + "2557 g\r\n"
-        + "62596 h.lst\r\n"
-        + "$ cd e\r\n"
-        + "$ ls\r\n"
-        + "584 i\r\n"
-        + "$ cd ..\r\n"
-        + "$ cd ..\r\n"
-        + "$ cd d\r\n"
-        + "$ ls\r\n"
-        + "4060174 j\r\n"
-        + "8033020 d.log\r\n"
-        + "5626152 d.ext\r\n"
-        + "7214296 k"
+        "$ cd /\r\n" +
+        "$ ls\r\n" +
+        "dir a\r\n" +
+        "14848514 b.txt\r\n" +
+        "8504156 c.dat\r\n" +
+        "dir d\r\n" +
+        "$ cd a\r\n" +
+        "$ ls\r\n" +
+        "dir e\r\n" +
+        "29116 f\r\n" +
+        "2557 g\r\n" +
+        "62596 h.lst\r\n" +
+        "$ cd e\r\n" +
+        "$ ls\r\n" +
+        "584 i\r\n" +
+        "$ cd ..\r\n" +
+        "$ cd ..\r\n" +
+        "$ cd d\r\n" +
+        "$ ls\r\n" +
+        "4060174 j\r\n" +
+        "8033020 d.log\r\n" +
+        "5626152 d.ext\r\n" +
+        "7214296 k"
     );
     
     @RequiredArgsConstructor
     @ToString(onlyExplicitlyIncluded = true)
     private static final class Node {
         @ToString.Include
-        private final List<String> name;
+        @Getter
+        private final String name;
         private final Node parent;
         @ToString.Include
-        private final long size;
+        private final int size;
         private final List<Node> children = new ArrayList<>();
         
         public void addChild(final Node node) {
