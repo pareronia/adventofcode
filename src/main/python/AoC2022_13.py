fa019b377cfdca3c7fdf53e9d6b0140883cea1b3
pareronia <49491686+pareronia@users.noreply.github.com>
2022-12-13 11:22:47 +0000

    AoC 2022 Day 13

--- src/main/python/AoC2022_13.py
+++ src/main/python/AoC2022_13.py
@@ -3,68 +3,45 @@
 # Advent of Code 2022 Day 13
 #
 
+from functools import cmp_to_key
 
 import aocd
 from aoc import my_aocd
 from aoc.common import log
 
 
-def _has_list(r: list) -> bool:
-    try:
-        return sum(1 for _ in r if type(_) == list) > 0
-    except TypeError:
-        return False
-
-
-def _compare(r1, r2):
-    # breakpoint()
+def _compare(r1, r2) -> int:
     log(f">Compare {r1} vs {r2}")
-    # size1 = len(r1)
-    # size2 = len(r2)
-    # if size1 < size2:
-    #     log("Right side ran out of numbers")
-    #     return False
-    # for i in range(size2):
-    #     if type(r1[i]) != list:
-    #         if type(r2[i]) == list:
-    #             log("Mixed types; convert left to list and retry comparison")
-    #             if not _compare([r1[i]], r2[i]):
-    #                 return False
-    #     elif type(r2[i]) != list:
-    #         log("Mixed types; convert right to list and retry comparison")
-    #         if not _compare(r1[i], [r2[i]]):
-    #             return False
-    #     else:
-    #         log(f"Compare lists {r1[i]} vs {r2[i]}")
-    #         if not _compare(r1[i], r2[i]):
-    #             return False
     if type(r1) != list and type(r2) != list:
-        if r1 > r2:
-            return False
+        if r1 == r2:
+            return 0
+        elif r1 < r2:
+            return -1
+        else:
+            return 1
     elif type(r1) == list and type(r2) != list:
         log("Mixed types; convert right to list and retry comparison")
-        if not _compare(r1, [r2]):
-            return False
+        return _compare(r1, [r2])
     elif type(r1) != list and type(r2) == list:
         log("Mixed types; convert left to list and retry comparison")
-        if not _compare([r1], r2):
-            return False
+        return _compare([r1], r2)
     else:
         log(f"Compare lists {r1} vs {r2}")
         size1 = len(r1)
         size2 = len(r2)
-        for i in range(max(size1, size2)):
-            if i == size1 and i <= size2:
-                log("Left side ran out of numbers")
-                return True
-            elif i == size2 and i <= size1:
-                log("Right side ran out of numbers")
-                return False
-            else:
-                log(f"Compare {r1[i]} vs {r2[i]}")
-                if not _compare(r1[i], r2[i]):
-                    return False
-    return True
+        for i in range(size1):
+            n1 = r1[i]
+            try:
+                n2 = r2[i]
+            except IndexError:
+                log("Right side ran out of items")
+                return 1
+            res = _compare(n1, n2)
+            if res == 0:
+                continue
+            return res
+        log("Left side ran out of items")
+        return -1 if size1 < size2 else 0
 
 
 def part_1(inputs: tuple[str]) -> int:
@@ -74,19 +51,24 @@ def part_1(inputs: tuple[str]) -> int:
         log(r1)
         r2 = eval(block[1])  # nosec
         log(r2)
-        if _compare(r1, r2):
+        if _compare(r1, r2) <= 0:
             log(True)
-            ans += (i + 1)
+            ans += i + 1
         else:
             log(False)
     return ans
 
 
 def part_2(inputs: tuple[str]) -> int:
-    return 0
+    s = [eval(_) for _ in inputs if len(_) > 0]  # nosec
+    s.append(eval("[[2]]"))  # nosec
+    s.append(eval("[[6]]"))  # nosec
+    s.sort(key=cmp_to_key(_compare))
+    return (s.index([[2]]) + 1) * (s.index([[6]]) + 1)
 
 
-TEST1 = """\
+TEST1 = tuple(
+    """\
 [1,1,3,1,1]
 [1,1,5,1,1]
 
@@ -111,10 +93,13 @@ TEST1 = """\
 [1,[2,[3,[4,[5,6,7]]]],8,9]
 [1,[2,[3,[4,[5,6,0]]]],8,9]
 """.splitlines()
-TEST2 = """\
+)
+TEST2 = tuple(
+    """\
 [[1],[2,3,4]]
 [[1],4]
 """.splitlines()
+)
 
 
 def main() -> None:
@@ -122,9 +107,8 @@ def main() -> None:
     my_aocd.print_header(puzzle.year, puzzle.day)
 
     assert part_1(TEST1) == 13
-    assert part_1(TEST2) == 0
-    # assert part_2(TEST1) == 0
-    return
+    assert part_1(TEST2) == 1
+    assert part_2(TEST1) == 140
 
     inputs = my_aocd.get_input_data(puzzle, 449)
     result1 = part_1(inputs)
