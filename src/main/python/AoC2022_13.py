9423103d59e6f8b4263934ac60aa3fa694b6433b
pareronia <49491686+pareronia@users.noreply.github.com>
2022-12-13 14:48:46 +0000

    AoC 2022 Day 13 - cleanup

--- src/main/python/AoC2022_13.py
+++ src/main/python/AoC2022_13.py
@@ -4,6 +4,7 @@
 #
 
 from functools import cmp_to_key
+from math import prod
 
 import aocd
 from aoc import my_aocd
@@ -12,20 +13,15 @@ from aoc.common import log
 
 def _compare(r1, r2) -> int:
     log(f">Compare {r1} vs {r2}")
-    if type(r1) != list and type(r2) != list:
-        if r1 == r2:
-            return 0
-        elif r1 < r2:
-            return -1
-        else:
-            return 1
-    elif type(r1) == list and type(r2) != list:
+    if isinstance(r1, int) and isinstance(r2, int):
+        return -1 if r1 < r2 else 1 if r1 > r2 else 0
+    elif isinstance(r1, list) and isinstance(r2, int):
         log("Mixed types; convert right to list and retry comparison")
         return _compare(r1, [r2])
-    elif type(r1) != list and type(r2) == list:
+    elif isinstance(r1, int) and isinstance(r2, list):
         log("Mixed types; convert left to list and retry comparison")
         return _compare([r1], r2)
-    else:
+    elif isinstance(r1, list) and isinstance(r2, list):
         log(f"Compare lists {r1} vs {r2}")
         size1 = len(r1)
         size2 = len(r2)
@@ -42,32 +38,28 @@ def _compare(r1, r2) -> int:
             return res
         log("Left side ran out of items")
         return -1 if size1 < size2 else 0
+    raise ValueError
 
 
 def part_1(inputs: tuple[str]) -> int:
-    ans = 0
-    for i, block in enumerate(my_aocd.to_blocks(inputs)):
-        r1 = eval(block[0])  # nosec
-        log(r1)
-        r2 = eval(block[1])  # nosec
-        log(r2)
-        if _compare(r1, r2) <= 0:
-            log(True)
-            ans += i + 1
-        else:
-            log(False)
-    return ans
+    return sum(
+        i + 1
+        for i, block in enumerate(my_aocd.to_blocks(inputs))
+        if _compare(eval(block[0]), eval(block[1])) < 1  # nosec
+    )
 
 
 def part_2(inputs: tuple[str]) -> int:
-    s = [eval(_) for _ in inputs if len(_) > 0]  # nosec
-    s.append(eval("[[2]]"))  # nosec
-    s.append(eval("[[6]]"))  # nosec
-    s.sort(key=cmp_to_key(_compare))
-    return (s.index([[2]]) + 1) * (s.index([[6]]) + 1)
+    dividers = [[[2]], [[6]]]
+    packets = [eval(line) for line in inputs if len(line) > 0]  # nosec
+    packets.extend(dividers)
+    packets.sort(key=cmp_to_key(_compare))
+    return prod(
+        i + 1 for i, packet in enumerate(packets) if packet in dividers
+    )
 
 
-TEST1 = tuple(
+TEST = tuple(
     """\
 [1,1,3,1,1]
 [1,1,5,1,1]
@@ -94,21 +86,14 @@ TEST1 = tuple(
 [1,[2,[3,[4,[5,6,0]]]],8,9]
 """.splitlines()
 )
-TEST2 = tuple(
-    """\
-[[1],[2,3,4]]
-[[1],4]
-""".splitlines()
-)
 
 
 def main() -> None:
     puzzle = aocd.models.Puzzle(2022, 13)
     my_aocd.print_header(puzzle.year, puzzle.day)
 
-    assert part_1(TEST1) == 13
-    assert part_1(TEST2) == 1
-    assert part_2(TEST1) == 140
+    assert part_1(TEST) == 13
+    assert part_2(TEST) == 140
 
     inputs = my_aocd.get_input_data(puzzle, 449)
     result1 = part_1(inputs)
