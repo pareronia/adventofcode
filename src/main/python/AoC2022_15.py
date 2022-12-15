45a9d10ab63488d038e4ca60232fb52eca4fd479
pareronia <49491686+pareronia@users.noreply.github.com>
2022-12-15 21:30:56 +0100

    AoC 2022 Day 15 - smaller, faster

--- src/main/python/AoC2022_15.py
+++ src/main/python/AoC2022_15.py
@@ -20,56 +20,48 @@ def _parse(inputs: tuple[str]) -> tuple[Sensors, Beacons]:
     beacons = defaultdict[int, set[int]](set)
     for line in inputs:
         sx, sy, bx, by = map(int, re.findall(r"-?[0-9]+", line))
-        sensors.append((sx, sy, _distance(sx, sy, bx, by)))
+        sensors.append((sx, sy, abs(sx - bx) + abs(sy - by)))
         beacons[by].add(bx)
     return sensors, beacons
 
 
-def _distance(x1, y1, x2, y2) -> int:
-    return abs(x1 - x2) + abs(y1 - y2)
-
-
-def _get_ranges(sensors: Sensors, beacons: Beacons, y: int) -> list[Range]:
-    ranges = set[Range]()
+def _get_ranges(sensors: Sensors, y: int) -> list[Range]:
+    ranges = list[Range]()
     for sx, sy, d in sensors:
         dy = abs(sy - y)
         if dy > d:
             continue
-        ranges.add((sx - d + dy, sx + d - dy))
-    return _merge_ranges(ranges)
-
-
-def _merge_ranges(ranges: set[Range]) -> list[Range]:
-    m = list[Range]()
-    for s_min, s_max in sorted(r for r in ranges):
-        if len(m) == 0:
-            m.append((s_min, s_max))
+        ranges.append((sx - d + dy, sx + d - dy))
+    merged = list[Range]()
+    for s_min, s_max in sorted(ranges):
+        if len(merged) == 0:
+            merged.append((s_min, s_max))
             continue
-        last_min, last_max = m[-1]
+        last_min, last_max = merged[-1]
         if s_min <= last_max:
-            m[-1] = (last_min, max(last_max, s_max))
+            merged[-1] = (last_min, max(last_max, s_max))
             continue
-        m.append((s_min, s_max))
-    return m
+        merged.append((s_min, s_max))
+    return merged
 
 
 def solve_1(inputs: tuple[str], y: int) -> int:
     sensors, beacons = _parse(inputs)
-    ans = 0
-    for r_min, r_max in _get_ranges(sensors, beacons, y):
-        ans += (
+    return sum(
+        (
             r_max
             - r_min
             + 1
             - sum(1 for bx in beacons[y] if r_min <= bx <= r_max)
         )
-    return ans
+        for r_min, r_max in _get_ranges(sensors, y)
+    )
 
 
 def solve_2(inputs: tuple[str], the_max: int) -> int:
-    sensors, beacons = _parse(inputs)
-    for y in range(the_max + 1):
-        ranges = _get_ranges(sensors, beacons, y)
+    sensors, _ = _parse(inputs)
+    for y in range(the_max, 0, -1):
+        ranges = _get_ranges(sensors, y)
         x = 0
         while x <= the_max:
             for r_min, r_max in ranges:
@@ -87,7 +79,8 @@ def part_2(inputs: tuple[str]) -> int:
     return solve_2(inputs, 4_000_000)
 
 
-TEST = """\
+TEST = tuple(
+    """\
 Sensor at x=2, y=18: closest beacon is at x=-2, y=15
 Sensor at x=9, y=16: closest beacon is at x=10, y=16
 Sensor at x=13, y=2: closest beacon is at x=15, y=3
@@ -103,10 +96,10 @@ Sensor at x=16, y=7: closest beacon is at x=15, y=3
 Sensor at x=14, y=3: closest beacon is at x=15, y=3
 Sensor at x=20, y=1: closest beacon is at x=15, y=3
 """.splitlines()
+)
 
 
 def main() -> None:
-    aocd.get_data(year=2022, day=15, block=True)
     puzzle = aocd.models.Puzzle(2022, 15)
     my_aocd.print_header(puzzle.year, puzzle.day)
 
