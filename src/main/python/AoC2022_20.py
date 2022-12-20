afc9d3515212f63cfde859daba04f6c3aa1e4ce1
pareronia <49491686+pareronia@users.noreply.github.com>
2022-12-20 22:19:17 +0100

    AoC 2022 Day 20 - refactor

--- src/main/python/AoC2022_20.py
+++ src/main/python/AoC2022_20.py
@@ -6,12 +6,9 @@
 
 from __future__ import annotations
 
-
 import aocd
 from aoc import my_aocd
-
 from aoc.common import clog
-from aoc.common import log
 
 
 class Num:
@@ -21,25 +18,7 @@ class Num:
         self.prev: Num | None = None
 
 
-def _parse(inputs: tuple[str]) -> tuple[dict[int, Num], Num]:
-    pos = dict[int, Num]()
-    for i, num in enumerate(list(map(int, inputs))):
-        n = Num(num)
-        if num == 0:
-            zero = n
-        pos[i] = n
-        if i == 0:
-            head = n
-        else:
-            n.prev = pos[i - 1]
-            n.prev.next = n
-            tail = n
-    head.prev = tail
-    tail.next = head
-    return pos, zero
-
-
-def _print(zero) -> None:
+def _print(zero: Num) -> None:
     tmp = zero
     s = []
     while True:
@@ -50,21 +29,17 @@ def _print(zero) -> None:
     return s
 
 
-def _do_mix(pos, zero) -> None:
-    def _do_move(to_move, move_to) -> None:
+def _do_mix(nums: list[Num], zero: Num) -> None:
+    def _do_move(to_move: Num, move_to: Num) -> None:
         before, after = move_to, move_to.next
         before.next = to_move
         to_move.prev = before
         after.prev = to_move
         to_move.next = after
 
-    size = len(pos)
-    for i in range(size):
-        to_move = pos[i]
+    size = len(nums)
+    for to_move in nums:
         clog(lambda: f"to_move: {to_move.num}")
-        if to_move.num == 0:
-            log("Nothing to do")
-            continue
         if to_move.num > 0:
             move_to = to_move.prev
             amount = to_move.num % (size - 1)
@@ -74,7 +49,7 @@ def _do_mix(pos, zero) -> None:
                 move_to = move_to.next
             clog(lambda: f"move to: {move_to.num}")
             _do_move(to_move, move_to)
-        else:
+        elif to_move.num < 0:
             move_to = to_move.next
             amount = abs(to_move.num) % (size - 1) + 1
             to_move.next.prev = to_move.prev
@@ -86,9 +61,16 @@ def _do_mix(pos, zero) -> None:
         clog(lambda: _print(zero))
 
 
-def _solve(pos: dict[int, Num], zero: Num, rounds: int) -> int:
+def _solve(inputs: tuple[str], rounds: int, factor: int = 1) -> int:
+    nums = list(map(lambda line: Num(int(line) * factor), inputs))
+    size = len(nums)
+    for i in range(1, size + 1):
+        if nums[i - 1].num == 0:
+            zero = nums[i - 1]
+        nums[i % size].prev = nums[(i - 1) % size]
+        nums[i % size].next = nums[(i + 1) % size]
     for i in range(rounds):
-        _do_mix(pos, zero)
+        _do_mix(nums, zero)
     ans = 0
     n = zero
     for i in range(1, 3001):
@@ -99,15 +81,11 @@ def _solve(pos: dict[int, Num], zero: Num, rounds: int) -> int:
 
 
 def part_1(inputs: tuple[str]) -> int:
-    pos, zero = _parse(inputs)
-    return _solve(pos, zero, 1)
+    return _solve(inputs, rounds=1)
 
 
 def part_2(inputs: tuple[str]) -> int:
-    pos, zero = _parse(inputs)
-    for i in pos:
-        pos[i].num *= 811_589_153
-    return _solve(pos, zero, 10)
+    return _solve(inputs, rounds=10, factor=811_589_153)
 
 
 TEST1 = tuple(
