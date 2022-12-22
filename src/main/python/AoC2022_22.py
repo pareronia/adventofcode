776beda7ec10f7b3a0694761023c45c43c175496
pareronia <49491686+pareronia@users.noreply.github.com>
2022-12-22 17:30:12 +0100

    AoC 2022 Day 22 Part 2

--- src/main/python/AoC2022_22.py
+++ src/main/python/AoC2022_22.py
@@ -2,6 +2,7 @@
 #
 # Advent of Code 2022 Day 22
 #
+# TODO: make generalized part2 solution that works on sample input as well
 
 
 import re
@@ -10,8 +11,14 @@ import aocd
 from aoc import my_aocd
 from aoc.common import log
 
+DIRS = {"N": (-1, 0), "E": (0, 1), "S": (1, 0), "W": (0, -1)}
+TURNS = {
+    "R": {"N": "E", "E": "S", "S": "W", "W": "N"},
+    "L": {"N": "W", "E": "N", "S": "E", "W": "S"},
+}
 
-def _parse(inputs: tuple[str]):
+
+def _parse(inputs: tuple[str]) -> tuple[list[str], list[tuple[str, int]]]:
     blocks = my_aocd.to_blocks(inputs)
     moves = [
         (a, int(b))
@@ -20,25 +27,33 @@ def _parse(inputs: tuple[str]):
     return blocks[0], moves
 
 
+def _ans(r: int, c: int, facing: str) -> int:
+    f = (
+        0
+        if facing == "E"
+        else 1
+        if facing == "S"
+        else 2
+        if facing == "W"
+        else 3
+    )
+    return 1000 * (r + 1) + 4 * (c + 1) + f
+
+
 def part_1(inputs: tuple[str]) -> int:
     grid, moves = _parse(inputs)
-    start = 0, next(c for c, _ in enumerate(grid[0]) if _ != " ")
+    size = len(grid) // 3
+    start = 0, 2 * size
     log(start)
     r, c = start
     facing = "N"
     for turn, steps in moves:
-        if facing == "N":
-            facing = "E" if turn == "R" else "W"
-        elif facing == "E":
-            facing = "S" if turn == "R" else "N"
-        elif facing == "S":
-            facing = "W" if turn == "R" else "E"
-        elif facing == "W":
-            facing = "N" if turn == "R" else "S"
+        facing = TURNS[turn][facing]
         for i in range(steps):
-            if facing == "N":
-                rr = r - 1
-                cc = c
+            dr, dc = DIRS[facing]
+            rr = r + dr
+            cc = c + dc
+            if facing == "N" or facing == "S":
                 rows = [
                     r
                     for r, _ in enumerate(
@@ -54,39 +69,7 @@ def part_1(inputs: tuple[str]) -> int:
                     break
                 else:
                     r, c = rr, cc
-            elif facing == "E":
-                rr = r
-                cc = c + 1
-                cols = [c for c, _ in enumerate(grid[r]) if _ != " "]
-                if cc < cols[0]:
-                    cc = cols[-1]
-                elif cc > cols[-1]:
-                    cc = cols[0]
-                if grid[rr][cc] == "#":
-                    break
-                else:
-                    r, c = rr, cc
-            elif facing == "S":
-                rr = r + 1
-                cc = c
-                rows = [
-                    r
-                    for r, _ in enumerate(
-                        row[c] for row in [row for row in grid if c < len(row)]
-                    )
-                    if _ != " "
-                ]
-                if rr < rows[0]:
-                    rr = rows[-1]
-                elif rr > rows[-1]:
-                    rr = rows[0]
-                if grid[rr][cc] == "#":
-                    break
-                else:
-                    r, c = rr, cc
-            elif facing == "W":
-                rr = r
-                cc = c - 1
+            elif facing == "E" or facing == "W":
                 cols = [c for c, _ in enumerate(grid[r]) if _ != " "]
                 if cc < cols[0]:
                     cc = cols[-1]
@@ -97,25 +80,208 @@ def part_1(inputs: tuple[str]) -> int:
                 else:
                     r, c = rr, cc
             log((r, c))
-    f = (
-        0
-        if facing == "E"
-        else 1
-        if facing == "S"
-        else 2
-        if facing == "W"
-        else 3
-    )
-    ans = 1000 * (r + 1) + 4 * (c + 1) + f
-    log(ans)
-    return ans
+    return _ans(r, c, facing)
 
 
 def part_2(inputs: tuple[str]) -> int:
-    return 0
+    grid, moves = _parse(inputs)
+    start = 0, next(c for c, _ in enumerate(grid[0]) if _ != " ")
+    log(start)
+    r, c = start
+    facing = "N"
+    for turn, steps in moves:
+        facing = TURNS[turn][facing]
+        for i in range(steps):
+            # assert cnt < 10
+            dr, dc = DIRS[facing]
+            log((r, c, dr, dc))
+            rr = r + dr
+            cc = c + dc
+            ff = facing
+            # top edge 1
+            if rr == -1 and 50 <= cc < 100:
+                assert ff == "N", ((r, c, facing), (rr, cc))
+                rr = cc + 100
+                cc = 0
+                assert rr >= 0 and cc >= 0, ((r, c, facing), (rr, cc))
+                # assert 6
+                assert 150 <= rr < 200 and 0 <= cc < 50, (
+                    (r, c, facing),
+                    (rr, cc),
+                )
+                ff = "E"
+            # left edge 1
+            elif cc == 49 and 0 <= rr < 50:
+                assert ff == "W", ((r, c, facing), (rr, cc))
+                rr = 149 - rr
+                cc = 0
+                assert rr >= 0 and cc >= 0, ((r, c, facing), (rr, cc))
+                # assert 4
+                assert 100 <= rr < 150 and 0 <= cc < 50, (
+                    (r, c, facing),
+                    (rr, cc),
+                )
+                ff = "E"
+            # top edge 2
+            elif rr == -1 and 100 <= cc < 150:
+                assert ff == "N", ((r, c, facing), (rr, cc))
+                rr = 199
+                cc -= 100
+                assert rr >= 0 and cc >= 0, ((r, c, facing), (rr, cc))
+                # assert 6
+                assert 150 <= rr < 200 and 0 <= cc < 50, (
+                    (r, c, facing),
+                    (rr, cc),
+                )
+                ff = "N"
+            # right edge 2
+            elif cc == 150 and 0 <= rr < 50:
+                assert ff == "E", ((r, c, facing), (rr, cc))
+                rr = 149 - rr
+                cc = 99
+                assert rr >= 0 and cc >= 0, ((r, c, facing), (rr, cc))
+                # assert 5
+                assert 100 <= rr < 150 and 50 <= cc < 100, (
+                    (r, c, facing),
+                    (rr, cc),
+                )
+                ff = "W"
+            # bottom edge 2
+            elif rr == 50 and 100 <= cc < 150 and ff == "S":
+                assert ff == "S", ((r, c, facing), (rr, cc))
+                rr = cc - 50
+                cc = 99
+                assert rr >= 0 and cc >= 0, ((r, c, facing), (rr, cc))
+                # assert 3
+                assert 50 <= rr < 100 and 50 <= cc < 100, (
+                    (r, c, facing),
+                    (rr, cc),
+                )
+                ff = "W"
+            # left edge 3
+            elif cc == 49 and 50 <= rr < 100:
+                assert ff == "W", ((r, c, facing), (rr, cc))
+                cc = rr - 50
+                rr = 100
+                assert rr >= 0 and cc >= 0, ((r, c, facing), (rr, cc))
+                # assert 4
+                assert 100 <= rr < 150 and 0 <= cc < 50, (
+                    (r, c, facing),
+                    (rr, cc),
+                )
+                ff = "S"
+            # right edge 3
+            elif cc == 100 and 50 <= rr < 100 and ff == "E":
+                assert ff == "E", ((r, c, facing), (rr, cc))
+                cc = rr + 50
+                rr = 49
+                assert rr >= 0 and cc >= 0, ((r, c, facing), (rr, cc))
+                # assert 2
+                assert 0 <= rr < 50 and 100 <= cc < 150, (
+                    (r, c, facing),
+                    (rr, cc),
+                )
+                ff = "N"
+            # left edge 4
+            elif cc == -1 and 100 <= rr < 150:
+                assert ff == "W", ((r, c, facing), (rr, cc))
+                rr = 149 - rr
+                cc = 50
+                assert rr >= 0 and cc >= 0, ((r, c, facing), (rr, cc))
+                # assert 1
+                assert 0 <= rr < 50 and 50 <= cc < 100, (
+                    (r, c, facing),
+                    (rr, cc),
+                )
+                ff = "E"
+            # top edge 4
+            elif rr == 99 and 0 <= cc < 50:
+                assert ff == "N", ((r, c, facing), (rr, cc))
+                rr = cc + 50
+                cc = 50
+                assert rr >= 0 and cc >= 0, ((r, c, facing), (rr, cc))
+                # assert 3
+                assert 50 <= rr < 100 and 50 <= cc < 100, (
+                    (r, c, facing),
+                    (rr, cc),
+                )
+                ff = "E"
+            # right edge 5
+            elif cc == 100 and 100 <= rr < 150:
+                assert ff == "E", ((r, c, facing), (rr, cc))
+                rr = 149 - rr
+                cc = 149
+                assert rr >= 0 and cc >= 0, ((r, c, facing), (rr, cc))
+                # assert 2
+                assert 0 <= rr < 50 and 100 <= cc < 150, (
+                    (r, c, facing),
+                    (rr, cc),
+                )
+                ff = "W"
+            # bottom edge 5
+            elif rr == 150 and 50 <= cc < 100 and ff == "S":
+                assert ff == "S", ((r, c, facing), (rr, cc))
+                rr = 100 + cc
+                cc = 49
+                assert rr >= 0 and cc >= 0, ((r, c, facing), (rr, cc))
+                # assert 6
+                assert 150 <= rr < 200 and 0 <= cc < 50, (
+                    (r, c, facing),
+                    (rr, cc),
+                )
+                ff = "W"
+            # left edge 6
+            elif cc == -1 and 150 <= rr < 200:
+                assert ff == "W", ((r, c, facing), (rr, cc))
+                cc = rr - 100
+                rr = 0
+                assert rr >= 0 and cc >= 0, ((r, c, facing), (rr, cc))
+                # assert 1
+                assert 0 <= rr < 50 and 50 <= cc < 100, (
+                    (r, c, facing),
+                    (rr, cc),
+                )
+                ff = "S"
+            # right edge 6
+            elif cc == 50 and 150 <= rr < 200 and ff == "E":
+                assert ff == "E", ((r, c, facing), (rr, cc))
+                cc = rr - 100
+                rr = 149
+                assert rr >= 0 and cc >= 0, ((r, c, facing), (rr, cc))
+                # assert 5
+                assert 100 <= rr < 150 and 50 <= cc < 100, (
+                    (r, c, facing),
+                    (rr, cc),
+                )
+                ff = "N"
+            # bottom edge 6
+            elif rr == 200 and 0 <= cc < 50:
+                assert ff == "S", ((r, c, facing), (rr, cc))
+                rr = 0
+                cc = cc + 100
+                assert rr >= 0 and cc >= 0, ((r, c, facing), (rr, cc))
+                # assert 2
+                assert 0 <= rr < 50 and 100 <= cc < 150, (
+                    (r, c, facing),
+                    (rr, cc),
+                )
+                ff = "S"
+            try:
+                val = grid[rr][cc]
+            except IndexError as e:
+                log(((r, c, facing), (rr, cc)))
+                raise e
+            if val == "#":
+                break
+            else:
+                r = rr
+                c = cc
+                facing = ff
+    return _ans(r, c, facing)
 
 
-TEST = """\
+TEST = tuple(
+    """\
         ...#
         .#..
         #...
@@ -131,6 +297,7 @@ TEST = """\
 
 10R5L5R10L4R5L5
 """.splitlines()
+)
 
 
 def main() -> None:
@@ -139,7 +306,7 @@ def main() -> None:
     my_aocd.print_header(puzzle.year, puzzle.day)
 
     assert part_1(TEST) == 6032
-    assert part_2(TEST) == 0
+    # assert part_2(TEST) == 5031
 
     inputs = my_aocd.get_input_data(puzzle, 202)
     result1 = part_1(inputs)
