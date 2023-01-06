4827ecdf71a62bfb28b9777a6a624c8ecb8142b0
pareronia <49491686+pareronia@users.noreply.github.com>
2023-01-06 17:46:39 +0100

    java - clean up BFS

--- src/main/java/com/github/pareronia/aoc/graph/BFS.java
+++ src/main/java/com/github/pareronia/aoc/graph/BFS.java
@@ -7,38 +7,36 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
-import com.github.pareronia.aoc.Grid.Cell;
-
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public final class BFS {
     
-    public static int execute(
-            final Cell start,
-            final Predicate<Cell> isEnd,
-            final Function<Cell, Stream<Cell>> adjacent
+    public static <T> int execute(
+            final T start,
+            final Predicate<T> isEnd,
+            final Function<T, Stream<T>> adjacent
     ) {
-        final Deque<State> q = new ArrayDeque<>(Set.of(new State(start, 0)));
-        final Set<Cell> seen = new HashSet<>(Set.of(start));
+        final Deque<State<T>> q = new ArrayDeque<>(Set.of(new State<>(start, 0)));
+        final Set<T> seen = new HashSet<>(Set.of(start));
         while (!q.isEmpty()) {
-            final State state = q.poll();
-            if (isEnd.test(state.cell)) {
+            final State<T> state = q.poll();
+            if (isEnd.test(state.node)) {
                 return state.distance;
             }
-            adjacent.apply(state.cell)
+            adjacent.apply(state.node)
                 .filter(n -> !seen.contains(n))
                 .forEach(n -> {
                     seen.add(n);
-                    q.add(new State(n, state.distance + 1));
+                    q.add(new State<>(n, state.distance + 1));
                 });
         }
         throw new IllegalStateException("Unsolvable");
     }
 
     @RequiredArgsConstructor
-    private static final class State {
-        private final Cell cell;
+    private static final class State<T> {
+        private final T node;
         private final int distance;
     }
 }
