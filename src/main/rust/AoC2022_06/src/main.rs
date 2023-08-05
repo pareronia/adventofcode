3866a928a43c378dcf93ae3e6027b0767217ba8d
pareronia <49491686+pareronia@users.noreply.github.com>
2023-08-05 23:19:01 +0200

    AoC 2022 Day 6 - rust

---
+++ src/main/rust/AoC2022_06/src/main.rs
@@ -0,0 +1,66 @@
+#![allow(non_snake_case)]
+
+use aoc::Puzzle;
+use itertools::Itertools;
+
+struct AoC2022_06;
+
+impl AoC2022_06 {
+    fn solve(&self, line: &String, count: usize) -> usize {
+        for i in count..line.len() {
+            if line[i - count..i].chars().unique().count() == count {
+                return i;
+            }
+        }
+        panic!("Unsolvable");
+    }
+}
+
+impl aoc::Puzzle for AoC2022_06 {
+    type Input = String;
+    type Output1 = usize;
+    type Output2 = usize;
+
+    aoc::puzzle_year_day!(2022, 6);
+
+    fn parse_input(&self, lines: Vec<String>) -> String {
+        lines[0].to_string()
+    }
+
+    fn part_1(&self, input: &String) -> usize {
+        self.solve(input, 4)
+    }
+
+    fn part_2(&self, input: &String) -> usize {
+        self.solve(input, 14)
+    }
+
+    fn samples(&self) {
+        aoc::puzzle_samples! {
+            self, part_1, "mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7,
+            self, part_1, "bvwbjplbgvbhsrlpgdmjqwftvncz", 5,
+            self, part_1, "nppdvjthqldpwncqszvftbrmjlhg", 6,
+            self, part_1, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10,
+            self, part_1, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11,
+            self, part_2, "mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19,
+            self, part_2, "bvwbjplbgvbhsrlpgdmjqwftvncz", 23,
+            self, part_2, "nppdvjthqldpwncqszvftbrmjlhg", 23,
+            self, part_2, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29,
+            self, part_2, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26
+        };
+    }
+}
+
+fn main() {
+    AoC2022_06 {}.run(std::env::args());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn samples() {
+        AoC2022_06 {}.samples();
+    }
+}
