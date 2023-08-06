3856626cf1e9368be7c9e8056222ea3c45833085
pareronia <49491686+pareronia@users.noreply.github.com>
2023-08-06 14:54:17 +0200

    AoC 2022 Day 8 - rust

---
+++ src/main/rust/AoC2022_08/src/main.rs
@@ -0,0 +1,106 @@
+#![allow(non_snake_case)]
+
+use aoc::grid::*;
+use aoc::Puzzle;
+
+struct AoC2022_08;
+
+impl AoC2022_08 {
+    fn ignoring_borders(&self, grid: &IntGrid) -> Vec<Cell> {
+        grid.cells()
+            .filter(|cell| 1 <= cell.row)
+            .filter(|cell| cell.row < grid.height() - 1)
+            .filter(|cell| 1 <= cell.col)
+            .filter(|cell| cell.col < grid.width() - 1)
+            .collect()
+    }
+
+    fn visible_from_outside(&self, grid: &IntGrid, cell: &Cell) -> bool {
+        let val = grid.get(cell);
+        grid.cells_capital_directions(cell)
+            .iter()
+            .any(|dir| dir.iter().all(|cell| grid.get(cell) < val))
+    }
+
+    fn viewing_distance(
+        &self,
+        grid: &IntGrid,
+        direction: &Vec<Cell>,
+        val: u32,
+    ) -> u32 {
+        let mut n = 0;
+        let mut stop = false;
+        for cell in direction {
+            if stop {
+                break;
+            }
+            n += 1;
+            stop = grid.get(cell) >= val;
+        }
+        n
+    }
+
+    fn scenic_score(&self, grid: &IntGrid, cell: &Cell) -> u32 {
+        grid.cells_capital_directions(cell)
+            .iter()
+            .map(|dir| self.viewing_distance(grid, dir, grid.get(cell)))
+            .fold(1, |acc, val| acc * val)
+    }
+}
+
+impl aoc::Puzzle for AoC2022_08 {
+    type Input = IntGrid;
+    type Output1 = usize;
+    type Output2 = u32;
+
+    aoc::puzzle_year_day!(2022, 8);
+
+    fn parse_input(&self, lines: Vec<String>) -> IntGrid {
+        IntGrid::from(&lines)
+    }
+
+    fn part_1(&self, grid: &IntGrid) -> usize {
+        let ans = 2 * (grid.height() + grid.width()) - 4;
+        ans + self
+            .ignoring_borders(grid)
+            .iter()
+            .filter(|cell| self.visible_from_outside(grid, cell))
+            .count()
+    }
+
+    fn part_2(&self, grid: &IntGrid) -> u32 {
+        self.ignoring_borders(grid)
+            .iter()
+            .map(|cell| self.scenic_score(grid, cell))
+            .max()
+            .unwrap()
+    }
+
+    fn samples(&self) {
+        #[rustfmt::skip]
+        let test =
+            "30373\n\
+             25512\n\
+             65332\n\
+             33549\n\
+             35390";
+        aoc::puzzle_samples! {
+            self, part_1, test, 21,
+            self, part_2, test, 8
+        };
+    }
+}
+
+fn main() {
+    AoC2022_08 {}.run(std::env::args());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn samples() {
+        AoC2022_08 {}.samples();
+    }
+}
