64e3c56206581e49f1d64efd97553ed05c7de0a4
pareronia <49491686+pareronia@users.noreply.github.com>
2023-08-13 17:43:47 +0200

    AoC 2022 Day 14 - rust - faster

--- src/main/rust/AoC2022_14/src/main.rs
+++ src/main/rust/AoC2022_14/src/main.rs
@@ -1,66 +1,62 @@
 #![allow(non_snake_case)]
 
-use {
-    aoc::{
-        geometry::{Heading, Translate, XY},
-        Puzzle,
-    },
-    lazy_static::lazy_static,
-    std::collections::HashSet,
-};
+use {aoc::Puzzle, lazy_static::lazy_static, std::collections::HashSet};
 
 lazy_static! {
-    static ref SOURCE: XY = XY::of(500, 0);
-    static ref DIRS: Vec<Heading> =
-        vec![Heading::South, Heading::SouthEast, Heading::SouthWest];
+    static ref SOURCE: (usize, usize) = (500, 0);
 }
 
 #[derive(Clone, Debug)]
 struct Cave {
-    rocks: HashSet<XY>,
-    max_y: i32,
+    occupied: Vec<Vec<bool>>,
+    max_y: usize,
 }
 
 struct AoC2022_14;
 
 impl AoC2022_14 {
-    fn solve(&self, cave: &Cave, max_y: i32) -> usize {
-        fn drop(cave: &Cave, sand: &mut HashSet<XY>, max_y: i32) -> Option<XY> {
-            let mut curr = *SOURCE;
+    fn solve(&self, cave: &mut Cave, max_y: usize) -> usize {
+        fn drop(cave: &Cave, max_y: usize) -> Option<(usize, usize)> {
+            let (mut curr_x, mut curr_y) = *SOURCE;
             loop {
-                let x = curr.x();
-                let y = curr.y();
-                for h in DIRS.iter() {
-                    let test = curr.translate(&XY::try_from(*h).unwrap(), -1);
-                    if !sand.contains(&test) && !cave.rocks.contains(&test) {
-                        curr = test;
-                        break;
+                let x = curr_x;
+                let y = curr_y;
+                let yy = y + 1;
+                for xx in vec![x, x - 1, x + 1] {
+                    match cave.occupied.get(yy) {
+                        None => continue,
+                        Some(row) => match row.get(xx) {
+                            Some(val) if *val == false => {
+                                (curr_x, curr_y) = (xx, yy);
+                                break;
+                            }
+                            _ => continue,
+                        },
                     }
                 }
-                if curr.x() == x && curr.y() == y {
-                    return Some(curr);
+                if curr_x == x && curr_y == y {
+                    return Some((curr_x, curr_y));
                 }
-                if curr.y() > max_y {
+                if curr_y == max_y {
                     return None;
                 }
             }
         }
 
-        let mut sand: HashSet<XY> = HashSet::new();
+        let mut cnt = 0;
         loop {
-            match drop(cave, &mut sand, max_y) {
+            match drop(cave, max_y) {
                 None => break,
-                Some(val) if val == *SOURCE => {
-                    sand.insert(val);
-                    break;
-                }
-                Some(other) => {
-                    sand.insert(other);
-                    continue;
+                Some(val) => {
+                    cave.occupied[val.1][val.0] = true;
+                    cnt += 1;
+                    if val == *SOURCE {
+                        break;
+                    }
                 }
             }
         }
-        sand.len()
+        cnt
     }
 }
 
@@ -72,18 +68,19 @@ impl aoc::Puzzle for AoC2022_14 {
     aoc::puzzle_year_day!(2022, 14);
 
     fn parse_input(&self, lines: Vec<String>) -> Cave {
-        let mut rocks: HashSet<XY> = HashSet::new();
-        let mut max_y = i32::MIN;
+        let mut rocks: HashSet<(usize, usize)> = HashSet::new();
+        let mut max_x = usize::MIN;
+        let mut max_y = usize::MIN;
         lines.iter().for_each(|line| {
             let mut c = line
                 .split(" -> ")
                 .map(|split| {
                     split
                         .split(",")
-                        .map(|s| s.parse::<i32>().unwrap())
-                        .collect::<Vec<i32>>()
+                        .map(|s| s.parse::<usize>().unwrap())
+                        .collect::<Vec<usize>>()
                 })
-                .collect::<Vec<Vec<i32>>>();
+                .collect::<Vec<Vec<usize>>>();
             let mut c1 = c.clone();
             for (v1, v2) in c.iter_mut().zip(c1[1..].iter_mut()) {
                 let mut xs = vec![v1[0], v2[0]];
@@ -92,28 +89,30 @@ impl aoc::Puzzle for AoC2022_14 {
                 ys.sort_unstable();
                 for x in xs[0]..=xs[1] {
                     for y in ys[0]..=ys[1] {
-                        rocks.insert(XY::of(x, y));
+                        rocks.insert((x, y));
+                        max_x = max_x.max(x);
                         max_y = max_y.max(y);
                     }
                 }
             }
         });
-        Cave { rocks, max_y }
+        let size = max_y + 2;
+        let mut occupied: Vec<Vec<bool>> = Vec::with_capacity(size);
+        (0..size).for_each(|_| occupied.push(vec![false; max_x + 150]));
+        rocks.iter().for_each(|&(x, y)| occupied[y][x] = true);
+        Cave { occupied, max_y }
     }
 
-    fn part_1(&self, cave: &Cave) -> usize {
-        self.solve(cave, cave.max_y)
+    fn part_1(&self, input: &Cave) -> usize {
+        let max_y = input.max_y;
+        let mut cave = input.clone();
+        self.solve(&mut cave, max_y)
     }
 
     fn part_2(&self, input: &Cave) -> usize {
+        let max_y = input.max_y + 2;
         let mut cave = input.clone();
-        let mut xs: Vec<i32> = cave.rocks.iter().map(|r| r.x()).collect();
-        xs.sort_unstable();
-        let the_max = cave.max_y + 2;
-        for x in xs[0] - the_max..=xs.last().unwrap() + the_max {
-            cave.rocks.insert(XY::of(x, the_max));
-        }
-        self.solve(&cave, the_max)
+        self.solve(&mut cave, max_y)
     }
 
     fn samples(&self) {
