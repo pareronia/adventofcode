2e7b55bf1e486cc095634442528cfe07ec889827
pareronia <49491686+pareronia@users.noreply.github.com>
2023-08-18 19:41:19 +0200

    AoC 2022 Day 20 - faster

--- src/main/python/AoC2022_20.py
+++ src/main/python/AoC2022_20.py
@@ -9,72 +9,21 @@ from __future__ import annotations
 import aocd
 
 from aoc import my_aocd
-from aoc.common import clog, log
-
-
-class Nums:
-    def __init__(self, inputs: tuple[str], factor: int):
-        self.nums = list(map(lambda line: int(line) * factor, inputs))
-        size = len(self.nums)
-        self.next: list[int] = [None] * size
-        self.prev: list[int] = [None] * size
-        for i in range(1, size + 1, 1):
-            self.prev[i % size] = (i - 1) % size
-            self.next[i % size] = (i + 1) % size
-
-    def print(self, zero: int) -> list[int]:
-        tmp = zero
-        s = []
-        while True:
-            s.append(self.nums[tmp])
-            tmp = self.next[tmp]
-            if self.nums[tmp] == 0:
-                break
-        return s
-
-    def find_zero(self) -> int:
-        return next(i for i, num in enumerate(self.nums) if num == 0)
-
-    def round(self) -> None:
-        size = len(self.nums)
-        for i, to_move in enumerate(self.nums):
-            clog(lambda: f"to_move: {to_move}")
-            if to_move == 0:
-                continue
-            self.prev[self.next[i]] = self.prev[i]
-            self.next[self.prev[i]] = self.next[i]
-            if to_move > 0:
-                move_to = self.prev[i]
-                amount = to_move % (size - 1)
-                for _ in range(amount):
-                    move_to = self.next[move_to]
-            else:
-                move_to = self.next[i]
-                amount = abs(to_move) % (size - 1) + 1
-                for _ in range(amount):
-                    move_to = self.prev[move_to]
-            clog(lambda: f"move to: {self.nums[move_to]} ({amount})")
-            before, after = move_to, self.next[move_to]
-            self.next[before] = i
-            self.prev[i] = before
-            self.prev[after] = i
-            self.next[i] = after
 
 
 def _solve(inputs: tuple[str], rounds: int, factor: int = 1) -> int:
-    nums = Nums(inputs, factor)
-    zero = nums.find_zero()
-    for round in range(rounds):
-        log(f"Round {round}")
-        nums.round()
-        clog(lambda: f"{nums.print(zero)}")
-    ans = 0
-    n = zero
-    for i in range(1, 3001):
-        n = nums.next[n]
-        if i % 1000 == 0:
-            ans += nums.nums[n]
-    return ans
+    nums = [int(line) * factor for line in inputs]
+    idxs = [i for i in range(len(nums))]
+    for _ in range(rounds):
+        for i, num in enumerate(nums):
+            idx = idxs.index(i)
+            idxs.remove(i)
+            new_idx = (idx + num) % len(idxs)
+            idxs.insert(new_idx, i)
+    zero_idx = idxs.index(nums.index(0))
+    return sum(
+        nums[idxs[(zero_idx + i) % len(idxs)]] for i in [1000, 2000, 3000]
+    )
 
 
 def part_1(inputs: tuple[str]) -> int:
