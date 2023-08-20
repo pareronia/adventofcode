894fb73e06e799485fb5ec6c55d5be0bc9fd11f1
pareronia <49491686+pareronia@users.noreply.github.com>
2023-08-21 00:11:46 +0200

    AoC 2015 Day 4 - java - refactor

--- src/main/java/AoC2015_04.java
+++ src/main/java/AoC2015_04.java
@@ -1,19 +1,22 @@
 import static com.github.pareronia.aoc.IntegerSequence.Range.range;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import com.github.pareronia.aoc.codec.MD5;
 import com.github.pareronia.aocd.Aocd;
 import com.github.pareronia.aocd.Puzzle;
 
+import lombok.RequiredArgsConstructor;
+
 public final class AoC2015_04 extends AoCBase {
 
-    private final transient String input;
+    private final AdventCoinsMiner miner;
 
     private AoC2015_04(final List<String> inputs, final boolean debug) {
         super(debug);
         assert inputs.size() == 1;
-        this.input = inputs.get(0);
+        this.miner = new AdventCoinsMiner(inputs.get(0));
     }
 
     public static AoC2015_04 create(final List<String> input) {
@@ -24,39 +27,14 @@ public final class AoC2015_04 extends AoCBase {
         return new AoC2015_04(input, true);
     }
     
-    private boolean checkZeroes(final byte[] digest, final int zeroes) {
-        int cnt = 0;
-        for (final int j : range(zeroes / 2 + zeroes % 2)) {
-            final byte c = digest[j];
-            if ((c & 0xF0) == 0) {
-                cnt++;
-                if ((c & 0x0F) == 0) {
-                    cnt++;
-                    continue;
-                }
-            }
-            break;
-        }
-        return cnt == zeroes;
-    }
-    
-    private int findMd5StartingWithZeroes(final String seed, final int zeroes) {
-		for (int i = 1; ; i++) {
-			final byte[] digest = MD5.md5(seed + String.valueOf(i));
-			if (checkZeroes(digest, zeroes)) {
-			    return i;
-			}
-		}
-    }
-    
     @Override
     public Integer solvePart1() {
-        return findMd5StartingWithZeroes(this.input, 5);
+        return this.miner.findMd5StartingWithZeroes(5);
     }
 
     @Override
     public Integer solvePart2() {
-        return findMd5StartingWithZeroes(this.input, 6);
+        return this.miner.findMd5StartingWithZeroes(6);
     }
 
     public static void main(final String[] args) throws Exception {
@@ -73,4 +51,34 @@ public final class AoC2015_04 extends AoCBase {
 
     private static final List<String> TEST1 = splitLines("abcdef");
     private static final List<String> TEST2 = splitLines("pqrstuv");
+    
+    @RequiredArgsConstructor
+    private static final class AdventCoinsMiner {
+        private final String secretKey;
+    
+        private boolean checkZeroes(final byte[] digest, final int zeroes) {
+            int cnt = 0;
+            for (final int j : range(zeroes / 2 + zeroes % 2)) {
+                final byte c = digest[j];
+                if ((c & 0xF0) != 0) {
+                    break;
+                }
+                cnt++;
+                if ((c & 0x0F) != 0) {
+                    break;
+                }
+                cnt++;
+            }
+            return cnt == zeroes;
+        }
+        
+        public int findMd5StartingWithZeroes(final int zeroes) {
+            return IntStream.iterate(1, i -> i + 1)
+                .dropWhile(i -> {
+                    final String data = this.secretKey + String.valueOf(i);
+                    return !checkZeroes(MD5.md5(data), zeroes);
+                })
+                .findFirst().getAsInt();
+        }
+    }
 }
