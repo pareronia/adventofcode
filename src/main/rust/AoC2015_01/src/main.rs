36cedaef9cfdb7f8278414c2a3152b1e34853509
pareronia <49491686+pareronia@users.noreply.github.com>
2023-08-20 17:26:42 +0200

    AoC 2015 Day 1 - rust - refactor

--- src/main/rust/AoC2015_01/src/main.rs
+++ src/main/rust/AoC2015_01/src/main.rs
@@ -1,10 +1,12 @@
 #![allow(non_snake_case)]
 
 use aoc::Puzzle;
+use std::{iter::Sum, ops::AddAssign};
 
 const UP: char = '(';
 const DOWN: char = ')';
 
+#[derive(Clone, Copy)]
 enum Direction {
     Up = 1,
     Down = -1,
@@ -20,36 +22,50 @@ impl Direction {
     }
 }
 
+impl AddAssign<&Direction> for i32 {
+    fn add_assign(&mut self, rhs: &Direction) {
+        *self = *self + *rhs as i32
+    }
+}
+
+impl<'a> Sum<&'a Direction> for i32 {
+    fn sum<I>(iter: I) -> i32
+    where
+        I: Iterator<Item = &'a Direction>,
+    {
+        iter.fold(0, |a, b| a + *b as i32)
+    }
+}
+
 struct AoC2015_01;
 
 impl AoC2015_01 {}
 
 impl aoc::Puzzle for AoC2015_01 {
-    type Input = String;
+    type Input = Vec<Direction>;
     type Output1 = i32;
     type Output2 = usize;
 
     aoc::puzzle_year_day!(2015, 1);
 
-    fn parse_input(&self, lines: Vec<String>) -> String {
-        lines[0].clone()
+    fn parse_input(&self, lines: Vec<String>) -> Vec<Direction> {
+        lines[0]
+            .chars()
+            .map(|ch| Direction::from_char(&ch))
+            .collect()
     }
 
-    fn part_1(&self, input: &String) -> i32 {
-        input
-            .chars()
-            .map(|ch| Direction::from_char(&ch) as i32)
-            .sum()
+    fn part_1(&self, input: &Vec<Direction>) -> i32 {
+        input.iter().sum()
     }
 
-    fn part_2(&self, input: &String) -> usize {
-        let mut sum = 0;
+    fn part_2(&self, input: &Vec<Direction>) -> usize {
+        let mut sum: i32 = 0;
         input
-            .chars()
-            .map(|ch| Direction::from_char(&ch) as i32)
+            .iter()
             .enumerate()
             .skip_while(|(_, dir)| {
-                sum += dir;
+                sum += *dir;
                 sum != -1
             })
             .map(|(i, _)| i + 1)
