6271909de1e926128e9f97957758849976e6adda
pareronia <49491686+pareronia@users.noreply.github.com>
2023-08-25 22:44:51 +0200

    AoC 2015 Day 15 - java - faster, refactor

--- src/main/java/AoC2015_15.java
+++ src/main/java/AoC2015_15.java
@@ -1,48 +1,21 @@
-import static com.github.pareronia.aoc.AssertUtils.assertTrue;
-import static java.util.stream.Collectors.toList;
-
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import com.github.pareronia.aoc.IterTools;
+import com.github.pareronia.aoc.Utils;
 import com.github.pareronia.aocd.Aocd;
 import com.github.pareronia.aocd.Puzzle;
 
-public class AoC2015_15 extends AoCBase {
-    
-    private static final String CAPACITY = "capacity";
-    private static final String DURABILITY = "durability";
-    private static final String FLAVOR = "flavor";
-    private static final String TEXTURE = "texture";
-    private static final String CALORIES = "calories";
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 
-    private static final String REGEXP =
-        "([A-Za-z]+): capacity ([-0-9]+), durability ([-0-9]+), flavor ([-0-9]+), texture ([-0-9]+), calories ([-0-9]+)";
-    
-    private List<Map<String, Integer>> ingredients;
+public class AoC2015_15 extends AoCBase {
+
+    private final Ingredients ingredients;
     
     private AoC2015_15(final List<String> inputs, final boolean debug) {
         super(debug);
-        final Pattern pattern = Pattern.compile(REGEXP);
-        this.ingredients = inputs.stream()
-            .map(s -> {
-                final Matcher m = pattern.matcher(s);
-                assertTrue(m.matches(), () -> "No match found");
-                return Map.of(
-                        CAPACITY, Integer.valueOf(m.group(2)),
-                        DURABILITY, Integer.valueOf(m.group(3)),
-                        FLAVOR, Integer.valueOf(m.group(4)),
-                        TEXTURE, Integer.valueOf(m.group(5)),
-                        CALORIES, Integer.valueOf(m.group(6)));
-            })
-            .collect(toList());
-        log(this.ingredients);
-        assert this.ingredients.size() == 2 || this.ingredients.size() == 4;
+        this.ingredients = Ingredients.fromInput(inputs);
     }
 
     public static final AoC2015_15 create(final List<String> input) {
@@ -53,68 +26,14 @@ public class AoC2015_15 extends AoCBase {
         return new AoC2015_15(input, true);
     }
     
-    private void generatePermutations(final List<Integer> amounts, final Stream.Builder<List<Integer>> builder) {
-        if (amounts.stream().mapToInt(Integer::intValue).sum() != 100) {
-            return;
-        }
-        IterTools.permutations(amounts, builder::add);
-    }
-    
-    private Stream<List<Integer>> generateMeasures() {
-        final Stream.Builder<List<Integer>> builder = Stream.builder();
-        for (int i = 100; i >= 0; i--) {
-            for (int j = i; j >= 0; j--) {
-                if (ingredients.size() == 2) {
-                    generatePermutations(List.of(i, j), builder);
-                    continue;
-                }
-                for (int k = j; k >= 0; k--) {
-                    for (int m = k; m >= 0; m--) {
-                        generatePermutations(List.of(i, j, k, m), builder);
-                    }
-                }
-            }
-        }
-        return builder.build();
-    }
-    
-    private int calculateScore(final List<Integer> measures) {
-        return calculateScore(measures, null);
-    }
-        
-    private int calculateScore(final List<Integer> measures, final Integer caloriesTarget) {
-        assert ingredients.size() == measures.size();
-        final int size = ingredients.size();
-        if (caloriesTarget != null) {
-            final int total = IntStream.range(0, size).map(i -> ingredients.get(i).get(CALORIES) * measures.get(i)).sum();
-            if (total != caloriesTarget) {
-                return 0;
-            }
-        }
-        final Map<String, Integer> totals = new HashMap<>();
-        for (final String attribute : List.of(CAPACITY, DURABILITY, FLAVOR, TEXTURE)) {
-            totals.put(
-                    attribute,
-                    IntStream.range(0, size).map(i -> ingredients.get(i).get(attribute) * measures.get(i)).sum());
-            if (totals.get(attribute) <= 0) {
-                return 0;
-            }
-        }
-        return totals.values().stream().reduce(1, (a, b) -> a * b);
-    }
-    
     @Override
     public Integer solvePart1() {
-        return generateMeasures()
-            .mapToInt(this::calculateScore)
-            .max().orElseThrow();
+        return this.ingredients.getHighestScore();
     }
     
     @Override
     public Integer solvePart2() {
-        return generateMeasures()
-            .mapToInt(m -> calculateScore(m, 500))
-            .max().orElseThrow();
+        return this.ingredients.getHighestScoreWithCalorieLimit(500);
     }
 
     public static void main(final String[] args) throws Exception {
@@ -130,7 +49,68 @@ public class AoC2015_15 extends AoCBase {
     }
     
     private static final List<String> TEST = splitLines(
-            "Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8\r\n"
-            + "Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3"
+        "Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8\r\n" +
+        "Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3"
     );
+    
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    private static final class Ingredients {
+        private enum Property { CAPACITY, DURABILITY, FLAVOR, TEXTURE, CALORIES  }
+        
+        private final int[][] ingredients;
+        
+        public static Ingredients fromInput(final List<String> inputs) {
+            return new Ingredients(inputs.stream()
+                    .map(line -> Utils.integerNumbers(line))
+                    .toArray(int[][]::new));
+        }
+        
+        public int getHighestScore() {
+            return getMaximumScore(null);
+        }
+        
+        public int getHighestScoreWithCalorieLimit(final int limit) {
+            return getMaximumScore(limit);
+        }
+        
+        private int getMaximumScore(final Integer limit) {
+            return generateMeasures()
+                    .mapToInt(m -> calculateScore(m, limit))
+                    .max().orElseThrow();
+        }
+        
+        private Stream<int[]> generateMeasures() {
+            final Stream.Builder<int[]> builder = Stream.builder();
+            for (int i = 0; i <= 100; i++) {
+                if (this.ingredients.length == 2) {
+                    builder.add(new int[] { i, 100 - i });
+                    continue;
+                }
+                for (int j = 0; j <= 100 - i; j++) {
+                    for (int k = 0; k <= 100 - i - j; k++) {
+                        final int m = 100 - i - j - k;
+                        builder.add(new int[] { i, j, k, m });
+                    }
+                }
+            }
+            return builder.build();
+        }
+
+        private int getPropertyScore(final int[] measures, final Property p) {
+            return IntStream.range(0, this.ingredients.length)
+                    .map(i -> this.ingredients[i][p.ordinal()] * measures[i])
+                    .sum();
+        }
+        
+        private int calculateScore(final int[] measures, final Integer caloriesTarget) {
+            if (caloriesTarget != null
+                    && getPropertyScore(measures, Property.CALORIES) != caloriesTarget) {
+                return 0;
+            }
+            return Stream.of(Property.values())
+                .filter(p -> p != Property.CALORIES)
+                .mapToInt(p -> Math.max(0, getPropertyScore(measures, p)))
+                .reduce(1, (a, b) -> a * b);
+        }
+    }
 }
