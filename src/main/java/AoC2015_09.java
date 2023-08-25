5a440e93d53ee5c3462bee5d221fdff2806ee7a2
pareronia <49491686+pareronia@users.noreply.github.com>
2023-08-25 16:28:35 +0200

    AoC 2015 Day 9 - java - faster, refactor

--- src/main/java/AoC2015_09.java
+++ src/main/java/AoC2015_09.java
@@ -1,39 +1,25 @@
-import static java.util.Collections.emptyList;
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.toList;
-
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Stream;
-import java.util.stream.Stream.Builder;
+import java.util.stream.IntStream;
 
+import com.github.pareronia.aoc.IterTools;
+import com.github.pareronia.aoc.MutableInt;
 import com.github.pareronia.aocd.Aocd;
+import com.github.pareronia.aocd.Puzzle;
 
-import lombok.Getter;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 public class AoC2015_09 extends AoCBase {
     
-    private final transient Map<String, List<Distance>> inputs;
+    private final Distances distances;
 
     private AoC2015_09(final List<String> input, final boolean debug) {
         super(debug);
-        this.inputs = input.stream()
-                .flatMap(s -> {
-                    final String[] ss1 = s.split(" = ");
-                    final String[] ss2 = ss1[0].split(" to ");
-                    return Stream.of(
-                            new Distance(ss2[0], ss2[1], Integer.valueOf(ss1[1])),
-                            new Distance(ss2[1], ss2[0], Integer.valueOf(ss1[1]))
-                    );
-                })
-                .collect(groupingBy(Distance::getFrom, toList()));
-        log(this.inputs);
+        this.distances = Distances.fromInput(input);
+        log(this.distances);
     }
 
     public static final AoC2015_09 create(final List<String> input) {
@@ -44,52 +30,27 @@ public class AoC2015_09 extends AoCBase {
         return new AoC2015_09(input, true);
     }
     
-    private Stream<Route> allRoutesStartingFrom(final String start) {
-        final Builder<Route> builder = Stream.builder();
-        final Deque<Route> queue = new ArrayDeque<>();
-        queue.add(new Route(List.of(start), 0));
-        while (!queue.isEmpty()) {
-            final Route route = queue.poll();
-            builder.add(route);
-            final String last = route.getLastLocation();
-            for (final Distance d : this.inputs.getOrDefault(last, emptyList())) {
-                if (!route.visited(d.getTo())) {
-                    queue.add(route.add(d.getTo(), d.getAmount()));
-                }
-            }
-        }
-        return builder.build();
-    }
-    
-    private Stream<Route> getAllCompleteRoutes() {
-        final Set<String> allLocations = this.inputs.keySet();
-        return allLocations.stream()
-                .flatMap(this::allRoutesStartingFrom)
-                .filter(route -> route.getLength() == allLocations.size());
-    }
-
     @Override
     public Integer solvePart1() {
-        return getAllCompleteRoutes()
-                .mapToInt(Route::getTotalDistance)
-                .min().getAsInt();
+        return this.distances.getDistancesOfCompleteRoutes().min().getAsInt();
     }
 
     @Override
     public Integer solvePart2() {
-        return getAllCompleteRoutes()
-                .mapToInt(Route::getTotalDistance)
-                .max().getAsInt();
+        return this.distances.getDistancesOfCompleteRoutes().max().getAsInt();
     }
 
     public static void main(final String[] args) throws Exception {
         assert AoC2015_09.createDebug(TEST).solvePart1() == 605;
         assert AoC2015_09.createDebug(TEST).solvePart2() == 982;
 
-        final List<String> input = Aocd.getData(2015, 9);
+        final Puzzle puzzle = Aocd.puzzle(2015, 9);
+        final List<String> inputData = puzzle.getInputData();
 
-        lap("Part 1", () -> AoC2015_09.create(input).solvePart1());
-        lap("Part 2", () -> AoC2015_09.create(input).solvePart2());
+        puzzle.check(
+            () -> lap("Part 1", AoC2015_09.create(inputData)::solvePart1),
+            () -> lap("Part 2", AoC2015_09.create(inputData)::solvePart2)
+        );
     }
 
     private static final List<String> TEST = splitLines(
@@ -98,39 +59,38 @@ public class AoC2015_09 extends AoCBase {
             "Dublin to Belfast = 141"
     );
     
-    @RequiredArgsConstructor
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
     @ToString
-    @Getter
-    private static final class Distance {
-        private final String from;
-        private final String to;
-        private final int amount;
-    }
-    
-    @RequiredArgsConstructor
-    @ToString
-    private static final class Route {
-        private final List<String> locations;
-        @Getter
-        private final Integer totalDistance;
+    private static final class Distances {
+        private final int[][] matrix;
         
-        public Route add(final String location, final int distance) {
-            final List<String> newLocations = new ArrayList<String>(this.locations);
-            newLocations.add(location);
-            final int newTotal = this.totalDistance + distance;
-            return new Route(newLocations, newTotal);
+        public static Distances fromInput(final List<String> inputs) {
+            final Map<String, Integer> map = new HashMap<>();
+            final Map<int[], Integer> values = new HashMap<>();
+            final MutableInt cnt = new MutableInt(0);
+            for (final String input : inputs) {
+                final String[] ss1 = input.split(" = ");
+                final String[] ss2 = ss1[0].split(" to ");
+                final int idx1 = map.computeIfAbsent(ss2[0], x -> cnt.getAndIncrement());
+                final int idx2 = map.computeIfAbsent(ss2[1], x -> cnt.getAndIncrement());
+                final int value = Integer.parseInt(ss1[1]);
+                values.put(new int[] { idx1, idx2 }, value);
+                values.put(new int[] { idx2, idx1 }, value);
+            }
+            final int[][] matrix = new int[map.size()][map.size()];
+            values.entrySet().stream()
+                .forEach(e -> {
+                    matrix[e.getKey()[0]][e.getKey()[1]] = e.getValue();
+                });
+            return new Distances(matrix);
         }
         
-        public int getLength() {
-            return this.locations.size();
-        }
-        
-        public String getLastLocation() {
-            return this.locations.get(this.locations.size() - 1);
-        }
-        
-        public boolean visited(final String location) {
-            return this.locations.contains(location);
+        public IntStream getDistancesOfCompleteRoutes() {
+            final int[] idxs = IntStream.range(0, this.matrix.length).toArray();
+            return IterTools.permutations(idxs).mapToInt(p ->
+                    IntStream.range(1, p.length)
+                        .map(i -> this.matrix[p[i -1]][p[i]])
+                        .sum());
         }
     }
 }
