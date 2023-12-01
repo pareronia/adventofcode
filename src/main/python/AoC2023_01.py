e272020d241bc891b92734dd86c74ebdc20c376c
pareronia <49491686+pareronia@users.noreply.github.com>
2023-12-01 09:26:05 +0100

    AoC 2023 Day 1 - cleanup

--- src/main/python/AoC2023_01.py
+++ src/main/python/AoC2023_01.py
@@ -5,6 +5,7 @@
 
 import re
 import sys
+from typing import Callable
 
 from aoc.common import InputData
 from aoc.common import SolutionBase
@@ -14,7 +15,12 @@ Input = InputData
 Output1 = int
 Output2 = int
 
-
+TEST1 = """\
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+"""
 TEST2 = """\
 two1nine
 eightwothree
@@ -30,51 +36,52 @@ class Solution(SolutionBase[Input, Output1, Output2]):
     def parse_input(self, input_data: InputData) -> Input:
         return input_data
 
-    def part_1(self, input: InputData) -> Output1:
+    def _solve(
+        self, input: InputData, f: Callable[[str], tuple[int, int]]
+    ) -> int:
         ans = 0
         for line in input:
-            digits = [c for c in line if c.isdigit()]
-            ans += int(digits[0] + digits[-1])
+            first, last = f(line)
+            ans += first * 10 + last
         return ans
+
+    def part_1(self, input: InputData) -> Output1:
+        def get_first_and_last_digit(line: str) -> tuple[int, int]:
+            digits = [c for c in line if c.isdigit()]
+            return int(digits[0]), int(digits[-1])
+
+        return self._solve(input, get_first_and_last_digit)
 
     def part_2(self, input: InputData) -> Output2:
         nums = {
-            "one": 1,
-            "two": 2,
-            "three": 3,
-            "four": 4,
-            "five": 5,
-            "six": 6,
-            "seven": 7,
-            "eight": 8,
-            "nine": 9,
-            "zero": 0,
+            "one": "1",
+            "two": "2",
+            "three": "3",
+            "four": "4",
+            "five": "5",
+            "six": "6",
+            "seven": "7",
+            "eight": "8",
+            "nine": "9",
+            "zero": "0",
         }
-        ans = 0
-        for line in input:
-            print(line)
-            tmp = []
+
+        def get_first_and_last_digit(line: str) -> tuple[int, int]:
+            digits = []
             for x in nums:
-                tmp.extend(
+                digits.extend(
                     [(m.start(), nums[x]) for m in re.finditer(x, line)]
                 )
             for x in "0123456789":
-                tmp.extend(
-                    [(m.start(), int(x)) for m in re.finditer(x, line)]
-                )
-            # tmp.extend([(line.find(c), int(c)) for c in line if c.isdigit()])
-            tmp.sort()
-            print(tmp)
-            if len(tmp) == 0:
-                continue
-            d = tmp[0][1] * 10 + tmp[-1][1]
-            print(d)
-            ans += d
-        return ans
+                digits.extend([(m.start(), x) for m in re.finditer(x, line)])
+            digits.sort()
+            return int(digits[0][1]), int(digits[-1][1])
+
+        return self._solve(input, get_first_and_last_digit)
 
     @aoc_samples(
         (
-            # ("part_1", TEST, "TODO"),
+            ("part_1", TEST1, 142),
             ("part_2", TEST2, 281),
         )
     )
