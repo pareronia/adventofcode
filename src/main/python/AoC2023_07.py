ebd3ad76bbfae7b281094530b7f450247e6dbf50
pareronia <49491686+pareronia@users.noreply.github.com>
2023-12-08 21:03:36 +0100

    AoC 2023 Day 7 - cleanup

--- src/main/python/AoC2023_07.py
+++ src/main/python/AoC2023_07.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 
 import sys
 from collections import Counter
-from functools import total_ordering
+from typing import Callable
 from typing import NamedTuple
 
 from aoc.common import InputData
@@ -22,138 +22,44 @@ KTJJT 220
 QQQJA 483
 """
 
-VALUES = {
-    "5_KIND": 1000,
-    "4_KIND": 900,
-    "FULL_HOUSE": 800,
-    "3_KIND": 700,
-    "2_PAIR": 600,
-    "1_PAIR": 500,
-    "HIGH_CARD": 400,
-}
-
-STRENGTH = {
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "T": 10,
-    "J": 11,
-    "Q": 12,
-    "K": 13,
-    "A": 14,
-}
+JOKER = "J"
+BEST = "AAAAA"
 
 
-def _value(s: str) -> str:
-    c = Counter(s)
-    if 5 in c.values():
-        type = "5_KIND"
-    elif 4 in c.values():
-        type = "4_KIND"
-    elif sorted(c.values()) == [2, 3]:
-        type = "FULL_HOUSE"
-    elif 3 in c.values():
-        type = "3_KIND"
-    elif sorted(c.values()) == [1, 2, 2]:
-        type = "2_PAIR"
-    elif 2 in c.values():
-        type = "1_PAIR"
-    else:
-        type = "HIGH_CARD"
-    return type
-
-
-def _strength(s: str) -> list[int]:
-    return [STRENGTH[s[i]] if s[i] != "J" else 1 for i in range(5)]
-
-
-@total_ordering
 class Hand(NamedTuple):
     cards: str
     bid: int
+    value: int
+    strength: str
+    value_with_jokers: int
+    strength_with_jokers: str
 
     @classmethod
     def from_input(cls, line: str) -> Hand:
-        c, b = line.split()
-        return Hand(c, int(b))
+        def get_value(cards: str) -> int:
+            mc = Counter(cards).most_common()
+            return 2 * mc[0][1] + (mc[1][1] if len(mc) > 1 else 0)
 
-    def value(self) -> str:
-        return _value(self.cards)
+        def with_jokers(cards: str) -> str:
+            c = Counter(card for card in cards if card != JOKER)
+            if not c:
+                return BEST
+            best = c.most_common()[0][0]
+            return "".join((card if card != JOKER else best) for card in cards)
 
-    # def value(self) -> str:
-    #     c = Counter(self.cards)
-    #     if 5 in c.values():
-    #         type = "5_KIND"
-    #     elif 4 in c.values():
-    #         if c.get("J") == 1:
-    #             type = "5_KIND"
-    #         else:
-    #             type = "4_KIND"
-    #     elif sorted(c.values()) == [2, 3]:
-    #         if c.get("J") == 2 or c.get("J") == 3:
-    #             type = "5_KIND"
-    #         else:
-    #             type = "FULL_HOUSE"
-    #     elif 3 in c.values():
-    #         if c.get("J") == 1:
-    #             type = "4_KIND"
-    #         elif c.get("J") == 2:
-    #             type = "5_KIND"
-    #         else:
-    #             type = "3_KIND"
-    #     elif sorted(c.values()) == [1, 2, 2]:
-    #         if c.get("J") == 1:
-    #             type = "FULL_HOUSE"
-    #         elif c.get("J") == 2:
-    #             type = "4_KIND"
-    #         else:
-    #             type = "2_PAIR"
-    #     elif 2 in c.values():
-    #         if c.get("J") == 1:
-    #             type = "3_KIND"
-    #         elif c.get("J") == 2:
-    #             type = "4_KIND"
-    #         else:
-    #             type = "1_PAIR"
-    #     else:
-    #         if c.get("J") == 1:
-    #             type = "1_PAIR"
-    #         else:
-    #             type = "HIGH_CARD"
-    #     return type
-
-    def strength(self) -> list[int]:
-        return [STRENGTH[self.cards[i]] for i in range(5)]
-
-    def __eq__(self, other: Hand) -> bool:
-        if VALUES[self.value()] == VALUES[other.value()]:
-            return self.strength() == other.strength()
-        else:
-            return False
-
-    def __lt__(self, other: Hand) -> bool:
-        if VALUES[self.value()] < VALUES[other.value()]:
-            return True
-        elif VALUES[self.value()] == VALUES[other.value()]:
-            return self.strength() < other.strength()
-        return False
-
-    def all_possible(self) -> list[str]:
-        if "J" not in self.cards:
-            return [self.cards]
-        ans = []
-        for i, c in enumerate(self.cards):
-            if c == "J":
-                for x in "23456789TQKA":
-                    ans.append(
-                        "".join(x if _ == "J" else _ for _ in self.cards)
-                    )
-        return ans
+        cards, bid = line.split()
+        value = get_value(cards)
+        strength = cards.translate(str.maketrans("TJQKA", "BCDEF"))
+        value_with_jokers = get_value(with_jokers(cards))
+        strength_with_jokers = cards.translate(str.maketrans("TJQKA", "B0DEF"))
+        return Hand(
+            cards,
+            int(bid),
+            value,
+            strength,
+            value_with_jokers,
+            strength_with_jokers,
+        )
 
 
 Input = list[Hand]
@@ -165,31 +71,24 @@ class Solution(SolutionBase[Input, Output1, Output2]):
     def parse_input(self, input_data: InputData) -> Input:
         return [Hand.from_input(line) for line in input_data]
 
+    def _solve(
+        self,
+        hands: Input,
+        order_func: Callable[[Hand], tuple[int, str]],
+    ) -> int:
+        return sum(
+            i * hand.bid
+            for i, hand in enumerate(sorted(hands, key=order_func), start=1)
+        )
+
     def part_1(self, hands: Input) -> Output1:
-        hands = sorted(hands)
-        ans = sum((i + 1) * hands[i].bid for i in range(len(hands)))
-        return ans
+        return self._solve(hands, lambda hand: (hand.value, hand.strength))
 
     def part_2(self, hands: Input) -> Output2:
-        # for hand in hands:
-        #   assert not (hand.value() == "HIGH_CARD" and "J" in hand.cards)
-        #   assert not (hand.value() == "1_PAIR" and hand.cards.count("J") > 1)
-        #   assert not (hand.value() == "2_PAIR" and "J" in hand.cards)
-        new_hands = []
-        for h in hands:
-            p = h.all_possible()
-            new_hands.append(
-                (
-                    max(p, key=lambda x: (VALUES[_value(x)])),
-                    h.cards,
-                    h.bid,
-                )
-            )
-        new_hands.sort(
-            key=lambda hand: (VALUES[_value(hand[0])], _strength(hand[1]))
+        return self._solve(
+            hands,
+            lambda hand: (hand.value_with_jokers, hand.strength_with_jokers),
         )
-        ans = sum((i + 1) * new_hands[i][2] for i in range(len(new_hands)))
-        return ans
 
     @aoc_samples(
         (
