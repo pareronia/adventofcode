8658f52a5310ef8d435cbeccf96f5ba03f97bd3d
pareronia <49491686+pareronia@users.noreply.github.com>
2023-12-12 21:16:11 +0100

    AoC 2023 Day 12 Part 2

--- src/main/python/AoC2023_12.py
+++ src/main/python/AoC2023_12.py
@@ -4,12 +4,13 @@
 #
 
 import sys
+from functools import cache
 from itertools import product
+from typing import Callable
 
 from aoc.common import InputData
 from aoc.common import SolutionBase
 from aoc.common import aoc_samples
-from aoc.common import log
 
 Input = InputData
 Output1 = int
@@ -26,43 +27,84 @@ TEST = """\
 """
 
 
+@cache
+def count(s: str, counts: tuple[int, ...], idx: int) -> int:
+    # base case: end of s
+    if s == "":
+        if len(counts) == 0 and idx == 0:
+            # no more groups required and not in group -> OK
+            return 1
+        else:
+            # still group(s) required or still in group -> NOK
+            return 0
+    # otherwise:
+    ans = 0
+    # '?' can be '.' or '#'
+    nxts = {".", "#"} if s[0] == "?" else {s[0]}
+    for nxt in nxts:
+        if nxt == "#":
+            # if '#': move to next char in current group
+            ans += count(s[1:], counts, idx + 1)
+        elif nxt == ".":
+            # if '.' (between groups)
+            if idx > 0:
+                # was in group before
+                if len(counts) > 0 and idx == counts[0]:
+                    # finished group matches required -> find next required
+                    ans += count(s[1:], counts[1:], 0)
+            else:
+                # was not in group: keep looking for next group
+                ans += count(s[1:], counts, 0)
+        else:
+            # should not happen
+            assert False
+    return ans
+
+
 class Solution(SolutionBase[Input, Output1, Output2]):
     def parse_input(self, input_data: InputData) -> Input:
         return input_data
 
-    def count(self, s: str, counts: tuple[int, ...]) -> int:
-        def match(s: str, counts: tuple[int, ...]) -> bool:
-            c = tuple(len(_) for _ in s.split(".") if len(_) != 0)
-            return c == counts
-
+    def brute_force_count(self, s: str, counts: tuple[int, ...]) -> int:
         ans = 0
-        cq = s.count("?")
-        pp = product("#.", repeat=cq)
-        for p in pp:
+        total = sum(counts)
+        s_count = s.count("#")
+        for p in product("#.", repeat=s.count("?")):
+            if s_count + p.count("#") != total:
+                continue
             pi = iter(p)
             test = "".join(
                 s[i] if s[i] != "?" else next(pi) for i in range(len(s))
             )
-            if match(test, counts):
+            if tuple(len(_) for _ in test.split(".") if len(_) != 0) == counts:
                 ans += 1
         return ans
 
+    def solve(
+        self, input: Input, f: Callable[[str], tuple[str, tuple[int, ...]]]
+    ) -> int:
+        return sum(count(*f(line), 0) for line in input)
+
     def part_1(self, input: Input) -> Output1:
-        ans = 0
-        for line in input:
+        def parse(line: str) -> tuple[str, tuple[int, ...]]:
             s, w = line.split()
             counts = tuple(int(_) for _ in w.split(","))
-            log((s, counts))
-            ans += self.count(s, counts)
-        return ans
+            return s + ".", counts
+
+        return self.solve(input, parse)
 
     def part_2(self, input: Input) -> Output2:
-        return 0
+        def parse(line: str) -> tuple[str, tuple[int, ...]]:
+            s, w = line.split()
+            counts = tuple(int(_) for _ in w.split(","))
+            return "?".join([s] * 5) + ".", counts * 5
+
+        return self.solve(input, parse)
 
     @aoc_samples(
         (
             ("part_1", TEST, 21),
-            # ("part_2", TEST, "TODO"),
+            ("part_2", TEST, 525152),
         )
     )
     def samples(self) -> None:
