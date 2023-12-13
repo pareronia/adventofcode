6025e0a1148a84ff20f488e00b857d9e80ad4190
pareronia <49491686+pareronia@users.noreply.github.com>
2023-12-13 10:58:29 +0100

    AoC 2023 Day 13 Part 2

--- src/main/python/AoC2023_13.py
+++ src/main/python/AoC2023_13.py
@@ -9,7 +9,6 @@ from aoc import my_aocd
 from aoc.common import InputData
 from aoc.common import SolutionBase
 from aoc.common import aoc_samples
-from aoc.common import log
 from aoc.grid import Cell
 from aoc.grid import CharGrid
 
@@ -44,53 +43,74 @@ class Solution(SolutionBase[Input, Output1, Output2]):
             for block in my_aocd.to_blocks(input_data)
         ]
 
+    def solve(self, grid: CharGrid) -> list[tuple[int, int]]:
+        ans = []
+        h = grid.get_height()
+        w = grid.get_width()
+        for c in range(w - 1):
+            for dc in range(w):
+                left = c - dc
+                right = c + dc + 1
+                if 0 <= left < right < w:
+                    for r in range(h):
+                        if grid.get_value(Cell(r, left)) != grid.get_value(
+                            Cell(r, right)
+                        ):
+                            break
+                    else:
+                        continue
+                    break
+            else:
+                ans.append((0, c + 1))
+        for r in range(h - 1):
+            for dr in range(h):
+                up = r - dr
+                down = r + dr + 1
+                if 0 <= up < down < h:
+                    for c in range(w):
+                        if grid.get_value(Cell(up, c)) != grid.get_value(
+                            Cell(down, c)
+                        ):
+                            break
+                    else:
+                        continue
+                    break
+            else:
+                ans.append((r + 1, 0))
+        return ans
+
     def part_1(self, grids: Input) -> Output1:
         ans = 0
         for grid in grids:
-            h = grid.get_height()
-            w = grid.get_width()
-            for c in range(w - 1):
-                for dc in range(w - c):
-                    left = c - dc
-                    right = c + dc + 1
-                    if 0 <= left < right < w:
-                        for r in range(h):
-                            if grid.get_value(Cell(r, left)) != grid.get_value(
-                                Cell(r, right)
-                            ):
-                                break
-                        else:
-                            continue
-                        break
-                else:
-                    ans += c + 1
-                    break
-            for r in range(h - 1):
-                for dr in range(h - r):
-                    up = r - dr
-                    down = r + dr + 1
-                    if 0 <= up < down < h:
-                        for c in range(w):
-                            if grid.get_value(Cell(up, c)) != grid.get_value(
-                                Cell(down, c)
-                            ):
-                                break
-                        else:
-                            continue
-                        break
-                else:
-                    ans += 100 * (r + 1)
-                    break
-        log(ans)
+            syms = self.solve(grid)
+            assert len(syms) <= 1
+            ans += (100 * syms[0][0]) + syms[0][1]
         return ans
 
-    def part_2(self, input: Input) -> Output2:
-        return 0
+    def part_2(self, grids: Input) -> Output2:
+        ans = 0
+        for i, grid in enumerate(grids):
+            osyms = self.solve(grid)
+            g = CharGrid([[c for c in row] for row in grid.values])
+            for cell in g.get_cells():
+                tmp = g.get_value(cell)
+                g.set_value(cell, "." if tmp == "#" else "#")
+                syms = self.solve(g)
+                for s in syms:
+                    if not osyms or s != osyms[0]:
+                        ans += (100 * s[0]) + s[1]
+                        break
+                else:
+                    g.set_value(cell, tmp)
+                    continue
+                g.set_value(cell, tmp)
+                break
+        return ans
 
     @aoc_samples(
         (
             ("part_1", TEST, 405),
-            # ("part_2", TEST, "TODO"),
+            ("part_2", TEST, 400),
         )
     )
     def samples(self) -> None:
