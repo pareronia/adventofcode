ba94688d7db0bccd9fcfac9158c24acfad215287
pareronia <49491686+pareronia@users.noreply.github.com>
2023-12-15 21:18:17 +0100

    implementation_tables module - don't include empty rows

--- src/main/python/aoc/implementation_tables/implementation_tables.py
+++ src/main/python/aoc/implementation_tables/implementation_tables.py
@@ -1,14 +1,17 @@
 #! /usr/bin/env python3
 
-import sys
-import os
 import logging
+import os
+import sys
 from datetime import datetime
 from typing import IO
+
 if __name__ == "__main__":
-    from config import Row, config
+    from config import Row  # type:ignore[import-not-found]
+    from config import config
 else:
-    from aoc.implementation_tables.config import Row, config
+    from aoc.implementation_tables.config import Row
+    from aoc.implementation_tables.config import config
 
 log = logging.getLogger(__name__)
 
@@ -18,36 +21,40 @@ def _build_link(base_dir: str, pattern: str, year: int, day: int) -> str:
     return f"[✓]({path})" if os.path.exists(path) else ""
 
 
-def _build_row(days) -> str:
+def _build_row(days: list[str]) -> str:
     return "| " + " | ".join(days) + " |"
 
 
 def _print_year(year: int, rows: list[Row], f: IO[str]) -> None:
     log.debug(f"Adding {year}")
-    print("| " + _build_row(str(day) for day in range(1, 26)), file=f)
-    print("| ---" + _build_row("---" for day in range(1, 26)), file=f)
+    print("| " + _build_row([str(day) for day in range(1, 26)]), file=f)
+    print("| ---" + _build_row(["---" for day in range(1, 26)]), file=f)
     for row in rows:
         log.debug(f"Adding {row.language}")
+        days = [
+            _build_link(row.base_dir, row.pattern + row.ext, year, day)
+            for day in range(1, 26)
+        ]
+        if len("".join(days)) == 0:
+            continue
         print(
-            "| " + row.language + " "
-            + _build_row(
-                _build_link(row.base_dir, row.pattern + row.ext, year, day)
-                for day in range(1, 26)),
-            file=f)
+            "| " + row.language + " " + _build_row(days),
+            file=f,
+        )
 
 
 def _get_rows() -> list[Row]:
     rows = config.get_rows()
     log.debug(rows)
-    return rows
+    return rows  # type:ignore[no-any-return]
 
 
 def main(file_name: str) -> None:
     log.debug(f"file: {file_name}")
     rows = _get_rows()
-    with open(file_name, 'r') as f:
+    with open(file_name, "r") as f:
         tmp = f.read()
-    with open(file_name, 'w') as f:
+    with open(file_name, "w") as f:
         in_table = False
         for line in tmp.splitlines():
             if line.startswith("<!-- @BEGIN:ImplementationsTable"):
@@ -67,4 +74,4 @@ if __name__ == "__main__":
     now = datetime.now()
     for year in range(2015, now.year + int(now.month == 12)):
         _print_year(year, _get_rows(), sys.stdout)
-    main('README.md')
+    main("README.md")
