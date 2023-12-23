beabd9d2db88f7f7422571f065b546487bf9bc7f
pareronia <49491686+pareronia@users.noreply.github.com>
2023-12-23 18:01:16 +0100

    AoC 2023 Day 23

--- src/main/python/AoC2023_23.py
+++ src/main/python/AoC2023_23.py
@@ -4,12 +4,12 @@
 #
 
 import sys
-from typing import Iterator
+from collections import defaultdict
+from collections import deque
 
 from aoc.common import InputData
 from aoc.common import SolutionBase
 from aoc.common import aoc_samples
-from aoc.common import log
 from aoc.geometry import Direction
 from aoc.grid import Cell
 from aoc.grid import CharGrid
@@ -17,6 +17,7 @@ from aoc.grid import CharGrid
 Input = CharGrid
 Output1 = int
 Output2 = int
+Edge = tuple[Cell, int]
 
 
 TEST = """\
@@ -46,52 +47,97 @@ TEST = """\
 """
 
 
+class PathFinder:
+    def __init__(self, grid: CharGrid) -> None:
+        self.grid = grid
+        self.start = Cell(0, 1)
+        self.end = Cell(grid.get_height() - 1, grid.get_width() - 2)
+
+    def find_longest_hike_length_with_only_downward_slopes(self) -> int:
+        forks = self._find_forks()
+        graph = self._build_graph(forks, downward_slope_only=True)
+        return self._find_longest(graph, self.start, set[Cell]())
+
+    def find_longest_hike_length(self) -> int:
+        forks = self._find_forks()
+        graph = self._build_graph(forks, downward_slope_only=False)
+        return self._find_longest(graph, self.start, set[Cell]())
+
+    def _find_forks(self) -> set[Cell]:
+        def is_fork(cell: Cell) -> bool:
+            return (
+                cell == self.start
+                or cell == self.end
+                or self.grid.get_value(cell) != "#"
+                and sum(
+                    1
+                    for n in self.grid.get_capital_neighbours(cell)
+                    if self.grid.get_value(n) != "#"
+                )
+                > 2
+            )
+
+        return {cell for cell in self.grid.get_cells() if is_fork(cell)}
+
+    def _build_graph(
+        self, forks: set[Cell], downward_slope_only: bool
+    ) -> dict[Cell, set[Edge]]:
+        graph = defaultdict[Cell, set[Edge]](set)
+        for fork in forks:
+            q = deque([(fork, 0)])
+            seen = set[Cell]({fork})
+            while len(q) > 0:
+                cell, distance = q.popleft()
+                if cell in forks and cell != fork:
+                    graph[fork].add((cell, distance))
+                    continue
+                for d in Direction.capitals():
+                    n = cell.at(d)
+                    if not self.grid.is_in_bounds(n):
+                        continue
+                    val = self.grid.get_value(n)
+                    if val == "#" or (
+                        downward_slope_only
+                        and val in {"^", ">", "v", "<"}
+                        and Direction.from_str(val) != d
+                    ):
+                        continue
+                    if n not in seen:
+                        seen.add(n)
+                        q.append((n, distance + 1))
+        return graph
+
+    def _find_longest(
+        self, graph: dict[Cell, set[Edge]], curr: Cell, seen: set[Cell]
+    ) -> int:
+        if curr == self.end:
+            return 0
+        ans = -1_000_000_000
+        seen.add(curr)
+        for vertex, distance in graph[curr]:
+            if vertex in seen:
+                continue
+            ans = max(ans, distance + self._find_longest(graph, vertex, seen))
+        seen.remove(curr)
+        return ans
+
+
 class Solution(SolutionBase[Input, Output1, Output2]):
     def parse_input(self, input_data: InputData) -> Input:
         return CharGrid.from_strings([line for line in input_data])
 
     def part_1(self, grid: Input) -> Output1:
-        def adjacent(cell: Cell) -> Iterator[Cell]:
-            for d in Direction.capitals():
-                n = cell.at(d)
-                val = grid.get_value(n)
-                if val == "#":
-                    continue
-                if (
-                    val in {"^", ">", "v", "<"}
-                    and Direction.from_str(val) != d
-                ):
-                    continue
-                yield n
-
-        def find_longest(cell: Cell, size: int) -> int:
-            if cell == end:
-                return size
-            if cell in seen:
-                return int(-1e9)
-            max_size = size
-            a = [_ for _ in adjacent(cell)]
-            for n in a:
-                seen.add(cell)
-                max_size = max(max_size, find_longest(n, size + 1))
-                seen.remove(cell)
-            return max_size
-
-        sys.setrecursionlimit(10_000)
-        start = Cell(0, 1)
-        end = (grid.get_height() - 1, grid.get_width() - 2)
-        seen = set[Cell]()
-        ans = find_longest(start, 0)
-        log(ans)
-        return ans
+        return PathFinder(
+            grid
+        ).find_longest_hike_length_with_only_downward_slopes()
 
     def part_2(self, grid: Input) -> Output2:
-        return 0
+        return PathFinder(grid).find_longest_hike_length()
 
     @aoc_samples(
         (
             ("part_1", TEST, 94),
-            # ("part_2", TEST, "TODO"),
+            ("part_2", TEST, 154),
         )
     )
     def samples(self) -> None:
