78a4b003c945e569b868c04b2c21cd6f9c7bae81
pareronia <49491686+pareronia@users.noreply.github.com>
2024-04-14 12:48:06 +0200

    AoC 2020 Day 4 - java - fix + refactor

--- src/main/java/AoC2020_04.java
+++ src/main/java/AoC2020_04.java
@@ -6,123 +6,105 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.function.Predicate;
 
 import com.github.pareronia.aoc.RangeInclusive;
+import com.github.pareronia.aoc.StringOps;
 import com.github.pareronia.aoc.StringUtils;
 import com.github.pareronia.aoc.Utils;
-import com.github.pareronia.aocd.Puzzle;
+import com.github.pareronia.aoc.solution.Sample;
+import com.github.pareronia.aoc.solution.Samples;
+import com.github.pareronia.aoc.solution.SolutionBase;
 
-public class AoC2020_04 extends AoCBase {
+public class AoC2020_04
+        extends SolutionBase<Set<AoC2020_04.Passport>, Integer, Integer> {
 	
-	private final Set<Passport> passports;
-	
-	private AoC2020_04(final List<String> input, final boolean debug) {
+	private AoC2020_04(final boolean debug) {
 		super(debug);
-		this.passports = parse(input);
 	}
 	
-	public static AoC2020_04 create(final List<String> input) {
-		return new AoC2020_04(input, false);
+	public static AoC2020_04 create() {
+		return new AoC2020_04(false);
 	}
 
-	public static AoC2020_04 createDebug(final List<String> input) {
-		return new AoC2020_04(input, true);
+	public static AoC2020_04 createDebug() {
+		return new AoC2020_04(true);
 	}
 	
-	private Set<Passport> parse(final List<String> inputs) {
-		final Function<List<String>, Passport> buildPassport = block -> {
-			final Passport.PassportBuilder passportBuilder = Passport.builder();
-			final Consumer<String> applyField = field -> {
-				final String[] fieldSplits = field.split(":");
-				try {
-					Passport.PassportBuilder.class
-						.getMethod(fieldSplits[0], String.class)
-						.invoke(passportBuilder, fieldSplits[1]);
-				} catch (IllegalAccessException | IllegalArgumentException
-						| InvocationTargetException
-						| NoSuchMethodException | SecurityException e) {
-					throw new RuntimeException(e);
-				}
-			};
-			block.stream().flatMap(line -> Arrays.stream(line.split(" ")))
-			    .forEach(applyField);
-			return passportBuilder.build();
-		};
-		return toBlocks(inputs).stream()
-				.map(buildPassport)
+	@Override
+    protected Set<Passport> parseInput(final List<String> inputs) {
+		return StringOps.toBlocks(inputs).stream()
+				.map(Passport::fromInput)
 				.collect(toSet());
 	}
 
 	@Override
-	public Long solvePart1() {
-		return countValid(Passport::isValid1);
+	public Integer solvePart1(final Set<Passport> passports) {
+		return countValid(passports, Passport::isValid1);
 	}
 
 	@Override
-	public Long solvePart2() {
-		return countValid(Passport::isValid2);
+	public Integer solvePart2(final Set<Passport> passports) {
+		return countValid(passports, Passport::isValid2);
 	}
 
-	private long countValid(final Predicate<Passport> predicate) {
-		return this.passports.stream().filter(predicate).count();
+	private int countValid(
+	        final Set<Passport> passports,
+	        final Predicate<Passport> predicate
+	) {
+		return (int) passports.stream().filter(predicate).count();
 	}
 
+	@Samples({
+	    @Sample(method = "part1", input = TEST, expected = "10"),
+	    @Sample(method = "part2", input = TEST, expected = "6"),
+	})
 	public static void main(final String[] args) throws Exception {
-		assert AoC2020_04.createDebug(TEST).solvePart1() == 10;
-		assert AoC2020_04.createDebug(TEST).solvePart2() == 6;
-		
-		final Puzzle puzzle = Puzzle.create(2020, 4);
-		final List<String> input = puzzle.getInputData();
-		puzzle.check(
-		    () -> lap("Part 1", AoC2020_04.create(input)::solvePart1),
-		    () -> lap("Part 2", AoC2020_04.create(input)::solvePart2)
-		);
+		AoC2020_04.create().run();
 	}
 	
-	private static final List<String> TEST = splitLines(
-			"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\r\n" +
-			"byr:1937 iyr:2017 cid:147 hgt:183cm\r\n" +
-			"\r\n" +
-			"iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\r\n" +
-			"hcl:#cfa07d byr:1929\r\n" +
-			"\r\n" +
-			"hcl:#ae17e1 iyr:2013\r\n" +
-			"eyr:2024\r\n" +
-			"ecl:brn pid:760753108 byr:1931\r\n" +
-			"hgt:179cm\r\n" +
-			"\r\n" +
-			"hcl:#cfa07d eyr:2025 pid:166559648\r\n" +
-			"iyr:2011 ecl:brn hgt:59in\r\n" +
-			"\r\n" +
-			"eyr:1972 cid:100\r\n" +
-			"hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926\r\n" +
-			"\r\n" +
-			"iyr:2019\r\n" +
-			"hcl:#602927 eyr:1967 hgt:170cm\r\n" +
-			"ecl:grn pid:012533040 byr:1946\r\n" +
-			"\r\n" +
-			"hcl:dab227 iyr:2012\r\n" +
-			"ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277\r\n" +
-			"\r\n" +
-			"hgt:59cm ecl:zzz\r\n" +
-			"eyr:2038 hcl:74454a iyr:2023\r\n" +
-			"pid:3556412378 byr:2007\r\n" +
-			"\r\n" +
-			"pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\r\n" +
-			"hcl:#623a2f\r\n" +
-			"\r\n" +
-			"eyr:2029 ecl:blu cid:129 byr:1989\r\n" +
-			"iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm\r\n" +
-			"\r\n" +
-			"hcl:#888785\r\n" +
-			"hgt:164cm byr:2001 iyr:2015 cid:88\r\n" +
-			"pid:545766238 ecl:hzl\r\n" +
-			"eyr:2022\r\n" +
-			"\r\n" +
-			"iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"
-	);
+	private static final String TEST = """
+	        ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+	        byr:1937 iyr:2017 cid:147 hgt:183cm
+	        
+	        iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+	        hcl:#cfa07d byr:1929
+	        
+	        hcl:#ae17e1 iyr:2013
+	        eyr:2024
+	        ecl:brn pid:760753108 byr:1931
+	        hgt:179cm
+	        
+	        hcl:#cfa07d eyr:2025 pid:166559648
+	        iyr:2011 ecl:brn hgt:59in
+	        
+	        eyr:1972 cid:100
+	        hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+	        
+	        iyr:2019
+	        hcl:#602927 eyr:1967 hgt:170cm
+	        ecl:grn pid:012533040 byr:1946
+	        
+	        hcl:dab227 iyr:2012
+	        ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+	        
+	        hgt:59cm ecl:zzz
+	        eyr:2038 hcl:74454a iyr:2023
+	        pid:3556412378 byr:2007
+	        
+	        pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+	        hcl:#623a2f
+	        
+	        eyr:2029 ecl:blu cid:129 byr:1989
+	        iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+	        
+	        hcl:#888785
+	        hgt:164cm byr:2001 iyr:2015 cid:88
+	        pid:545766238 ecl:hzl
+	        eyr:2022
+	        
+	        iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+	        """;
 	
 	record Passport(
 		String byr,  // (Birth Year)
@@ -133,19 +115,26 @@ public class AoC2020_04 extends AoCBase {
 		String ecl,  // (Eye Color)
 		String pid   // (Passport ID)
     ) {
+	    
+	    public static Passport fromInput(final List<String> block) {
+            final Passport.PassportBuilder passportBuilder = Passport.builder();
+            final Consumer<String> applyField = field -> {
+                final String[] fieldSplits = field.split(":");
+                try {
+                    Passport.PassportBuilder.class
+                        .getMethod(fieldSplits[0], String.class)
+                        .invoke(passportBuilder, fieldSplits[1]);
+                } catch (IllegalAccessException | IllegalArgumentException
+                        | InvocationTargetException
+                        | NoSuchMethodException | SecurityException e) {
+                    throw new RuntimeException(e);
+                }
+            };
+            block.stream().flatMap(line -> Arrays.stream(line.split(" ")))
+                .forEach(applyField);
+            return passportBuilder.build();
+	    }
 		
-		private static Passport create(final PassportBuilder builder) {
-		    return new Passport(
-                builder.byr,
-                builder.iyr,
-                builder.eyr,
-                builder.hgt,
-                builder.hcl,
-                builder.ecl,
-                builder.pid
-		    );
-		}
-
 		public boolean isValid1() {
 			return this.byr != null
 				&& this.iyr != null
@@ -169,10 +158,12 @@ public class AoC2020_04 extends AoCBase {
 		}
 		
 		private boolean hgtValid() {
-		    final Integer hgt = Integer.valueOf(this.hgt.substring(0, this.hgt.length() - 2));
-			if (this.hgt.endsWith("in")) {
+			final int len = this.hgt.length();
+            if (this.hgt.endsWith("in")) {
+			    final Integer hgt = Integer.valueOf(this.hgt.substring(0, len - 2));
 				return RangeInclusive.between(59, 76).contains(hgt);
 			} else if (this.hgt.endsWith("cm")) {
+			    final Integer hgt = Integer.valueOf(this.hgt.substring(0, len - 2));
 				return RangeInclusive.between(150, 193).contains(hgt);
 			} else {
 				return false;
@@ -256,7 +247,7 @@ public class AoC2020_04 extends AoCBase {
             }
 
             public Passport build() {
-		        return Passport.create(this);
+		        return new Passport(byr, iyr, eyr, hgt, hcl, ecl, pid);
 		    }
 		}
 	}
