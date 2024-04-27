d9f0e25a41665603335879f5eac3b69cbee3da5f
pareronia <49491686+pareronia@users.noreply.github.com>
2024-04-27 23:06:19 +0200

    AoC 2016 Day 4 - refactor

--- src/main/python/AoC2016_04.py
+++ src/main/python/AoC2016_04.py
@@ -4,13 +4,25 @@
 #
 
 from __future__ import annotations
-from typing import NamedTuple
+
 import re
-from aoc import my_aocd
+import sys
+from collections import Counter
+from typing import NamedTuple
 
+from aoc.common import InputData
+from aoc.common import SolutionBase
+from aoc.common import aoc_samples
 
-REGEXP = r'([-a-z]+)-([0-9]+)\[([a-z]{5})\]$'
-MATCH = r'[a-z]{9}-[a-z]{6}-[a-z]{7}$'
+REGEXP = r"([-a-z]+)-([0-9]+)\[([a-z]{5})\]$"
+MATCH = r"[a-z]{9}-[a-z]{6}-[a-z]{7}$"
+
+TEST = """\
+aaaaa-bbb-z-y-x-123[abxyz]
+a-b-c-d-e-f-g-h-987[abcde]
+not-a-real-room-404[oarel]
+totally-real-room-200[decoy]
+"""
 
 
 class Room(NamedTuple):
@@ -19,61 +31,57 @@ class Room(NamedTuple):
     checksum: str
 
     @classmethod
-    def of(cls, name: str, sector_id: str, checksum: str) -> Room:
-        return Room(name, int(sector_id), checksum)
+    def from_input(cls, line: str) -> Room:
+        m = re.search(REGEXP, line)
+        assert m is not None
+        return Room(m.groups()[0], int(m.groups()[1]), m.groups()[2])
 
     def is_real(self) -> bool:
-        hist = {c: self.name.count(c)
-                for c in set(self.name.replace('-', ''))}
-        items = list(hist.items())
-        items.sort(key=lambda x: x[1]*-100 + ord(x[0]))
-        return "".join([x for x, y in items][:5]) == self.checksum
+        most_common = sorted(
+            Counter(self.name.replace("-", "")).items(),
+            key=lambda item: item[1] * -100 + ord(item[0]),
+        )
+        return "".join(x for x, _ in most_common[:5]) == self.checksum
 
     def decrypt(self) -> str:
         shift = self.sector_id % 26
-        return "".join([chr((ord(c) + shift - 97) % 26 + 97)
-                        if c != '-' else ' '
-                        for c in self.name])
+        return "".join(
+            chr((ord(c) + shift - 97) % 26 + 97) if c != "-" else " "
+            for c in self.name
+        )
 
 
-def parse(inputs: tuple[str]) -> tuple[Room]:
-    return (Room.of(*re.search(REGEXP, input_).groups())
-            for input_ in inputs)
+Input = list[Room]
+Output1 = int
+Output2 = int
 
 
-def part_1(inputs: tuple[str]) -> int:
-    return sum(room.sector_id
-               for room in
-               parse(inputs) if room.is_real())
+class Solution(SolutionBase[Input, Output1, Output2]):
+    def parse_input(self, input_data: InputData) -> Input:
+        return [Room.from_input(line) for line in input_data]
+
+    def part_1(self, rooms: Input) -> int:
+        return sum(room.sector_id for room in rooms if room.is_real())
+
+    def part_2(self, rooms: Input) -> int:
+        return next(
+            room.sector_id
+            for room in rooms
+            if re.match(MATCH, room.name) is not None
+            and room.decrypt() == "northpole object storage"
+        )
+
+    @aoc_samples((("part_1", TEST, 1514),))
+    def samples(self) -> None:
+        pass
 
 
-def part_2(inputs: tuple[str]) -> int:
-    matches = [room.sector_id for room in parse(inputs)
-               if re.match(MATCH, room.name) is not None
-               and room.decrypt() == 'northpole object storage']
-    assert len(matches) == 1
-    return matches[0]
-
-
-TEST = '''\
-aaaaa-bbb-z-y-x-123[abxyz]
-a-b-c-d-e-f-g-h-987[abcde]
-not-a-real-room-404[oarel]
-totally-real-room-200[decoy]
-'''.splitlines()
+solution = Solution(2016, 4)
 
 
 def main() -> None:
-    my_aocd.print_header(2016, 4)
-
-    assert part_1(TEST) == 1514
-
-    inputs = my_aocd.get_input(2016, 4, 935)
-    result1 = part_1(inputs)
-    print(f"Part 1: {result1}")
-    result2 = part_2(inputs)
-    print(f"Part 2: {result2}")
+    solution.run(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
