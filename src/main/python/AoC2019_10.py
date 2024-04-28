ae65b885b555179d2c6141a55c573b000cc6f77b
pareronia <49491686+pareronia@users.noreply.github.com>
2024-04-28 22:38:53 +0200

    AoC 2019 Day 10 - refactor

--- src/main/python/AoC2019_10.py
+++ src/main/python/AoC2019_10.py
@@ -9,18 +9,15 @@ import math
 import sys
 from functools import reduce
 from typing import Iterator
+from typing import NamedTuple
 
 from aoc.common import InputData
 from aoc.common import SolutionBase
 from aoc.common import aoc_samples
 from aoc.grid import CharGrid
 
-Input = CharGrid
-Output1 = int
-Output2 = int
 Position = tuple[int, int]
 ASTEROID = "#"
-
 
 TEST1 = """\
 .#..#
@@ -89,62 +86,82 @@ TEST5 = """\
 """
 
 
+def asteroids(grid: CharGrid) -> Iterator[Position]:
+    return (
+        (c, r)
+        for r in range(grid.get_height())
+        for c in range(grid.get_width())
+        if grid.get_value_at(r, c) == ASTEROID
+    )
+
+
+class Asteroid(NamedTuple):
+    position: Position
+    others: dict[float, Position]
+
+    @classmethod
+    def from_(cls, position: Position, grid: CharGrid) -> Asteroid:
+        return Asteroid(position, Asteroid.angles(grid, position))
+
+    @classmethod
+    def angles(
+        cls, grid: CharGrid, asteroid: Position
+    ) -> dict[float, Position]:
+        def merge(
+            d: dict[float, Position], other: Position
+        ) -> dict[float, Position]:
+
+            def angle(asteroid: Position, other: Position) -> float:
+                angle = (
+                    math.atan2(other[1] - asteroid[1], other[0] - asteroid[0])
+                    + math.pi / 2
+                )
+                return angle + 2 * math.pi if angle < 0 else angle
+
+            def distance_squared(a: Position, b: Position) -> int:
+                dx = a[0] - b[0]
+                dy = a[1] - b[1]
+                return dx**2 + dy**2
+
+            a = angle(asteroid, other)
+            d[a] = (
+                min(
+                    other,
+                    d[a],
+                    key=lambda x: distance_squared(x, asteroid),
+                )
+                if a in d
+                else other
+            )
+            return d
+
+        return reduce(
+            merge,
+            filter(lambda x: x != asteroid, asteroids(grid)),
+            dict[float, Position](),
+        )
+
+
+Input = list[Asteroid]
+Output1 = int
+Output2 = int
+
+
 class Solution(SolutionBase[Input, Output1, Output2]):
     def parse_input(self, input_data: InputData) -> Input:
-        return CharGrid.from_strings(list(input_data))
+        grid = CharGrid.from_strings(list(input_data))
+        return [Asteroid.from_(p, grid) for p in asteroids(grid)]
 
-    def part_1(self, grid: Input) -> Output1:
-        return len(self.best(grid))
+    def best(self, asteroids: list[Asteroid]) -> Asteroid:
+        return max(asteroids, key=lambda a: len(a.others))
 
-    def part_2(self, grid: Input) -> Output2:
-        d = self.best(grid)
+    def part_1(self, asteroids: Input) -> Output1:
+        return len(self.best(asteroids).others)
+
+    def part_2(self, asteroids: Input) -> Output2:
+        d = self.best(asteroids).others
         x, y = d[sorted(d.keys())[199]]
         return x * 100 + y
-
-    def best(self, grid: CharGrid) -> dict[float, Position]:
-        def asteroids() -> Iterator[Position]:
-            for r in range(grid.get_height()):
-                for c in range(grid.get_width()):
-                    if grid.get_value_at(r, c) == ASTEROID:
-                        yield (c, r)
-
-        def angles(asteroid: Position) -> dict[float, Position]:
-            def merge(
-                d: dict[float, Position], other: Position
-            ) -> dict[float, Position]:
-                def angle(asteroid: Position, other: Position) -> float:
-                    angle = (
-                        math.atan2(
-                            other[1] - asteroid[1], other[0] - asteroid[0]
-                        )
-                        + math.pi / 2
-                    )
-                    return angle + 2 * math.pi if angle < 0 else angle
-
-                def distance_squared(a: Position, b: Position) -> int:
-                    dx = a[0] - b[0]
-                    dy = a[1] - b[1]
-                    return dx**2 + dy**2
-
-                a = angle(asteroid, other)
-                d[a] = (
-                    min(
-                        other,
-                        d[a],
-                        key=lambda x: distance_squared(x, asteroid),
-                    )
-                    if a in d
-                    else other
-                )
-                return d
-
-            return reduce(
-                merge,
-                filter(lambda x: x != asteroid, asteroids()),
-                dict[float, Position](),
-            )
-
-        return max((angles((x, y)) for x, y in asteroids()), key=len)
 
     @aoc_samples(
         (
