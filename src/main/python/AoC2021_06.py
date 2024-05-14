617ff0b57c724c1a098ede6e8f72a056d22a4c16
pareronia <49491686+pareronia@users.noreply.github.com>
2024-05-14 17:24:59 +0000

    Refactor to SolutionBase

--- src/main/python/AoC2021_06.py
+++ src/main/python/AoC2021_06.py
@@ -3,47 +3,57 @@
 # Advent of Code 2021 Day 6
 #
 
+import sys
 from collections import deque
-from aoc import my_aocd
+
+from aoc.common import InputData
+from aoc.common import SolutionBase
+from aoc.common import aoc_samples
+
+TEST = "3,4,3,1,2"
 
 
-def _solve(inputs: tuple[str], days: int) -> int:
-    assert len(inputs) == 1
-    fishies = deque([0] * 9)
-    for n in inputs[0].split(','):
-        fishies[int(n)] += 1
-    for i in range(days):
-        zeroes = fishies[0]
-        fishies.rotate(-1)
-        fishies[6] += zeroes
-    return sum(fishies)
+Input = list[int]
+Output1 = int
+Output2 = int
 
 
-def part_1(inputs: tuple[str]) -> int:
-    return _solve(inputs, 80)
+class Solution(SolutionBase[Input, Output1, Output2]):
+    def parse_input(self, input_data: InputData) -> Input:
+        return [int(_) for _ in list(input_data)[0].split(",")]
+
+    def solve(self, inputs: list[int], days: int) -> int:
+        fishies = deque([0] * 9)
+        for n in inputs:
+            fishies[n] += 1
+        for i in range(days):
+            zeroes = fishies[0]
+            fishies.rotate(-1)
+            fishies[6] += zeroes
+        return sum(fishies)
+
+    def part_1(self, inputs: Input) -> Output1:
+        return self.solve(inputs, days=80)
+
+    def part_2(self, inputs: Input) -> Output2:
+        return self.solve(inputs, days=256)
+
+    @aoc_samples(
+        (
+            ("part_1", TEST, 5_934),
+            ("part_2", TEST, 26_984_457_539),
+        )
+    )
+    def samples(self) -> None:
+        pass
 
 
-def part_2(inputs: tuple[str]) -> int:
-    return _solve(inputs, 256)
-
-
-TEST = """\
-3,4,3,1,2
-""".splitlines()
+solution = Solution(2021, 6)
 
 
 def main() -> None:
-    my_aocd.print_header(2021, 6)
-
-    assert part_1(TEST) == 5_934
-    assert part_2(TEST) == 26_984_457_539
-
-    inputs = my_aocd.get_input(2021, 6, 1)
-    result1 = part_1(inputs)
-    print(f"Part 1: {result1}")
-    result2 = part_2(inputs)
-    print(f"Part 2: {result2}")
+    solution.run(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/main/python/AoC2021_09.py
+++ src/main/python/AoC2021_09.py
@@ -3,60 +3,16 @@
 # Advent of Code 2021 Day 9
 #
 
-from collections.abc import Iterator
+import sys
 from math import prod
-from aoc import my_aocd
-from aoc.grid import Cell, IntGrid
-from aoc.common import log
+from typing import Iterator
+
+from aoc.common import InputData
+from aoc.common import SolutionBase
+from aoc.common import aoc_samples
 from aoc.graph import flood_fill
-import aocd
-
-
-def _parse(inputs: tuple[str, ...]) -> IntGrid:
-    return IntGrid([[int(_) for _ in list(r)] for r in inputs])
-
-
-def _find_lows(grid: IntGrid) -> Iterator[Cell]:
-    for low in (
-        c
-        for c in grid.get_cells()
-        if (
-            all(
-                grid.get_value(c) < grid.get_value(n)
-                for n in grid.get_capital_neighbours(c)
-            )
-        )
-    ):
-        log(low)
-        yield low
-
-
-def part_1(inputs: tuple[str, ...]) -> int:
-    grid = _parse(inputs)
-    log(grid)
-    return sum([grid.get_value(c) + 1 for c in _find_lows(grid)])
-
-
-def _size_of_basin_around_low(grid: IntGrid, c: Cell) -> int:
-    basin = flood_fill(
-        c,
-        lambda c: (
-            n for n in grid.get_capital_neighbours(c) if grid.get_value(n) != 9
-        ),
-    )
-    log(basin)
-    log(len(basin))
-    return len(basin)
-
-
-def part_2(inputs: tuple[str, ...]) -> int:
-    grid = _parse(inputs)
-    return prod(
-        sorted(
-            _size_of_basin_around_low(grid, low) for low in _find_lows(grid)
-        )[-3:]
-    )
-
+from aoc.grid import Cell
+from aoc.grid import IntGrid
 
 TEST = """\
 2199943210
@@ -64,22 +20,65 @@ TEST = """\
 9856789892
 8767896789
 9899965678
-""".splitlines()
+"""
+
+Input = IntGrid
+Output1 = int
+Output2 = int
+
+
+class Solution(SolutionBase[Input, Output1, Output2]):
+    def parse_input(self, input_data: InputData) -> Input:
+        return IntGrid.from_strings(list(input_data))
+
+    def find_lows(self, grid: IntGrid) -> Iterator[Cell]:
+        return (
+            c
+            for c in grid.get_cells()
+            if (
+                all(
+                    grid.get_value(c) < grid.get_value(n)
+                    for n in grid.get_capital_neighbours(c)
+                )
+            )
+        )
+
+    def part_1(self, grid: Input) -> int:
+        return sum(grid.get_value(c) + 1 for c in self.find_lows(grid))
+
+    def part_2(self, grid: Input) -> int:
+        def size_of_basin_around_low(c: Cell) -> int:
+            basin = flood_fill(
+                c,
+                lambda c: (
+                    n
+                    for n in grid.get_capital_neighbours(c)
+                    if grid.get_value(n) != 9
+                ),
+            )
+            return len(basin)
+
+        return prod(
+            sorted(
+                size_of_basin_around_low(low) for low in self.find_lows(grid)
+            )[-3:]
+        )
+
+    @aoc_samples(
+        (
+            ("part_1", TEST, 15),
+            ("part_2", TEST, 1134),
+        )
+    )
+    def samples(self) -> None:
+        pass
+
+
+solution = Solution(2021, 9)
 
 
 def main() -> None:
-    puzzle = aocd.models.Puzzle(2021, 9)
-    my_aocd.print_header(puzzle.year, puzzle.day)
-
-    assert part_1(TEST) == 15  # type:ignore[arg-type]
-    assert part_2(TEST) == 1134  # type:ignore[arg-type]
-
-    inputs = my_aocd.get_input(puzzle.year, puzzle.day, 100)
-    result1 = part_1(inputs)
-    print(f"Part 1: {result1}")
-    result2 = part_2(inputs)
-    print(f"Part 2: {result2}")
-    my_aocd.check_results(puzzle, result1, result2)
+    solution.run(sys.argv)
 
 
 if __name__ == "__main__":
