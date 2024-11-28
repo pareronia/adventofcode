c8c77a927b18a73af53c0a7d2ab1bc77ce7ec275
pareronia <49491686+pareronia@users.noreply.github.com>
2024-11-28 16:51:42 +0100

    Refactor to SolutionBase

--- src/main/python/AoC2015_03.py
+++ src/main/python/AoC2015_03.py
@@ -3,67 +3,78 @@
 # Advent of Code 2015 Day 3
 #
 
-import aocd
-from aoc import my_aocd
-from aoc.navigation import NavigationWithHeading, Heading
+import sys
+
+from aoc.common import InputData
+from aoc.common import SolutionBase
+from aoc.common import aoc_samples
 from aoc.geometry import Position
+from aoc.navigation import Heading
+from aoc.navigation import NavigationWithHeading
+
+START = Position(0, 0)
+Navigation = NavigationWithHeading
+Input = list[Heading]
+Output1 = int
+Output2 = int
+
+TEST1 = ">"
+TEST2 = "^>v<"
+TEST3 = "^v^v^v^v^v"
+TEST4 = "^v"
 
 
-def _count_unique_positions(positions: list[Position]) -> int:
-    return len({p for p in positions})
+class Solution(SolutionBase[Input, Output1, Output2]):
+    def parse_input(self, input_data: InputData) -> Input:
+        return [Heading.from_str(ch) for ch in list(input_data)[0]]
+
+    def count_unique_positions(self, positions: list[Position]) -> int:
+        return len({p for p in positions})
+
+    def add_navigation_instruction(
+        self, navigation: Navigation, c: Heading
+    ) -> None:
+        navigation.drift(c, 1)
+
+    def part_1(self, inputs: Input) -> Output1:
+        navigation = Navigation(START, Heading.NORTH)
+        for c in inputs:
+            self.add_navigation_instruction(navigation, c)
+        return self.count_unique_positions(
+            navigation.get_visited_positions(True)
+        )
+
+    def part_2(self, inputs: Input) -> Output2:
+        santa_navigation = Navigation(START, Heading.NORTH)
+        robot_navigation = Navigation(START, Heading.NORTH)
+        for i, c in enumerate(inputs):
+            self.add_navigation_instruction(
+                robot_navigation if i % 2 else santa_navigation, c
+            )
+        return self.count_unique_positions(
+            santa_navigation.get_visited_positions(True)
+            + robot_navigation.get_visited_positions(True)
+        )
+
+    @aoc_samples(
+        (
+            ("part_1", TEST1, 2),
+            ("part_1", TEST2, 4),
+            ("part_1", TEST3, 2),
+            ("part_2", TEST4, 3),
+            ("part_2", TEST2, 3),
+            ("part_2", TEST3, 11),
+        )
+    )
+    def samples(self) -> None:
+        pass
 
 
-def _add_navigation_instruction(
-    navigation: NavigationWithHeading, c: str
-) -> None:
-    navigation.drift(Heading.from_str(c), 1)
-
-
-def part_1(inputs: tuple[str, ...]) -> int:
-    assert len(inputs) == 1
-    navigation = NavigationWithHeading(Position(0, 0), Heading.NORTH)
-    for c in inputs[0]:
-        _add_navigation_instruction(navigation, c)
-    return _count_unique_positions(navigation.get_visited_positions(True))
-
-
-def part_2(inputs: tuple[str, ...]) -> int:
-    assert len(inputs) == 1
-    santa_navigation = NavigationWithHeading(Position(0, 0), Heading.NORTH)
-    robot_navigation = NavigationWithHeading(Position(0, 0), Heading.NORTH)
-    for i, c in enumerate(inputs[0]):
-        if i % 2 != 0:
-            _add_navigation_instruction(robot_navigation, c)
-        else:
-            _add_navigation_instruction(santa_navigation, c)
-    visited = santa_navigation.get_visited_positions(True)
-    visited.extend(robot_navigation.get_visited_positions(True))
-    return _count_unique_positions(visited)
-
-
-TEST1 = ">".splitlines()
-TEST2 = "^>v<".splitlines()
-TEST3 = "^v^v^v^v^v".splitlines()
-TEST4 = "^v".splitlines()
+solution = Solution(2015, 3)
 
 
 def main() -> None:
-    puzzle = aocd.models.Puzzle(2015, 3)
-    my_aocd.print_header(puzzle.year, puzzle.day)
-
-    assert part_1(TEST1) == 2  # type:ignore[arg-type]
-    assert part_1(TEST2) == 4  # type:ignore[arg-type]
-    assert part_1(TEST3) == 2  # type:ignore[arg-type]
-    assert part_2(TEST4) == 3  # type:ignore[arg-type]
-    assert part_2(TEST2) == 3  # type:ignore[arg-type]
-    assert part_2(TEST3) == 11  # type:ignore[arg-type]
-
-    inputs = my_aocd.get_input_data(puzzle, 1)
-    result1 = part_1(inputs)
-    print(f"Part 1: {result1}")
-    result2 = part_2(inputs)
-    print(f"Part 2: {result2}")
-    my_aocd.check_results(puzzle, result1, result2)
+    solution.run(sys.argv)
 
 
 if __name__ == "__main__":
--- src/main/python/AoC2015_04.py
+++ src/main/python/AoC2015_04.py
@@ -3,49 +3,56 @@
 # Advent of Code 2015 Day 4
 #
 
+import sys
 from hashlib import md5
-from aoc import my_aocd
+
+from aoc.common import InputData
+from aoc.common import SolutionBase
+from aoc.common import aoc_samples
 from aoc.common import spinner
 
-
-def _find_md5_starting_with_zeroes(input_: str, zeroes: int) -> int:
-    i = 0
-    val = input_
-    target = "0" * zeroes
-    while val[:zeroes] != target:
-        i += 1
-        spinner(i)
-        str2hash = input_ + str(i)
-        val = md5(str2hash.encode()).hexdigest()  # nosec
-    return i
+Input = str
+Output1 = int
+Output2 = int
 
 
-def part_1(inputs: tuple[str]) -> int:
-    assert len(inputs) == 1
-    return _find_md5_starting_with_zeroes(inputs[0], 5)
+class Solution(SolutionBase[Input, Output1, Output2]):
+    def parse_input(self, input_data: InputData) -> Input:
+        return list(input_data)[0]
+
+    def find_md5_starting_with_zeroes(self, seed: str, zeroes: int) -> int:
+        i = 0
+        val = seed
+        target = "0" * zeroes
+        while val[:zeroes] != target:
+            i += 1
+            spinner(i)
+            str2hash = seed + str(i)
+            val = md5(str2hash.encode()).hexdigest()  # nosec
+        return i
+
+    def part_1(self, seed: Input) -> Output1:
+        return self.find_md5_starting_with_zeroes(seed, 5)
+
+    def part_2(self, seed: Input) -> Output2:
+        return self.find_md5_starting_with_zeroes(seed, 6)
+
+    @aoc_samples(
+        (
+            ("part_1", "abcdef", 609043),
+            ("part_1", "pqrstuv", 1048970),
+        )
+    )
+    def samples(self) -> None:
+        pass
 
 
-def part_2(inputs: tuple[str]) -> int:
-    assert len(inputs) == 1
-    return _find_md5_starting_with_zeroes(inputs[0], 6)
-
-
-TEST1 = "abcdef".splitlines()
-TEST2 = "pqrstuv".splitlines()
+solution = Solution(2015, 4)
 
 
 def main() -> None:
-    my_aocd.print_header(2015, 4)
-
-    assert part_1(TEST1) == 609043
-    assert part_1(TEST2) == 1048970
-
-    inputs = my_aocd.get_input(2015, 4, 1)
-    result1 = part_1(inputs)
-    print(f"Part 1: {result1}")
-    result2 = part_2(inputs)
-    print(f"Part 2: {result2}")
+    solution.run(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
