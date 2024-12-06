35f2894fd071266803feb9017e39e9a809c5ef70
pareronia <49491686+pareronia@users.noreply.github.com>
2024-12-06 10:43:07 +0100

    AoC 2024 Day 6 [much faster]

--- src/main/python/AoC2024_06.py
+++ src/main/python/AoC2024_06.py
@@ -4,7 +4,7 @@
 #
 
 import sys
-from collections import defaultdict
+from collections import OrderedDict
 
 from aoc.common import InputData
 from aoc.common import SolutionBase
@@ -46,39 +46,40 @@ class Solution(SolutionBase[Input, Output1, Output2]):
         return h, w, obs, start
 
     def route(
-        self, h: int, w: int, obs: set[Cell], pos: Cell
-    ) -> tuple[bool, set[Cell]]:
-        d, dir = 0, DIRS[0]
-        seen = defaultdict[Cell, set[tuple[int, int]]](set)
-        seen[pos].add(dir)
+        self, h: int, w: int, obs: set[Cell], pos: Cell, dir: int
+    ) -> tuple[bool, OrderedDict[Cell, list[int]]]:
+        seen = OrderedDict[Cell, list[int]]()
+        seen.setdefault(pos, list()).append(dir)
         while True:
-            nxt = (pos[0] - dir[1], pos[1] + dir[0])
+            nxt = (pos[0] - DIRS[dir][1], pos[1] + DIRS[dir][0])
             if not (0 <= nxt[0] < h and 0 <= nxt[1] < w):
-                return False, {_ for _ in seen.keys()}
+                return False, seen
             if nxt in obs:
-                d = (d + 1) % len(DIRS)
-                dir = DIRS[d]
+                dir = (dir + 1) % len(DIRS)
             else:
                 pos = nxt
-            if dir in seen[pos]:
-                return True, set()
-            seen[pos].add(dir)
+            if pos in seen and dir in seen[pos]:
+                return True, OrderedDict()
+            seen.setdefault(pos, list()).append(dir)
 
     def part_1(self, input: Input) -> Output1:
         h, w, obs, start = input
-        _, seen = self.route(h, w, obs, start)
+        _, seen = self.route(h, w, obs, start, 0)
         return len(seen)
 
     def part_2(self, input: Input) -> Output2:
         h, w, obs, start = input
-        _, cells = self.route(h, w, obs, start)
+        _, seen = self.route(h, w, obs, start, 0)
+        prev_pos, prev_dirs = seen.popitem(last=False)
         ans = 0
-        for cell in cells:
-            obs.add(cell)
-            loop, _ = self.route(h, w, obs, start)
+        while len(seen) > 0:
+            pos, dirs = seen.popitem(last=False)
+            obs.add(pos)
+            loop, _ = self.route(h, w, obs, prev_pos, prev_dirs.pop(0))
             if loop:
                 ans += 1
-            obs.remove(cell)
+            obs.remove(pos)
+            prev_pos, prev_dirs = pos, dirs
         return ans
 
     @aoc_samples(
