e406ad87a0d471f675a6ece2d828c2c5fd173063
pareronia <49491686+pareronia@users.noreply.github.com>
2024-12-12 15:51:34 +0100

    AoC 2024 Day 12 - cleanup

--- src/main/python/AoC2024_12.py
+++ src/main/python/AoC2024_12.py
@@ -5,6 +5,8 @@
 
 import sys
 from collections import defaultdict
+from typing import Callable
+from typing import Iterator
 
 from aoc.common import InputData
 from aoc.common import SolutionBase
@@ -12,12 +14,17 @@ from aoc.common import aoc_samples
 from aoc.geometry import Direction
 from aoc.graph import flood_fill
 from aoc.grid import Cell
-from aoc.grid import CharGrid
 
-Input = CharGrid
+Input = InputData
 Output1 = int
 Output2 = int
 
+CORNER_DIRS = [
+    [Direction.LEFT_AND_UP, Direction.LEFT, Direction.UP],
+    [Direction.RIGHT_AND_UP, Direction.RIGHT, Direction.UP],
+    [Direction.RIGHT_AND_DOWN, Direction.RIGHT, Direction.DOWN],
+    [Direction.LEFT_AND_DOWN, Direction.LEFT, Direction.DOWN],
+]
 
 TEST1 = """\
 AAAA
@@ -63,118 +70,49 @@ AAAAAA
 
 class Solution(SolutionBase[Input, Output1, Output2]):
     def parse_input(self, input_data: InputData) -> Input:
-        return CharGrid.from_strings(list(input_data))
+        return input_data
 
-    def get_regions(self, grid: CharGrid) -> dict[str, list[set[Cell]]]:
-        p = defaultdict[str, set[Cell]](set)
-        for plot in grid.get_cells():
-            p[grid.get_value(plot)].add(plot)
-        regions = defaultdict[str, list[set[Cell]]](list)
-        for k, all_p in p.items():
-            while all_p:
-                r = flood_fill(
-                    next(iter(all_p)),
-                    lambda cell: (
-                        n
-                        for n in grid.get_capital_neighbours(cell)
-                        if n in all_p
-                    ),
-                )
-                regions[k].append(r)
-                all_p -= r
-        return regions
-
-    def part_1(self, grid: Input) -> Output1:
-        regions = self.get_regions(grid)
-        d = list[list[int]]()
-        for plant in regions:
-            for region in regions[plant]:
-                dd = list[int]()
-                for plot in region:
-                    c = 4
-                    for n in grid.get_capital_neighbours(plot):
-                        if grid.get_value(n) == plant:
-                            c -= 1
-                    dd.append(c)
-                d.append(dd)
-        return sum(len(dd) * sum(dd) for dd in d)
-
-    def part_2(self, grid: Input) -> Output2:
-        def count_corners(plot: Cell, region: set[Cell]) -> int:
-            ans = 0
-            DO = [
-                [Direction.LEFT, Direction.LEFT_AND_UP, Direction.UP],
-                [Direction.UP, Direction.RIGHT_AND_UP, Direction.RIGHT],
-                [Direction.RIGHT, Direction.RIGHT_AND_DOWN, Direction.DOWN],
-                [Direction.DOWN, Direction.LEFT_AND_DOWN, Direction.LEFT],
-            ]
-            for d in DO:
-                o = list(
-                    filter(
-                        lambda n: n in region, map(lambda dd: plot.at(dd), d)
+    def solve(
+        self, input: Input, count: Callable[[Cell, set[Cell]], int]
+    ) -> int:
+        def get_regions(input: Input) -> Iterator[set[Cell]]:
+            plots_by_plant = defaultdict[str, set[Cell]](set)
+            for r, row in enumerate(input):
+                for c, plant in enumerate(row):
+                    plots_by_plant[plant].add(Cell(r, c))
+            for all_plots_with_plant in plots_by_plant.values():
+                while all_plots_with_plant:
+                    region = flood_fill(
+                        next(iter(all_plots_with_plant)),
+                        lambda cell: (
+                            n
+                            for n in cell.get_capital_neighbours()
+                            if n in all_plots_with_plant
+                        ),
                     )
-                )
-                if len(o) == 0:
-                    ans += 1
-            if (
-                plot.at(Direction.LEFT_AND_DOWN) not in region
-                and plot.at(Direction.LEFT) in region
-                and plot.at(Direction.DOWN) in region
-            ):
-                ans += 1
-            if (
-                plot.at(Direction.LEFT_AND_UP) not in region
-                and plot.at(Direction.LEFT) in region
-                and plot.at(Direction.UP) in region
-            ):
-                ans += 1
-            if (
-                plot.at(Direction.RIGHT_AND_DOWN) not in region
-                and plot.at(Direction.RIGHT) in region
-                and plot.at(Direction.DOWN) in region
-            ):
-                ans += 1
-            if (
-                plot.at(Direction.RIGHT_AND_UP) not in region
-                and plot.at(Direction.RIGHT) in region
-                and plot.at(Direction.UP) in region
-            ):
-                ans += 1
-            if (
-                plot.at(Direction.LEFT_AND_DOWN) in region
-                and plot.at(Direction.LEFT) not in region
-                and plot.at(Direction.DOWN) not in region
-            ):
-                ans += 1
-            if (
-                plot.at(Direction.LEFT_AND_UP) in region
-                and plot.at(Direction.LEFT) not in region
-                and plot.at(Direction.UP) not in region
-            ):
-                ans += 1
-            if (
-                plot.at(Direction.RIGHT_AND_DOWN) in region
-                and plot.at(Direction.RIGHT) not in region
-                and plot.at(Direction.DOWN) not in region
-            ):
-                ans += 1
-            if (
-                plot.at(Direction.RIGHT_AND_UP) in region
-                and plot.at(Direction.RIGHT) not in region
-                and plot.at(Direction.UP) not in region
-            ):
-                ans += 1
-            return ans
+                    yield region
+                    all_plots_with_plant -= region
 
-        d = list[list[int]]()
-        regions = self.get_regions(grid)
-        for plant in regions:
-            for region in regions[plant]:
-                dd = list[int]()
-                for plot in region:
-                    dd.append(count_corners(plot, region))
-                d.append(dd)
-        return sum(len(dd) * sum(dd) for dd in d)
+        return sum(
+            sum(count(plot, region) for plot in region) * len(region)
+            for region in get_regions(input)
+        )
+
+    def part_1(self, input: Input) -> Output1:
+        def count_edges(plot: Cell, region: set[Cell]) -> int:
+            return 4 - sum(n in region for n in plot.get_capital_neighbours())
+
+        return self.solve(input, count_edges)
+
+    def part_2(self, input: Input) -> Output2:
+        def count_corners(plot: Cell, region: set[Cell]) -> int:
+            return sum(
+                tuple(1 if plot.at(d[i]) in region else 0 for i in range(3))
+                in ((0, 0, 0), (1, 0, 0), (0, 1, 1))
+                for d in CORNER_DIRS
+            )
+
+        return self.solve(input, count_corners)
 
     @aoc_samples(
         (
