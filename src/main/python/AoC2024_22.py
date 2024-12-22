51ad5c7f7ff3e283c7523c0ceb2d5bb891c7bc46
pareronia <49491686+pareronia@users.noreply.github.com>
2024-12-22 21:34:09 +0100

    AoC 2024 Day 22 [faster / multiprocessing]

--- src/main/python/AoC2024_22.py
+++ src/main/python/AoC2024_22.py
@@ -3,8 +3,11 @@
 # Advent of Code 2024 Day 22
 #
 
+import multiprocessing
+import os
 import sys
 from collections import defaultdict
+from typing import Callable
 
 from aoc.common import InputData
 from aoc.common import SolutionBase
@@ -39,39 +42,75 @@ class Solution(SolutionBase[Input, Output1, Output2]):
         num = (num ^ (num * 2048)) % 16777216
         return num
 
+    def solve(
+        self, seeds: Input, worker: Callable[[str, list[int]], None]
+    ) -> None:
+        if sys.platform.startswith("win"):
+            worker("main", seeds)
+        else:
+            n = os.process_cpu_count()
+            batch = [list[int]() for _ in range(n)]
+            cnt = 0
+            for seed in seeds:
+                batch[cnt % n].append(seed)
+                cnt += 1
+            jobs = []
+            for i in range(n):
+                p = multiprocessing.Process(
+                    target=worker, args=(str(i), batch[i])
+                )
+                jobs.append(p)
+                p.start()
+            for p in jobs:
+                p.join()
+
     def part_1(self, seeds: Input) -> Output1:
-        nums = seeds[:]
-        for _ in range(2000):
-            for i in range(len(nums)):
-                nums[i] = self.secret(nums[i])
-        return sum(nums)
+        m_ans = multiprocessing.Manager().dict()
+
+        def worker(id: str, seeds: list[int]) -> None:
+            ans = 0
+            for num in seeds:
+                for _ in range(2000):
+                    num = self.secret(num)
+                ans += num
+            m_ans[id] = ans
+
+        self.solve(seeds, worker)
+        return sum(m_ans.values())
 
     def part_2(self, seeds: Input) -> Output2:
-        p = defaultdict[int, int](int)
-        seen = [-1 for _ in range(19**4)]
-        for i, num in enumerate(seeds):
-            na = num
-            nb = self.secret(na)
-            nc = self.secret(nb)
-            nd = self.secret(nc)
-            b, c, d = (
-                na % 10 - nb % 10 + 9,
-                nb % 10 - nc % 10 + 9,
-                nc % 10 - nd % 10 + 9,
-            )
-            num = nd
-            prev_price = num % 10
-            for _ in range(3, 2000):
-                num = self.secret(num)
-                price = num % 10
-                a, b, c, d = b, c, d, prev_price - price + 9
-                prev_price = price
-                key = (a * 19**3) + (b * 19**2) + (c * 19) + d
-                if seen[key] == i:
-                    continue
-                seen[key] = i
-                p[key] += price
-        return max(p.values())
+        m_ans = multiprocessing.Array("i", [0 for _ in range(19**4)])
+
+        def worker(id: str, seeds: list[int]) -> None:
+            p = defaultdict[int, int](int)
+            seen = [-1 for _ in range(19**4)]
+            for i, num in enumerate(seeds):
+                na = num
+                nb = self.secret(na)
+                nc = self.secret(nb)
+                nd = self.secret(nc)
+                b, c, d = (
+                    na % 10 - nb % 10 + 9,
+                    nb % 10 - nc % 10 + 9,
+                    nc % 10 - nd % 10 + 9,
+                )
+                num = nd
+                prev_price = num % 10
+                for _ in range(3, 2000):
+                    num = self.secret(num)
+                    price = num % 10
+                    a, b, c, d = b, c, d, prev_price - price + 9
+                    prev_price = price
+                    key = (a * 19**3) + (b * 19**2) + (c * 19) + d
+                    if seen[key] == i:
+                        continue
+                    seen[key] = i
+                    p[key] += price
+            for k, v in p.items():
+                m_ans[k] += v
+
+        self.solve(seeds, worker)
+        return max(m_ans)  # type:ignore
 
     @aoc_samples(
         (
