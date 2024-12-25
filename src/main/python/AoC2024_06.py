978b5b91c723bfb89069be444e5b9b3b98506f8b
pareronia <49491686+pareronia@users.noreply.github.com>
2024-12-25 19:36:10 +0100

    AoC 2024 Day 6 - faster [multiprocessing]

--- src/main/python/AoC2024_06.py
+++ src/main/python/AoC2024_06.py
@@ -3,6 +3,8 @@
 # Advent of Code 2024 Day 6
 #
 
+import multiprocessing
+import os
 import sys
 from collections import OrderedDict
 
@@ -46,7 +48,13 @@ class Solution(SolutionBase[Input, Output1, Output2]):
         return h, w, obs, start
 
     def route(
-        self, h: int, w: int, obs: set[Cell], pos: Cell, dir: int
+        self,
+        h: int,
+        w: int,
+        obs: set[Cell],
+        extra_obs: Cell | None,
+        pos: Cell,
+        dir: int,
     ) -> tuple[bool, OrderedDict[Cell, list[int]]]:
         seen = OrderedDict[Cell, list[int]]()
         seen.setdefault(pos, list()).append(dir)
@@ -54,7 +62,7 @@ class Solution(SolutionBase[Input, Output1, Output2]):
             nxt = (pos[0] - DIRS[dir][1], pos[1] + DIRS[dir][0])
             if not (0 <= nxt[0] < h and 0 <= nxt[1] < w):
                 return False, seen
-            if nxt in obs:
+            if extra_obs is not None and nxt == extra_obs or nxt in obs:
                 dir = (dir + 1) % len(DIRS)
             else:
                 pos = nxt
@@ -64,23 +72,45 @@ class Solution(SolutionBase[Input, Output1, Output2]):
 
     def part_1(self, input: Input) -> Output1:
         h, w, obs, start = input
-        _, seen = self.route(h, w, obs, start, 0)
+        _, seen = self.route(h, w, obs, None, start, 0)
         return len(seen)
 
     def part_2(self, input: Input) -> Output2:
+        n = os.process_cpu_count()
+        ans = multiprocessing.Array("i", [0 for _ in range(n)])
+
+        def worker(
+            id: int, data: list[tuple[int, int, set[Cell], Cell, Cell, int]]
+        ) -> None:
+            ans[id] = sum(self.route(*row)[0] for row in data)
+
         h, w, obs, start = input
-        _, seen = self.route(h, w, obs, start, 0)
+        _, seen = self.route(h, w, obs, None, start, 0)
+        data = list[tuple[int, int, set[Cell], Cell, Cell, int]]()
         prev_pos, prev_dirs = seen.popitem(last=False)
-        ans = 0
         while len(seen) > 0:
             pos, dirs = seen.popitem(last=False)
-            obs.add(pos)
-            loop, _ = self.route(h, w, obs, prev_pos, prev_dirs.pop(0))
-            if loop:
-                ans += 1
-            obs.remove(pos)
+            data.append((h, w, obs, pos, prev_pos, prev_dirs.pop(0)))
             prev_pos, prev_dirs = pos, dirs
-        return ans
+        if sys.platform.startswith("win"):
+            worker(0, data)
+        else:
+            batch = [
+                list[tuple[int, int, set[Cell], Cell, Cell, int]]()
+                for _ in range(n)
+            ]
+            cnt = 0
+            for row in data:
+                batch[cnt % n].append(row)
+                cnt += 1
+            jobs = []
+            for i in range(n):
+                p = multiprocessing.Process(target=worker, args=(i, batch[i]))
+                jobs.append(p)
+                p.start()
+            for p in jobs:
+                p.join()
+        return sum(ans)  # type:ignore
 
     @aoc_samples(
         (
