4dc4d5a0f1c40f68c37d74b3f71a483fbe1cbf76
pareronia <49491686+pareronia@users.noreply.github.com>
2024-12-28 02:36:58 +0100

    AoC 2024 Day 16 - java

---
+++ src/main/java/AoC2024_16.java
@@ -0,0 +1,136 @@
+import static com.github.pareronia.aoc.IterTools.productIterator;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import com.github.pareronia.aoc.Grid.Cell;
+import com.github.pareronia.aoc.Utils;
+import com.github.pareronia.aoc.geometry.Direction;
+import com.github.pareronia.aoc.geometry.Turn;
+import com.github.pareronia.aoc.graph.Dijkstra;
+import com.github.pareronia.aoc.graph.Dijkstra.AllResults;
+import com.github.pareronia.aoc.solution.Sample;
+import com.github.pareronia.aoc.solution.Samples;
+import com.github.pareronia.aoc.solution.SolutionBase;
+
+public final class AoC2024_16
+        extends SolutionBase<AoC2024_16.ReindeerMaze, Integer, Integer> {
+    
+    private AoC2024_16(final boolean debug) {
+        super(debug);
+    }
+    
+    public static AoC2024_16 create() {
+        return new AoC2024_16(false);
+    }
+    
+    public static AoC2024_16 createDebug() {
+        return new AoC2024_16(true);
+    }
+    
+    @Override
+    protected ReindeerMaze parseInput(final List<String> inputs) {
+        return ReindeerMaze.fromInput(inputs);
+    }
+
+    @Override
+    public Integer solvePart1(final ReindeerMaze maze) {
+        return (int) Dijkstra.distance(
+                new State(maze.start, Direction.RIGHT),
+                state -> state.cell.equals(maze.end),
+                state -> maze.adjacent(state),
+                (curr, next) -> curr.direction == next.direction ? 1 : 1001);
+    }
+    
+    @Override
+    public Integer solvePart2(final ReindeerMaze maze) {
+        final AllResults<State> result = Dijkstra.all(
+                new State(maze.start, Direction.RIGHT),
+                state -> state.cell.equals(maze.end),
+                state -> maze.adjacent(state),
+                (curr, next) -> curr.direction == next.direction ? 1 : 1001);
+        return (int) Utils.stream(productIterator(List.of(maze.end), Direction.CAPITAL))
+            .flatMap(pp -> result.getPaths(new State(pp.first(), pp.second())).stream())
+            .flatMap(List::stream)
+            .map(State::cell)
+            .distinct()
+            .count();
+    }
+    
+    @Override
+    @Samples({
+        @Sample(method = "part1", input = TEST1, expected = "7036"),
+        @Sample(method = "part1", input = TEST2, expected = "11048"),
+        @Sample(method = "part2", input = TEST1, expected = "45"),
+        @Sample(method = "part2", input = TEST2, expected = "64"),
+    })
+    public void samples() {
+    }
+    
+    public static void main(final String[] args) throws Exception {
+        AoC2024_16.create().run();
+    }
+
+    private static final String TEST1 = """
+            ###############
+            #.......#....E#
+            #.#.###.#.###.#
+            #.....#.#...#.#
+            #.###.#####.#.#
+            #.#.#.......#.#
+            #.#.#####.###.#
+            #...........#.#
+            ###.#.#####.#.#
+            #...#.....#.#.#
+            #.#.#.###.#.#.#
+            #.....#...#.#.#
+            #.###.#.#.#.#.#
+            #S..#.....#...#
+            ###############
+            """;
+    private static final String TEST2 = """
+            #################
+            #...#...#...#..E#
+            #.#.#.#.#.#.#.#.#
+            #.#.#.#...#...#.#
+            #.#.#.#.###.#.#.#
+            #...#.#.#.....#.#
+            #.#.#.#.#.#####.#
+            #.#...#.#.#.....#
+            #.#.#####.#.###.#
+            #.#.#.......#...#
+            #.#.###.#####.###
+            #.#.#...#.....#.#
+            #.#.#.#####.###.#
+            #.#.#.........#.#
+            #.#.#.#########.#
+            #S#.............#
+            #################
+            """;
+    
+    record State(Cell cell, Direction direction) {}
+
+    record ReindeerMaze(char[][] grid, Cell start, Cell end) {
+
+        public static ReindeerMaze fromInput(final List<String> strings) {
+            final int h = strings.size();
+            final int w = strings.get(0).length();
+            final char[][] cells = new char[h][w];
+            for (int i = 0; i < h; i++) {
+                cells[i] = strings.get(i).toCharArray();
+            }
+            return new ReindeerMaze(cells, Cell.at(h - 2, 1), Cell.at(1, w - 2));
+        }
+        
+        public Stream<State> adjacent(final State state) {
+            return Set.of(
+                        state.direction,
+                        state.direction.turn(Turn.RIGHT),
+                        state.direction.turn(Turn.LEFT)
+                    ).stream()
+                    .map(dir -> new State(state.cell.at(dir), dir))
+                    .filter(st -> grid[st.cell.getRow()][st.cell.getCol()] != '#');
+        }
+    }
+}
--- src/main/java/com/github/pareronia/aoc/graph/Dijkstra.java
+++ src/main/java/com/github/pareronia/aoc/graph/Dijkstra.java
@@ -10,6 +10,8 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import com.github.pareronia.aoc.Utils;
+
 public class Dijkstra {
 
     public static <T> Result<T> execute(
@@ -40,6 +42,40 @@ public class Dijkstra {
             });
         }
         return new Result<>(start, best, parent);
+    }
+
+    public static <T> AllResults<T> all(
+            final T start,
+            final Predicate<T> end,
+            final Function<T, Stream<T>> adjacent,
+            final BiFunction<T, T, Integer> cost
+    ) {
+        final PriorityQueue<State<T>> q = new PriorityQueue<>();
+        q.add(new State<>(start, 0));
+        final Map<T, Long> distances = new HashMap<>();
+        distances.put(start, 0L);
+        final Map<T, List<T>> predecessors = new HashMap<>();
+        while (!q.isEmpty()) {
+            final State<T> state = q.poll();
+            if (end.test(state.node)) {
+                break;
+            }
+            final long total = distances.getOrDefault(state.node, Long.MAX_VALUE);
+            adjacent.apply(state.node)
+                .forEach(n -> {
+                    final long newDistance = total + cost.apply(state.node, n);
+                    final Long dist_n = distances.getOrDefault(n, Long.MAX_VALUE);
+                    if (newDistance < dist_n) {
+                        distances.put(n, newDistance);
+                        predecessors.put(n, new ArrayList<>(List.of(state.node)));
+                        q.add(new State<>(n, newDistance));
+                    } else if (newDistance == dist_n) {
+                        predecessors.computeIfAbsent(n, k -> new ArrayList<>())
+                                .add(state.node);
+                    }
+            });
+        }
+        return new AllResults<>(start, distances, predecessors);
     }
 
     public static <T> long distance(
@@ -92,17 +128,8 @@ public class Dijkstra {
         }
     }
 
-    public static class Result<T> {
-        private final T source;
-        private final Map<T, Long> distances;
-        private final Map<T, T> paths;
+    public record Result<T>(T source, Map<T, Long> distances, Map<T, T> paths) {
         
-        protected Result(final T source, final Map<T, Long> distances, final Map<T, T> paths) {
-            this.source = source;
-            this.distances = distances;
-            this.paths = paths;
-        }
-
         public Map<T, Long> getDistances() {
             return distances;
         }
@@ -124,6 +151,23 @@ public class Dijkstra {
                 p.add(this.source);
             }
             return p;
+        }
+    }
+    
+    public record AllResults<T>(
+            T source, Map<T, Long> distances, Map<T, List<T>> predecessors) {
+        
+        public List<List<T>> getPaths(final T t) {
+            if (t.equals(source)) {
+                return List.of(List.of(source));
+            }
+            final List<List<T>> paths = new ArrayList<>();
+            for (final T predecessor : predecessors.getOrDefault(t, List.of())) {
+                for (final List<T> path : getPaths(predecessor)) {
+                    paths.add(Utils.concat(path, t));
+                }
+            }
+            return paths;
         }
     }
 }
