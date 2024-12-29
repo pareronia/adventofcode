8dbeb0ea003faa28012f68ff380f32b5e7555f48
pareronia <49491686+pareronia@users.noreply.github.com>
2024-12-29 20:58:05 +0100

    AoC 2024 Day 21 - smaller

--- src/main/python/AoC2024_21.py
+++ src/main/python/AoC2024_21.py
@@ -4,13 +4,13 @@
 #
 
 import sys
+from dataclasses import dataclass
 from functools import cache
+from typing import Iterator
 
 from aoc.common import InputData
 from aoc.common import SolutionBase
 from aoc.common import aoc_samples
-from aoc.graph import Dijkstra
-from aoc.grid import Cell
 
 Input = InputData
 Output1 = int
@@ -25,74 +25,25 @@ TEST = """\
 379A
 """
 
-DIR_KEYPAD = {
-    "A": {
-        "^": "<A",
-        ">": "vA",
-        "v": "<vA",
-        "<": "v<<A",
-        "A": "A",
-    },
-    "^": {
-        "^": "A",
-        ">": "v>A",
-        "v": "vA",
-        "<": "v<A",
-        "A": ">A",
-    },
-    ">": {
-        "^": "<^A",
-        ">": "A",
-        "v": "<A",
-        "<": "<<A",
-        "A": "^A",
-    },
-    "v": {
-        "^": "^A",
-        ">": ">A",
-        "v": "A",
-        "<": "<A",
-        "A": "^>A",
-    },
-    "<": {
-        "^": ">^A",
-        ">": ">>A",
-        "v": ">A",
-        "<": "A",
-        "A": ">>^A",
-    },
-}
-NUM_KEYPAD = {
-    "7": Cell(0, 0),
-    "8": Cell(0, 1),
-    "9": Cell(0, 2),
-    "4": Cell(1, 0),
-    "5": Cell(1, 1),
-    "6": Cell(1, 2),
-    "1": Cell(2, 0),
-    "2": Cell(2, 1),
-    "3": Cell(2, 2),
-    "0": Cell(3, 1),
-    "A": Cell(3, 2),
-}
-SORT = {
-    ("^", "^"): 0,
-    ("^", ">"): -1,
-    ("^", "v"): -1,
-    ("^", "<"): 1,
-    (">", "^"): 1,
-    (">", ">"): 0,
-    (">", "v"): 1,
-    (">", "<"): 1,
-    ("v", "^"): 1,
-    ("v", ">"): -1,
-    ("v", "v"): 0,
-    ("v", "<"): 1,
-    ("<", "^"): -1,
-    ("<", ">"): -1,
-    ("<", "v"): -1,
-    ("<", "<"): 0,
-}
+
+@dataclass(frozen=True)
+class Keypad:
+    layout: list[str]
+
+    def get_position(self, k: str) -> tuple[int, int]:
+        return next(
+            (x, y)
+            for y, r in enumerate(self.layout)
+            for x, c in enumerate(r)
+            if c == k
+        )
+
+    def __getitem__(self, idx: int) -> str:
+        return self.layout[idx]
+
+
+NUMERIC = Keypad(layout=["789", "456", "123", " 0A"])
+DIRECTIONAL = Keypad(layout=[" ^A", "<v>"])
 
 
 class Solution(SolutionBase[Input, Output1, Output2]):
@@ -100,54 +51,39 @@ class Solution(SolutionBase[Input, Output1, Output2]):
         return input_data
 
     def solve(self, input: Input, levels: int) -> int:
-        @cache
-        def do_dir_keypad(seq: str, level: int) -> int:
-            if level == 0:
-                return sum(
-                    len(DIR_KEYPAD[first][second])
-                    for first, second in zip("A" + seq, seq)
-                )
-            else:
-                return sum(
-                    do_dir_keypad(DIR_KEYPAD[first][second], level - 1)
-                    for first, second in zip("A" + seq, seq)
-                )
+        def path(keypad: Keypad, from_: str, to: str) -> str:
+            from_x, from_y = keypad.get_position(from_)
+            to_x, to_y = keypad.get_position(to)
 
-        def do_num_keypad(prev: str, nxt: str, levels: int) -> int:
-            def get_paths(first: str, second: str) -> list[list[Cell]]:
-                start, end = NUM_KEYPAD[first], NUM_KEYPAD[second]
-                result = Dijkstra.all(
-                    start,
-                    lambda cell: cell == end,
-                    lambda cell: (
-                        n
-                        for n in cell.get_capital_neighbours()
-                        if n in NUM_KEYPAD.values()
-                    ),
-                    lambda curr, nxt: 1,
-                )
-                return result.get_paths(end)
+            def paths(x: int, y: int, s: str) -> Iterator[str]:
+                if (x, y) == (to_x, to_y):
+                    yield s + "A"
+                if to_x < x and keypad[y][x - 1] != " ":
+                    yield from paths(x - 1, y, s + "<")
+                if to_y < y and keypad[y - 1][x] != " ":
+                    yield from paths(x, y - 1, s + "^")
+                if to_y > y and keypad[y + 1][x] != " ":
+                    yield from paths(x, y + 1, s + "v")
+                if to_x > x and keypad[y][x + 1] != " ":
+                    yield from paths(x + 1, y, s + ">")
 
-            moves = [
-                [
-                    cell_1.to(cell_2).arrow + ""  # type:ignore
-                    for cell_1, cell_2 in zip(path, path[1:])
-                ]
-                for path in get_paths(prev, nxt)
-            ]
-            best = min(
-                sorted(
-                    moves,
-                    key=lambda m: [SORT[(a, b)] for a, b in zip(m, m[1:])],
-                ),
-                key=lambda m: sum(a != b for a, b in zip(m, m[1:])),
+            return min(
+                paths(from_x, from_y, ""),
+                key=lambda p: sum(a != b for a, b in zip(p, p[1:])),
             )
-            return do_dir_keypad("".join(best) + "A", levels - 1)
+
+        @cache
+        def count(s: str, level: int, max_level: int) -> int:
+            if level > max_level:
+                return len(s)
+            keypad = DIRECTIONAL if level else NUMERIC
+            return sum(
+                count(path(keypad, from_, to), level + 1, max_level)
+                for from_, to in zip("A" + s, s)
+            )
 
         return sum(
-            int(combo[:-1]) * do_num_keypad(a, b, levels)
-            for combo in input
-            for a, b in zip("A" + combo, combo)
+            int(combo[:-1]) * count(combo, 0, levels) for combo in input
         )
 
     def part_1(self, input: Input) -> Output1:
