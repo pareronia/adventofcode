ece0e38ce13640df47a098c931ad7b723e979e8b
pareronia <49491686+pareronia@users.noreply.github.com>
2025-01-03 20:09:05 +0100

    AoC 2024 Day 8 - refactor

--- src/main/python/AoC2024_08.py
+++ src/main/python/AoC2024_08.py
@@ -6,9 +6,11 @@
 import itertools
 import sys
 from collections import defaultdict
+from enum import Enum
+from enum import auto
+from enum import unique
 from functools import reduce
 from operator import ior
-from typing import Callable
 from typing import Iterator
 
 from aoc.common import InputData
@@ -40,6 +42,34 @@ TEST = """\
 """
 
 
+@unique
+class Mode(Enum):
+    MODE_1 = auto()
+    MODE_2 = auto()
+
+    def collect_antinodes(
+        self, h: int, w: int, pair: AntennaPair
+    ) -> set[Position]:
+        def get_antinodes(max_count: int) -> Iterator[Antenna]:
+            vec = Vector.of(pair[0].x - pair[1].x, pair[0].y - pair[1].y)
+            for pos, d in itertools.product(pair, {-1, 1}):
+                for a in range(1, max_count + 1):
+                    antinode = pos.translate(vec, d * a)
+                    if 0 <= antinode.x < w and 0 <= antinode.y < h:
+                        yield antinode
+                    else:
+                        break
+
+        match self:
+            case Mode.MODE_1:
+                return set(get_antinodes(max_count=1)) - {
+                    pair[0],
+                    pair[1],
+                }
+            case Mode.MODE_2:
+                return set(get_antinodes(max_count=sys.maxsize))
+
+
 class Solution(SolutionBase[Input, Output1, Output2]):
     def parse_input(self, input_data: InputData) -> Input:
         lines = list(input_data)
@@ -50,50 +80,25 @@ class Solution(SolutionBase[Input, Output1, Output2]):
                 antennae[freq].add(Antenna(c, h - r - 1))
         return h, w, list(antennae.values())
 
-    def get_antinodes(
-        self, pair: AntennaPair, h: int, w: int, max_count: int = sys.maxsize
-    ) -> Iterator[Antenna]:
-        vec = Vector.of(pair[0].x - pair[1].x, pair[0].y - pair[1].y)
-        for pos, d in itertools.product(pair, {-1, 1}):
-            for a in range(1, max_count + 1):
-                antinode = pos.translate(vec, d * a)
-                if 0 <= antinode.x < w and 0 <= antinode.y < h:
-                    yield antinode
-                else:
-                    break
-
     def solve(
-        self,
-        antennae: list[set[Antenna]],
-        collect_antinodes: Callable[[AntennaPair], set[Antenna]],
+        self, h: int, w: int, antennae: list[set[Antenna]], mode: Mode
     ) -> int:
-        pairs_with_same_frequency = (
-            pair
-            for same_frequency in antennae
-            for pair in itertools.combinations(same_frequency, 2)
-        )
         return len(
-            reduce(ior, map(collect_antinodes, pairs_with_same_frequency))
+            reduce(
+                ior,
+                (
+                    mode.collect_antinodes(h, w, pair)
+                    for same_frequency in antennae
+                    for pair in itertools.combinations(same_frequency, 2)
+                ),
+            )
         )
 
     def part_1(self, input: Input) -> Output1:
-        h, w, antennae = input
-
-        return self.solve(
-            antennae,
-            lambda pair: set(self.get_antinodes(pair, h, w, max_count=1))
-            - {
-                pair[0],
-                pair[1],
-            },
-        )
+        return self.solve(*input, Mode.MODE_1)
 
     def part_2(self, input: Input) -> Output2:
-        h, w, antennae = input
-
-        return self.solve(
-            antennae, lambda pair: set(self.get_antinodes(pair, h, w))
-        )
+        return self.solve(*input, Mode.MODE_2)
 
     @aoc_samples(
         (
