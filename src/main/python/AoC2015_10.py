bcdf5d6267ee1d93647556a3e06a066124ab70a7
pareronia <49491686+pareronia@users.noreply.github.com>
2025-01-18 21:45:31 +0100

    AoC 2015 Day 10 - refactor to SolutionBase

--- src/main/python/AoC2015_10.py
+++ src/main/python/AoC2015_10.py
@@ -3,54 +3,55 @@
 # Advent of Code 2015 Day 10
 #
 
-from aoc import my_aocd
+import sys
+
+from aoc.common import InputData
+from aoc.common import SolutionBase
 from aoc.common import spinner
 
-
-def _look_and_say(string: str) -> str:
-    result = ""
-    i = 0
-    while i < len(string):
-        digit = string[i]
-        j = 0
-        while i+j < len(string) and string[i+j] == digit:
-            j += 1
-        result += str(j) + digit
-        i += j
-    return result
+Input = str
+Output1 = int
+Output2 = int
 
 
-def _look_and_say_iterations(string: str, iterations: int) -> str:
-    for i in range(iterations):
-        string = _look_and_say(string)
-        spinner(i, iterations // 4)
-    return string
+class Solution(SolutionBase[Input, Output1, Output2]):
+    def parse_input(self, input_data: InputData) -> Input:
+        return list(input_data)[0]
+
+    def look_and_say(self, string: str) -> str:
+        result = ""
+        i = 0
+        while i < len(string):
+            digit = string[i]
+            j = 0
+            while i + j < len(string) and string[i + j] == digit:
+                j += 1
+            result += str(j) + digit
+            i += j
+        return result
+
+    def solve(self, string: str, iterations: int) -> str:
+        for i in range(iterations):
+            string = self.look_and_say(string)
+            spinner(i, iterations // 4)
+        return string
+
+    def part_1(self, input: Input) -> Output1:
+        return len(self.solve(input, iterations=40))
+
+    def part_2(self, input: Input) -> Output2:
+        return len(self.solve(input, iterations=50))
+
+    def samples(self) -> None:
+        assert self.solve("1", iterations=5) == "312211"
 
 
-def part_1(inputs: tuple[str]) -> int:
-    assert len(inputs) == 1
-    return len(_look_and_say_iterations(inputs[0], 40))
-
-
-def part_2(inputs: tuple[str]) -> int:
-    assert len(inputs) == 1
-    return len(_look_and_say_iterations(inputs[0], 50))
-
-
-TEST = "1".splitlines()
+solution = Solution(2015, 10)
 
 
 def main() -> None:
-    my_aocd.print_header(2015, 10)
-
-    assert _look_and_say_iterations(TEST, 5) == "312211"
-
-    inputs = my_aocd.get_input(2015, 10, 1)
-    result1 = part_1(inputs)
-    print(f"Part 1: {result1}")
-    result2 = part_2(inputs)
-    print(f"Part 2: {result2}")
+    solution.run(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
