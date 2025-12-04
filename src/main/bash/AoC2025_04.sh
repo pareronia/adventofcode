eed148e92aa40bcd1d7c0e2cad02d41014573cd4
pareronia <49491686+pareronia@users.noreply.github.com>
2025-12-05 00:13:47 +0100

    AoC 2025 Day 4 - bash - faster

--- src/main/bash/AoC2025_04.sh
+++ src/main/bash/AoC2025_04.sh
@@ -6,67 +6,63 @@
 year=2025
 day=04
 
-declare -a grid
-H=0
-W=0
+declare -A grid
 
-find_removable() {
-    local -a ans_removable=()
-    for ((r = 0; r < H; r++)); do
-        for ((c = 0; c < W; c++)); do
-            if [ "${grid[r]:c:1}" = '@' ]; then
-                local -i cnt=0
-                for dr in {-1,0,1}; do
-                    for dc in {-1,0,1}; do
-                        local -i rr=$((r + dr))
-                        local -i cc=$((c + dc))
-                        ((rr == r && cc == c)) && continue
-                        ((rr < 0 || rr >= H)) && continue
-                        ((cc < 0 || cc >= W)) && continue
-                        if [ "${grid[rr]:cc:1}" = '@' ]; then
-                            ((cnt++))
-                        fi
-                    done
-                done
-                if [ "$cnt" -lt 4 ]; then
-                    ans_removable+=("$r:$c")
-                fi
-            fi
+read_grid() {
+    local -i r=0
+    while read -r row || [ -n "$row" ]; do
+        for ((c = 0; c < ${#row}; c++)); do
+            [ "${row:c:1}" = '@' ] && grid[$r:$c]='@'
+        done
+        ((r++))
+    done < "$1"
+}
+
+is_removable() {
+    local k="$1"
+    local -i r="${k%%:*}"
+    local -i c="${k:${#r}+1}"
+    local -i cnt=0
+    for dr in {-1,0,1}; do
+        for dc in {-1,0,1}; do
+            [ -n "${grid[$((r+dr)):$((c+dc))]}" ] && ((cnt++))
         done
     done
-    for row in "${ans_removable[@]}"; do
-        echo "$row"
-    done
+    ((cnt < 5 )) && return 0 || return 1
 }
 
 part1() {
-    mapfile -t grid < "$@"
-    H=${#grid[@]}
-    W=${#grid[0]}
-    mapfile -t removable < <(find_removable)
-    echo "${#removable[@]}"
+    _debug "part 1"
+    read_grid "$1"
+    _debug "${#grid[@]} rolls"
+    local -i ans=0;
+    for k in "${!grid[@]}"; do
+        if is_removable "$k"; then
+            ((ans++))
+        fi
+    done
+    echo "$ans"
+    _debug "part 1 done"
     return 0
 }
 
 part2() {
-    mapfile -t grid < "$@"
-    H=${#grid[@]}
-    W=${#grid[0]}
+    _debug "part 2"
+    read_grid "$1"
     local -i ans=0
     while true; do
-        mapfile -t removable < <(find_removable)
-        ((${#removable[@]} == 0)) && break
-        ((ans += ${#removable[@]}))
-        _debug "${#removable[@]}"
-        for rem in "${removable[@]}"; do
-            IFS=':' read -ra rc <<< "$rem"
-            r="${rc[0]}"
-            c="${rc[1]}"
-            row="${grid[r]:0:c}.${grid[r]:c+1}"
-            grid[r]="$row"
+        _debug "${#grid[@]} rolls"
+        local -i cnt=0;
+        for k in "${!grid[@]}"; do
+            if is_removable "$k"; then
+                ((cnt++))
+                unset "grid[$k]"
+            fi
         done
-        _debug "updated grid"
+        ((cnt == 0)) && break
+        ((ans += cnt))
     done
+    _debug "part 2 done"
     echo "$ans"
     return 0
 }
