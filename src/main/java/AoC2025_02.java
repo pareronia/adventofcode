a7f92d237aa5a7583ef2a3050996228a0a73e842
pareronia <49491686+pareronia@users.noreply.github.com>
2025-12-15 17:20:33 +0100

    AoC 2025 Day 2 - java - faster

--- src/main/java/AoC2025_02.java
+++ src/main/java/AoC2025_02.java
@@ -6,7 +6,6 @@ import com.github.pareronia.aoc.solution.SolutionBase;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
 import java.util.stream.LongStream;
 
 @SuppressWarnings({"PMD.ClassNamingConventions", "PMD.NoPackage"})
@@ -35,24 +34,21 @@ public final class AoC2025_02 extends SolutionBase<List<AoC2025_02.Range>, Long,
                 .toList();
     }
 
-    private long solve(final List<Range> ranges, final String pattern) {
-        final Pattern regex = Pattern.compile(pattern);
+    private long solve(final List<Range> ranges, final Mode mode) {
         return ranges.stream()
                 .flatMapToLong(
-                        range ->
-                                LongStream.rangeClosed(range.from, range.to)
-                                        .filter(n -> regex.matcher(String.valueOf(n)).matches()))
+                        range -> LongStream.rangeClosed(range.from, range.to).filter(mode::check))
                 .sum();
     }
 
     @Override
     public Long solvePart1(final List<Range> ranges) {
-        return this.solve(ranges, "^(\\d+)\\1$");
+        return this.solve(ranges, Mode.EXACTLY_ONCE);
     }
 
     @Override
     public Long solvePart2(final List<Range> ranges) {
-        return this.solve(ranges, "^(\\d+)\\1+$");
+        return this.solve(ranges, Mode.AT_LEAST_ONCE);
     }
 
     @Samples({
@@ -71,4 +67,39 @@ public final class AoC2025_02 extends SolutionBase<List<AoC2025_02.Range>, Long,
             """;
 
     record Range(long from, long to) {}
+
+    enum Mode {
+        EXACTLY_ONCE,
+        AT_LEAST_ONCE;
+
+        @SuppressWarnings("PMD.OnlyOneReturn")
+        private boolean subCheck(final String str, final int sublen) {
+            final int len = str.length();
+            if (len % sublen != 0) {
+                return false;
+            }
+            final String sub = str.substring(0, sublen);
+            for (int i = sublen; i < len; i += sublen) {
+                if (!str.startsWith(sub, i)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        boolean check(final long n) {
+            final String str = String.valueOf(n);
+            return switch (this) {
+                case EXACTLY_ONCE -> str.length() % 2 == 0 && subCheck(str, str.length() / 2);
+                case AT_LEAST_ONCE -> {
+                    for (int i = 1; i <= str.length() / 2; i++) {
+                        if (subCheck(str, i)) {
+                            yield true;
+                        }
+                    }
+                    yield false;
+                }
+            };
+        }
+    }
 }
