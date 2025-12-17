fc64a95524b61b1e29c2fba79ab76f608c23140d
pareronia <49491686+pareronia@users.noreply.github.com>
2025-12-17 09:22:43 +0100

    AoC 2025 Day 7 - faster

--- src/main/python/AoC2025_07.py
+++ src/main/python/AoC2025_07.py
@@ -3,18 +3,13 @@
 # Advent of Code 2025 Day 7
 #
 
-import itertools
 import sys
-from functools import cache
 
 from aoc.common import InputData
 from aoc.common import SolutionBase
 from aoc.common import aoc_samples
-from aoc.geometry import Direction
-from aoc.grid import Cell
-from aoc.grid import CharGrid
 
-Input = CharGrid
+Input = tuple[str, ...]
 Output1 = int
 Output2 = int
 
@@ -44,43 +39,30 @@ START = "S"
 
 class Solution(SolutionBase[Input, Output1, Output2]):
     def parse_input(self, input_data: InputData) -> Input:
-        return CharGrid.from_strings(list(input_data))
+        return tuple(input_data)
+
+    def solve(self, grid: Input) -> tuple[int, int]:
+        start = grid[0].index(START)
+        beams = [0] * len(grid[0])
+        beams[start] = 1
+        splits = 0
+        for r in range(1, len(grid)):
+            new_beams = [0] * len(grid[0])
+            for c in range(len(grid[0])):
+                if grid[r][c] == SPLITTER:
+                    new_beams[c - 1] += beams[c]
+                    new_beams[c + 1] += beams[c]
+                    splits += beams[c] > 0
+                else:
+                    new_beams[c] += beams[c]
+            beams = new_beams
+        return splits, sum(beams)
 
     def part_1(self, grid: Input) -> Output1:
-        beams = {next(grid.get_all_equal_to(START)).col}
-        ans = 0
-        for sp in grid.get_all_equal_to(SPLITTER):
-            if sp.col in beams:
-                ans += 1
-                beams.remove(sp.col)
-                for d in (Direction.LEFT, Direction.RIGHT):
-                    beams.add(sp.at(d).col)
-        return ans
+        return self.solve(grid)[0]
 
     def part_2(self, grid: Input) -> Output2:
-        start = next(grid.get_all_equal_to(SPLITTER))
-        splitters = {start} | set(grid.get_all_equal_to(SPLITTER))
-
-        @cache
-        def dfs(cell: Cell) -> int:
-            if cell == start:
-                return 1
-            return sum(
-                dfs(nxt)
-                for nxt in (
-                    n.at(d)
-                    for d in (Direction.LEFT, Direction.RIGHT)
-                    for n in itertools.takewhile(
-                        lambda n: n not in splitters, grid.get_cells_n(cell)
-                    )
-                )
-                if nxt in splitters
-            )
-
-        bottom_left = Cell(grid.get_max_row_index(), 0)
-        return dfs(bottom_left) + sum(
-            dfs(cell) for cell in grid.get_cells_e(bottom_left)
-        )
+        return self.solve(grid)[1]
 
     @aoc_samples(
         (
