025bf78189e1c2877c9b18cfbe706234ca171088
pareronia <49491686+pareronia@users.noreply.github.com>
2025-12-22 16:06:42 +0100

    refactor to SolutionBase

--- src/main/python/AoC2017_16.py
+++ src/main/python/AoC2017_16.py
@@ -2,123 +2,146 @@
 #
 # Advent of Code 2017 Day 16
 #
-from typing import NamedTuple
+import sys
+from dataclasses import dataclass
 from string import ascii_lowercase
-from aoc import my_aocd
-import aocd
+from typing import Protocol
+from typing import Self
 
+from aoc.common import InputData
+from aoc.common import SolutionBase
+
+TEST = "s1,x3/4,pe/b"
 
 PROGRAMS = ascii_lowercase[:16]
 
 
-class Spin(NamedTuple):
+class Move(Protocol):
+    def execute(self, d: list[str]) -> list[str]:
+        pass
+
+
+@dataclass(frozen=True)
+class Spin(Move):
     amount: int
 
+    @classmethod
+    def from_input(cls, s: str) -> Self:
+        return cls(int(s))
 
-class Exchange(NamedTuple):
+    def execute(self, d: list[str]) -> list[str]:
+        n = len(d) - self.amount
+        return d[n:] + d[:n]
+
+
+@dataclass(frozen=True)
+class Exchange(Move):
     pos_1: int
     pos_2: int
 
+    @classmethod
+    def from_input(cls, s: str) -> Self:
+        a, b = s.split("/")
+        return cls(int(a), int(b))
 
-class Partner(NamedTuple):
+    def execute(self, d: list[str]) -> list[str]:
+        tmp = d[self.pos_1]
+        d[self.pos_1] = d[self.pos_2]
+        d[self.pos_2] = tmp
+        return d
+
+
+@dataclass(frozen=True)
+class Partner(Move):
     program_1: str
     program_2: str
 
+    @classmethod
+    def from_input(cls, s: str) -> Self:
+        a, b = s.split("/")
+        return cls(a, b)
 
-def _parse_move(input_: str):
-    if input_.startswith('s'):
-        return Spin(int(input_[1:]))
-    elif input_.startswith('x'):
-        a = int(input_[1:].split('/')[0])
-        b = int(input_[1:].split('/')[1])
-        return Exchange(a, b)
-    elif input_.startswith('p'):
-        a = input_[1:].split('/')[0][0]
-        b = input_[1:].split('/')[1][0]
-        return Partner(a, b)
-    else:
-        raise ValueError()
-
-
-def _parse(inputs: tuple[str]) -> list:
-    return [_parse_move(_) for _ in inputs[0].split(",")]
-
-
-def _from_string(string: str) -> dict:
-    return {v: i for i, v in enumerate(string)}
+    def execute(self, d: list[str]) -> list[str]:
+        pos_1, pos_2 = -1, -1
+        for i in range(len(d)):
+            if d[i] == self.program_1:
+                pos_1 = i
+            if d[i] == self.program_2:
+                pos_2 = i
+            if pos_1 >= 0 and pos_2 >= 0:
+                break
+        tmp = d[pos_1]
+        d[pos_1] = d[pos_2]
+        d[pos_2] = tmp
+        return d
 
 
-def _to_string(d: dict) -> str:
-    d2 = dict(sorted(d.items(), key=lambda x: x[1]))
-    return "".join([_ for _ in d2])
+@dataclass(frozen=True)
+class Dance:
+    moves: list[Move]
+
+    @classmethod
+    def parse_move(cls, s: str) -> Move:
+        match s[0]:
+            case "s":
+                return Spin.from_input(s[1:])
+            case "x":
+                return Exchange.from_input(s[1:])
+            case _:
+                return Partner.from_input(s[1:])
+
+    @classmethod
+    def from_input(cls, inputs: InputData) -> Self:
+        return cls([cls.parse_move(s) for s in next(iter(inputs)).split(",")])
+
+    def execute(self, string: str, reps: int) -> str:
+        d = list(string)
+        for _ in range(reps):
+            for move in self.moves:
+                d = move.execute(d)
+        return "".join(d)
 
 
-def _swap(d: dict, prog_1: str, prog_2: str) -> None:
-    tmp = d[prog_1]
-    d[prog_1] = d[prog_2]
-    d[prog_2] = tmp
+Input = Dance
+Output1 = str
+Output2 = str
 
 
-def _dance(moves: list, d: dict) -> dict:
-    for move in moves:
-        if isinstance(move, Spin):
-            for k in d:
-                d[k] = (d[k] + move.amount) % len(d)
-        elif isinstance(move, Exchange):
-            for k in d:
-                if d[k] == move.pos_1:
-                    program_1 = k
-                elif d[k] == move.pos_2:
-                    program_2 = k
-            _swap(d, program_1, program_2)
-        elif isinstance(move, Partner):
-            _swap(d, move.program_1, move.program_2)
-    return d
+class Solution(SolutionBase[Input, Output1, Output2]):
+    def parse_input(self, input_data: InputData) -> Input:
+        return Dance.from_input(input_data)
+
+    def part_1(self, dance: Dance) -> str:
+        return dance.execute(PROGRAMS, reps=1)
+
+    def part_2(self, dance: Dance) -> str:
+        cnt = 0
+        ans = PROGRAMS
+        while True:
+            ans = dance.execute(ans, reps=1)
+            cnt += 1
+            if ans == PROGRAMS:
+                break
+        return dance.execute(PROGRAMS, reps=1_000_000_000 % cnt)
+
+    def samples(self) -> None:
+        assert Spin.from_input("3").execute(list("abcde")) == list("cdeab")
+        assert Exchange.from_input("3/4").execute(list("abcde")) == list(
+            "abced"
+        )
+        assert Partner.from_input("a/c").execute(list("abcde")) == list(
+            "cbade"
+        )
+        assert Dance.from_input([TEST]).execute("abcde", 1) == "baedc"
+        assert Dance.from_input([TEST]).execute("abcde", 2) == "ceadb"
 
 
-def _solve(moves: list, string: str, reps: int):
-    d = _from_string(string)
-    for i in range(reps):
-        d = _dance(moves, d)
-    return _to_string(d)
-
-
-def part_1(inputs: tuple[str]) -> int:
-    moves = _parse(inputs)
-    return _solve(moves, PROGRAMS, 1)
-
-
-def part_2(inputs: tuple[str]) -> int:
-    moves = _parse(inputs)
-    cnt = 0
-    ans = PROGRAMS
-    while True:
-        ans = _solve(moves, ans, 1)
-        cnt += 1
-        if ans == PROGRAMS:
-            break
-    return _solve(moves, PROGRAMS, 1_000_000_000 % cnt)
-
-
-TEST = """\
-s1,x3/4,pe/b
-""".splitlines()
+solution = Solution(2017, 16)
 
 
 def main() -> None:
-    puzzle = aocd.models.Puzzle(2017, 16)
-    my_aocd.print_header(puzzle.year, puzzle.day)
-
-    assert _solve(_parse(TEST), "abcde", 1) == "baedc"
-    assert _solve(_parse(TEST), "abcde", 2) == "ceadb"
-
-    inputs = my_aocd.get_input(2017, 16, 1)
-    result1 = part_1(inputs)
-    print(f"Part 1: {result1}")
-    result2 = part_2(inputs)
-    print(f"Part 2: {result2}")
-    my_aocd.check_results(puzzle, result1, result2)
+    solution.run(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
